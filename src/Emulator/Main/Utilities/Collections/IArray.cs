aaa93dafe5e251805ac54cd8591aabe299e3adbc
Piotr Zierhoffer <pzierhoffer@antmicro.com>
2017-11-15 11:30:30 +0100

    [#8793] Added IArray interface.

---
+++ src/Emulator/Main/Utilities/Collections/IArray.cs
@@ -0,0 +1,16 @@
+﻿//
+// Copyright (c) 2010-2017 Antmicro
+//
+// This file is licensed under the MIT License.
+// Full license text is available in 'licenses/MIT.txt'.
+//
+using System.Collections.Generic;
+
+namespace Antmicro.Renode.Utilities.Collections
+{
+    public interface IArray<T> : IEnumerable<T>
+    {
+        int Length { get; }
+        T this[int index] { get; set; }
+    }
+}
