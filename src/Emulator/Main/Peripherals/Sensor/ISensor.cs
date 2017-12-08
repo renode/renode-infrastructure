e93e5edd585ad773fcf42592c2491971a2bc8f9f
Michał Szprejda <mszprejda@antmicro.com>
2017-12-08 15:19:59 +0100

    [#9204] Add new sensor interfaces.

---
+++ src/Emulator/Main/Peripherals/Sensor/ISensor.cs
@@ -0,0 +1,13 @@
+﻿//
+// Copyright (c) 2010-2017 Antmicro
+//
+//  This file is licensed under the MIT License.
+//  Full license text is available in 'licenses/MIT.txt'.
+//
+
+namespace Antmicro.Renode.Peripherals.Sensor
+{
+    public interface ISensor : IPeripheral
+    {
+    }
+}
