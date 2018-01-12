1c81ff5759edffdddf5f5b417bcd8b8c5b9c9832
Mateusz Hołenko <mholenko@antmicro.com>
2018-01-12 09:30:06 +0100

    [DEV] Add IIdentifiable interface.

---
+++ src/Emulator/Main/Debug/IIdentifiable.cs
@@ -0,0 +1,25 @@
+﻿//
+// Copyright (c) 2010-2018 Antmicro
+//
+//  This file is licensed under the MIT License.
+//  Full license text is available in 'licenses/MIT.txt'.
+//
+namespace Antmicro.Renode.Debugging
+{
+    public interface IIdentifiable
+    {
+#if DEBUG
+        int UniqueObjectId { get; }
+#endif
+    }
+
+    public static class IdentifiableExtensions
+    {
+#if DEBUG
+        public static string GetDescription(this IIdentifiable @this)
+        {
+            return $"{@this.GetType().Name}#{@this.UniqueObjectId}";
+        }
+#endif
+    }
+}
---
+++ src/Emulator/Main/Debug/IdentifiableObject.cs
@@ -0,0 +1,29 @@
+﻿//
+// Copyright (c) 2010-2018 Antmicro
+//
+//  This file is licensed under the MIT License.
+//  Full license text is available in 'licenses/MIT.txt'.
+//
+using System.Threading;
+
+namespace Antmicro.Renode.Debugging
+{
+    public class IdentifiableObject : IIdentifiable
+    {
+#if DEBUG
+        public IdentifiableObject()
+        {
+            uniqueObjectId = Interlocked.Increment(ref IdCounter);
+        }
+
+        public int UniqueObjectId
+        {
+            get { return uniqueObjectId; }
+        }
+
+        private int uniqueObjectId;
+
+        private static int IdCounter = 0;
+#endif
+    }
+}
