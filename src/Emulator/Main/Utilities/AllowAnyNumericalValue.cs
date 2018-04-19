e4f5ec994dbf77c40a0bd6750fa0425c3f5080af
Michał Szprejda <mszprejda@antmicro.com>
2018-04-19 13:14:55 +0200

    [#9516] Add AllowAnyNumericalValue attribute.

---
+++ src/Emulator/Main/Utilities/AllowAnyNumericalValue.cs
@@ -0,0 +1,14 @@
+﻿﻿﻿//
+// Copyright (c) 2010-2018 Antmicro
+//
+//  This file is licensed under the MIT License.
+//  Full license text is available in 'licenses/MIT.txt'.
+//
+using System;
+namespace Antmicro.Renode.Utilities
+{
+    [AttributeUsage(AttributeTargets.Enum)]
+    public class AllowAnyNumericalValue : Attribute
+    {
+    }
+}
