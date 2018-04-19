983f54ac6e48dd68818c70891b2b9168a1959b45
Michał Szprejda <mszprejda@antmicro.com>
2018-04-19 13:15:48 +0200

    [#9516] Add mock peripheral with enum attributes.

---
+++ src/Emulator/Main/Tests/UnitTests/Mocks/MockPeripheralWithEnumAttribute.cs
@@ -0,0 +1,49 @@
+﻿//
+// Copyright (c) 2010-2018 Antmicro
+//
+//  This file is licensed under the MIT License.
+//  Full license text is available in 'licenses/MIT.txt'.
+//
+using System;
+using Antmicro.Renode.Peripherals.Bus;
+using Antmicro.Renode.Utilities;
+
+namespace Antmicro.Renode.Tests.UnitTests.Mocks
+{
+    public class MockPeripheralWithEnumAttribute : IBytePeripheral
+    {
+        public MockPeripheralWithEnumAttribute(MockEnum mockEnum = MockEnum.ValidValue, MockEnumWithAttribute mockEnumWithAttribute = MockEnumWithAttribute.ValidValue)
+        {
+            MockEnumValue = mockEnum;
+            MockEnumWithAttributeValue = mockEnumWithAttribute;
+        }
+
+        public void Reset()
+        {
+        }
+
+        public byte ReadByte(long offset)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteByte(long offset, byte value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MockEnum MockEnumValue { get; set; }
+        public MockEnumWithAttribute MockEnumWithAttributeValue { get; set; }
+
+        public enum MockEnum : byte
+        {
+            ValidValue = 1
+        }
+
+        [AllowAnyNumericalValue]
+        public enum MockEnumWithAttribute : byte
+        {
+            ValidValue = 1
+        }
+    }
+}
