b0a4b7ed591e3e5398f03e27cca2ecd7aaafaa9a
Mateusz Hołenko <mholenko@antmicro.com>
2018-04-25 13:13:16 +0200

    [#10331] Handle multiple targets in PLIC.

--- src/Emulator/Peripherals/Peripherals/IRQControllers/PlatformLevelInterruptController.cs
+++ src/Emulator/Peripherals/Peripherals/IRQControllers/PlatformLevelInterruptController.cs
@@ -17,27 +17,38 @@ using Antmicro.Renode.Utilities;
 namespace Antmicro.Renode.Peripherals.IRQControllers
 {
     [AllowedTranslations(AllowedTranslation.ByteToDoubleWord)]
-    public class PlatformLevelInterruptController : IDoubleWordPeripheral, IKnownSize, IIRQController
+    public class PlatformLevelInterruptController : IDoubleWordPeripheral, IKnownSize, IIRQController, INumberedGPIOOutput
     {
         public PlatformLevelInterruptController(Machine machine, int numberOfSources, int numberOfTargets = 1, bool prioritiesEnabled = true)
         {
             // numberOfSources has to fit between these two registers, one bit per source
             if(Math.Ceiling((numberOfSources + 1) / 32.0) * 4 > Registers.Target1Enables - Registers.Target0Enables)
             {
-                throw new ConstructionException($"Current {this.GetType().Name} implementation more than {Registers.Target1Enables - Registers.Target0Enables} sources");
+                throw new ConstructionException($"Current {this.GetType().Name} implementation does not support more than {Registers.Target1Enables - Registers.Target0Enables} sources");
             }
 
             this.prioritiesEnabled = prioritiesEnabled;
 
             this.machine = machine;
-            IRQ = new GPIO();
+            var connections = new Dictionary<int, IGPIO>();
+            for(var i = 0; i < numberOfTargets; i++)
+            {
+                connections[i] = new GPIO();
+            }
+            Connections = connections;
+
             // irqSources are initialized from 1, as the source "0" is not used.
             irqSources = new IrqSource[numberOfSources + 1];
-            for(var i = 1; i <= numberOfSources; i++)
+            for(var i = 1u; i <= numberOfSources; i++)
             {
-                irqSources[i] = new IrqSource();
+                irqSources[i] = new IrqSource(i);
             }
-            activeInterrupts = new Stack<uint>();
+
+            irqTargets = new IrqTarget[numberOfTargets];
+            for(var i = 0u; i < numberOfTargets; i++)
+            {
+                irqTargets[i] = new IrqTarget(i, this);
+            }
 
             var registersMap = new Dictionary<long, DoubleWordRegister>();
 
@@ -68,22 +79,23 @@ namespace Antmicro.Renode.Peripherals.IRQControllers
 
             // When writing to TargetXEnables registers, the user will get an error in log when he tries to write to an
             // unused bit of a register that is at least partially used. A warning will be issued on a usual undefined register access otherwise.
-            for(var target = 0u; target < numberOfTargets; target++)
+            for(var targetId = 0u; targetId < numberOfTargets; targetId++)
             {
-                var lTarget = target;
-                registersMap.Add((long)Registers.Target0ClaimComplete + (targetWidth * target), new DoubleWordRegister(this).WithValueField(0, 32, valueProviderCallback: _ =>
+                var lTargetId = targetId;
+                var lTarget = irqTargets[lTargetId];
+                registersMap.Add((long)Registers.Target0ClaimComplete + (targetWidth * targetId), new DoubleWordRegister(this).WithValueField(0, 32, valueProviderCallback: _ =>
                 {
-                    return AcknowledgePendingInterrupt(lTarget);
+                    return lTarget.AcknowledgePendingInterrupt();
                 }, writeCallback:(_, value) =>
                 {
-                    CompleteHandlingInterrupt(value);
+                    lTarget.CompleteHandlingInterrupt(irqSources[value]);
                 }
                 ));
 
                 for(var offset = 0u; offset < maximumSourceDoubleWords; offset += 4)
                 {
                     var lOffset = offset;
-                    registersMap.Add((long)Registers.Target0Enables + (vectorWidth * target) + offset, new DoubleWordRegister(this).WithValueField(0, 32, writeCallback: (_, value) =>
+                    registersMap.Add((long)Registers.Target0Enables + (vectorWidth * targetId) + offset, new DoubleWordRegister(this).WithValueField(0, 32, writeCallback: (_, value) =>
                     {
                         lock(irqSources)
                         {
@@ -94,30 +106,22 @@ namespace Antmicro.Renode.Peripherals.IRQControllers
                             var bits = BitHelper.GetBits(value);
                             for(var bit = 0u; bit < bits.Length; bit++)
                             {
-                                if(sourceIdBase + bit == 0)
+                                var sourceNumber = sourceIdBase + bit;
+                                if(sourceNumber == 0)
                                 {
-                                    //Source number 0 is not used.
+                                    // Source number 0 is not used.
                                     continue;
                                 }
-                                if(irqSources.Length <= (sourceIdBase + bit))
+                                if(irqSources.Length <= sourceNumber)
                                 {
                                     if(bits[bit])
                                     {
-                                        this.Log(LogLevel.Noisy, "Trying to enable non-existing source: {0}", sourceIdBase + bit);
+                                        this.Log(LogLevel.Noisy, "Trying to enable non-existing source: {0}", sourceNumber);
                                     }
                                     continue;
                                 }
-                                var targets = irqSources[sourceIdBase + bit].EnabledTargets;
-                                if(bits[bit])
-                                {
-                                    this.Log(LogLevel.Noisy, "Enabling target: {0}", sourceIdBase + bit);
-                                    targets.Add(lTarget);
-                                }
-                                else
-                                {
-                                    this.Log(LogLevel.Noisy, "Disabling target: {0}", sourceIdBase + bit);
-                                    targets.Remove(lTarget);
-                                }
+
+                                lTarget.EnableSource(irqSources[sourceNumber], bits[bit]);
                             }
                             RefreshInterrupts();
                         }
@@ -138,12 +142,15 @@ namespace Antmicro.Renode.Peripherals.IRQControllers
             this.Log(LogLevel.Noisy, "Resetting peripheral state");
 
             registers.Reset();
-            activeInterrupts.Clear();
-            foreach(var irqSource in irqSources)
+            foreach(var irqSource in irqSources.Skip(1))
             {
-                irqSource?.Clear();
+                irqSource.Reset();
             }
-            IRQ.Set(false);
+            foreach(var irqTarget in irqTargets)
+            {
+                irqTarget.Reset();
+            }
+            RefreshInterrupts();
         }
 
         public void WriteDoubleWord(long offset, uint value)
@@ -168,8 +175,7 @@ namespace Antmicro.Renode.Peripherals.IRQControllers
             }
         }
 
-        // it looks like every hart has it's own IRQ line; right now we support only one
-        public GPIO IRQ { get; private set; }
+        public IReadOnlyDictionary<int, IGPIO> Connections { get; private set; }
 
         public long Size => 0x4000000;
 
@@ -177,95 +183,132 @@ namespace Antmicro.Renode.Peripherals.IRQControllers
         {
             lock(irqSources)
             {
-                bool val;
-                // Skip(1) because irqSources[0] is unused.
-                if(activeInterrupts.Count > 0)
+                foreach(var target in irqTargets)
                 {
-                    var currentPriority = irqSources[activeInterrupts.Peek()].Priority;
-                    val = irqSources.Skip(1).Any(x => x.Priority > currentPriority && x.IsPending && x.EnabledTargets.Any());
+                    target.RefreshInterrupts();
                 }
-                else
-                {
-                    val = irqSources.Skip(1).Any(x => x.Priority != 0 && x.IsPending && x.EnabledTargets.Any());
-                }
-
-                IRQ.Set(val);
             }
         }
 
-        private uint AcknowledgePendingInterrupt(uint targetId)
-        {
-            lock(irqSources)
-            {
-                //Select required for by-index ordering. Skip(1) to omit first, unused entry.
-                var result = irqSources.Select((x, i) => new { Value = x, Key = (uint)i }).Skip(1)
-                                       .Where(x => x.Value.IsPending && x.Value.EnabledTargets.Contains(targetId))
-                                       .OrderByDescending(x => x.Value.Priority)
-                                       .ThenBy(x => x.Key).FirstOrDefault();
-                if(result?.Value == null)
-                {
-                    this.Log(LogLevel.Noisy, "There is no pending interrupt to acknowledge at the moment");
-                    return 0;
-                }
-                result.Value.IsPending = false;
-                activeInterrupts.Push(result.Key);
-
-                this.Log(LogLevel.Noisy, "Acknowledging pending interrupt #{0}", result.Key);
-
-                IRQ.Set(false);
-                return result.Key;
-            }
-        }
-
-        private void CompleteHandlingInterrupt(uint irqId)
-        {
-            lock(irqSources)
-            {
-                this.Log(LogLevel.Noisy, "Completing irq {0}", irqId);
-
-                var topActiveInterrupt = activeInterrupts.Pop();
-                if(topActiveInterrupt != irqId)
-                {
-                    this.Log(LogLevel.Error, "Trying to complete irq {0}, but {1} is the active one", irqId, topActiveInterrupt);
-                    return;
-                }
-                irqSources[irqId].IsPending = irqSources[irqId].State;
-                RefreshInterrupts();
-            }
-        }
-
-        private Stack<uint> activeInterrupts;
         private readonly IrqSource[] irqSources;
+        private readonly IrqTarget[] irqTargets;
         private readonly Machine machine;
         private readonly DoubleWordRegisterCollection registers;
         private readonly bool prioritiesEnabled;
 
         private class IrqSource
         {
-            public IrqSource()
+            public IrqSource(uint id)
             {
-                EnabledTargets = new HashSet<uint>();
-                Clear();
+                Id = id;
+                Reset();
             }
 
             public override string ToString()
             {
-                return $"IrqSource; priority: {Priority}, state: {State}, is pending: {IsPending}, has enabled targets: {EnabledTargets.Any()}";
+                return $"IrqSource id: {Id}, priority: {Priority}, state: {State}, pending: {IsPending}";
             }
 
-            public void Clear()
+            public void Reset()
             {
-                //1 is the default, lowest value. 0 means "no interrupt".
-                Priority = 1;
+                Priority = DefaultPriority;
                 State = false;
                 IsPending = false;
-                EnabledTargets.Clear();
             }
 
+            public uint Id { get; private set; }
             public uint Priority { get; set; }
             public bool State { get; set; }
             public bool IsPending { get; set; }
-            public HashSet<uint> EnabledTargets { get; private set; }
+
+            // 1 is the default, lowest value. 0 means "no interrupt".
+            private const uint DefaultPriority = 1;
+        }
+
+        private class IrqTarget
+        {
+            public IrqTarget(uint id, PlatformLevelInterruptController irqController)
+            {
+                Id = id;
+                this.irqController = irqController;
+                activeInterrupts = new Stack<IrqSource>();
+                enabledSources = new HashSet<IrqSource>();
+            }
+
+            public uint AcknowledgePendingInterrupt()
+            {
+                lock(irqController.irqSources)
+                {
+                    var pendingIrq = enabledSources.Where(x => x.IsPending)
+                        .OrderByDescending(x => x.Priority)
+                        .ThenBy(x => x.Id).FirstOrDefault();
+                    if(pendingIrq == null)
+                    {
+                        irqController.Log(LogLevel.Noisy, "There is no pending interrupt to acknowledge at the moment for target {0}", Id);
+                        return 0;
+                    }
+                    pendingIrq.IsPending = false;
+                    activeInterrupts.Push(pendingIrq);
+
+                    irqController.Log(LogLevel.Noisy, "Acknowledging pending interrupt #{0} at target {1}", pendingIrq.Id, Id);
+
+                    RefreshInterrupts();
+                    return pendingIrq.Id;
+                }
+            }
+
+            public void CompleteHandlingInterrupt(IrqSource irq)
+            {
+                lock(irqController.irqSources)
+                {
+                    irqController.Log(LogLevel.Noisy, "Completing irq {0} at target {1}", irq.Id, Id);
+
+                    var topActiveInterrupt = activeInterrupts.Pop();
+                    if(topActiveInterrupt != irq)
+                    {
+                        irqController.Log(LogLevel.Error, "Trying to complete irq {0} at target {1}, but {2} is the active one", irq.Id, Id, topActiveInterrupt.Id);
+                        return;
+                    }
+
+                    irq.IsPending = irq.State;
+                    RefreshInterrupts();
+                }
+            }
+
+            public void RefreshInterrupts()
+            {
+                lock(irqController.irqSources)
+                {
+                    var currentPriority = activeInterrupts.Count > 0 ? activeInterrupts.Peek().Priority : 0;
+                    irqController.Connections[(int)Id].Set(enabledSources.Any(x => x.Priority > currentPriority && x.IsPending));
+                }
+            }
+
+            public void EnableSource(IrqSource s, bool enabled)
+            {
+                if(enabled)
+                {
+                    enabledSources.Add(s);
+                }
+                else
+                {
+                    enabledSources.Remove(s);
+                }
+                RefreshInterrupts();
+            }
+
+            public void Reset()
+            {
+                activeInterrupts.Clear();
+                enabledSources.Clear();
+                RefreshInterrupts();
+            }
+
+            public uint Id { get; private set; }
+
+            private readonly HashSet<IrqSource> enabledSources;
+            private readonly PlatformLevelInterruptController irqController;
+            private readonly Stack<IrqSource> activeInterrupts;
         }
 
         private enum Registers : long
