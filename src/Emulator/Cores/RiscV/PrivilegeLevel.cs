ec7e67b9fe0f28afd2b12d6a798d970617793041
Piotr Zierhoffer <pzierhoffer@antmicro.com>
2018-05-04 16:04:31 +0200

    [#10331] Add PrivilegeLevel enum.

---
+++ src/Emulator/Cores/RiscV/PrivilegeLevel.cs
@@ -0,0 +1,19 @@
+﻿//
+// Copyright (c) 2010-2018 Antmicro
+//
+// This file is licensed under the MIT License.
+// Full license text is available in 'licenses/MIT.txt'.
+//
+
+namespace Antmicro.Renode.Peripherals.CPU
+{
+    public enum PrivilegeLevel
+    {
+        User = 0,
+        Supervisor = 1,
+        //The Hypervisor level is not yet supported
+        //Hypervisor = 2,
+        Machine = 3
+    }
+}
+
