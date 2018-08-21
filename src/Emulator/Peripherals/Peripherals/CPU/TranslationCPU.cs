7a64257413d94f0a1331614e3bdcc3dcc79335d7
Mateusz Hołenko <mholenko@antmicro.com>
2018-08-21 08:28:37 +0200

    [#10872] Add some new traces.

--- src/Emulator/Peripherals/Peripherals/CPU/TranslationCPU.cs
+++ src/Emulator/Peripherals/Peripherals/CPU/TranslationCPU.cs
@@ -1520,26 +1520,31 @@ namespace Antmicro.Renode.Peripherals.CPU
             {
                 lock(haltedLock)
                 {
+                    this.Trace();
                     if(value == isHalted)
                     {
                         return;
                     }
+                    this.Trace();
                     isHalted = value;
                     if(TimeHandle != null)
                     {
                         if(insideBlockHook)
                         {
+                            this.Trace();
                             // deferr disabling to the moment of unlatch, otherwise we would deadlock in block begin hook
                             TimeHandle.DisableRequest = value;
                         }
                         else
                         {
+                            this.Trace();
                             // this is needed to quit 'RequestTimeInterval'
                             TimeHandle.Enabled = !value;
                         }
                     }
                     if(isHalted)
                     {
+                        this.Trace();
                         InnerPause(new HaltArguments(HaltReason.Pause));
                     }
                 }
