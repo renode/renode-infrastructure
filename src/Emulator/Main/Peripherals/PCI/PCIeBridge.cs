39534cc4f76a64292b07260f4aa7ef8f711c088b
Piotr Zierhoffer <pzierhoffer@antmicro.com>
2018-10-24 12:04:18 +0200

    [#10834] Base implementation of a PCIe peripheral.

---
+++ src/Emulator/Main/Peripherals/PCI/PCIeBridge.cs
@@ -0,0 +1,28 @@
+//
+// Copyright (c) 2010-2018 Antmicro
+//
+// This file is licensed under the MIT License.
+// Full license text is available in 'licenses/MIT.txt'.
+//
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Antmicro.Renode.Core;
+using Antmicro.Renode.Core.Structure;
+using Antmicro.Renode.Core.Structure.Registers;
+using Antmicro.Renode.Exceptions;
+using Antmicro.Renode.Logging;
+using Antmicro.Renode.Peripherals.Bus;
+using Antmicro.Renode.Utilities;
+
+namespace Antmicro.Renode.Peripherals.PCI
+{
+    public abstract class PCIeBridge : PCIeBasePeripheral
+    {
+        //todo: add registers enum
+        public PCIeBridge(IPCIeRouter parent) : base(parent, HeaderType.Bridge)
+        {
+        }
+    }
+}
+
