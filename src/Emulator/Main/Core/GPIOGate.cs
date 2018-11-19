f4167beb75058db7b0cf17bb489bb8113f742117
Mateusz Hołenko <mholenko@antmicro.com>
2018-11-19 09:21:37 +0100

    [#12033] Add `GPIOGate` helper class.

---
+++ src/Emulator/Main/Core/GPIOGate.cs
@@ -0,0 +1,86 @@
+//
+// Copyright (c) 2010-2018 Antmicro
+//
+// This file is licensed under the MIT License.
+// Full license text is available in 'licenses/MIT.txt'.
+//
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Antmicro.Renode.Core
+{
+    public class GPIOGate
+    {
+        public GPIOGate(GPIO destinationIrq)
+        {
+            destination = destinationIrq;
+            nodes = new List<IrqNode>();
+        }
+
+        public IGPIO GetGPIO()
+        {
+            var result = new IrqNode(destination, HandleChangeState);
+            nodes.Add(result);
+            return result;
+        }
+
+        private void HandleChangeState()
+        {
+            for(var i = 0; i < nodes.Count; ++i)
+            {
+                if(nodes[i].LocalState)
+                {
+                    destination.Set(true);
+                    return;
+                }
+            }
+            destination.Set(false);
+        }
+
+        private readonly GPIO destination;
+
+        private readonly List<IrqNode> nodes;
+
+        private class IrqNode : IGPIO
+        {
+            public IrqNode(IGPIO destination, Action changeCallback)
+            {
+                this.destination = destination;
+                this.changeCallback = changeCallback;
+            }
+
+            public void Connect(IGPIOReceiver destination, int destinationNumber)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Disconnect()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Set(bool value)
+            {
+                LocalState = value;
+                changeCallback();
+            }
+
+            public void Toggle()
+            {
+                Set(!LocalState);
+            }
+
+            public bool LocalState { get; private set; }
+
+            public bool IsSet => destination.IsSet;
+
+            public bool IsConnected => destination.IsConnected;
+
+            public GPIOEndpoint Endpoint => destination.Endpoint;
+
+            private readonly IGPIO destination;
+            private readonly Action changeCallback;
+        }
+    }
+}
