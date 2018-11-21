8eb6e144869880f5ab6a42270075a2dd316849e0
Michał Szprejda <mszprejda@antmicro.com>
2018-11-21 18:26:07 +0100

    [#11882] Add CAN message frame.

---
+++ src/Emulator/Main/Peripherals/CAN/CANMessageFrame.cs
@@ -0,0 +1,29 @@
+﻿﻿﻿//
+// Copyright (c) 2010-2018 Antmicro
+//
+//  This file is licensed under the MIT License.
+//  Full license text is available in 'licenses/MIT.txt'.
+//
+using System;
+using System.Linq;
+using Antmicro.Renode.Utilities;
+
+namespace Antmicro.Renode.Core.CAN
+{
+    public class CANMessageFrame
+    {
+        public CANMessageFrame(uint id, byte[] data)
+        {
+            Id = id;
+            Data = data;
+        }
+
+        public override string ToString()
+        {
+            return $"[Message: Data=[{Data.Select(x => "0x{0:X}".FormatWith(x)).Stringify()}], Id={Id}, DataLength={Data.Length}]";
+        }
+
+        public uint Id { get; }
+        public byte[] Data { get; }
+    }
+}
