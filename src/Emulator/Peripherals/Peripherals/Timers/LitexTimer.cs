f7d9f18c51a47a4840d4674bc39c734df64d5b16
Mateusz Hołenko <mholenko@antmicro.com>
2018-12-03 14:32:34 +0100

    LitexTimer cleanup.

--- src/Emulator/Peripherals/Peripherals/Timers/LitexTimer.cs
+++ src/Emulator/Peripherals/Peripherals/Timers/LitexTimer.cs
@@ -15,85 +15,78 @@ namespace Antmicro.Renode.Peripherals.Timers
         {
             IRQ = new GPIO();
             innerTimer = new LimitTimer[TimersCount];
-            // reloadValue = new IValueRegisterField[TimersCount];
             for(var i = 0; i < innerTimer.Length; i++)
             {
                 var j = i;
-                innerTimer[i] = new LimitTimer(machine.ClockSource, frequency, eventEnabled: true);
+                innerTimer[i] = new LimitTimer(machine.ClockSource, frequency, eventEnabled: true, autoUpdate: true);
                 innerTimer[i].LimitReached += delegate
                 {
-                    if(reloadValue[j].Value != 0)
-                    {
-                        innerTimer[j].Value = reloadValue[j].Value;
-                        innerTimer[j].Limit = reloadValue[j].Value;
-                    }
-                    if(irqEnabled.Value)
-                    {
-                        IRQ.Set();
-                    }
+                    innerTimer[j].Limit = reloadValue[j].Value;
+                    UpdateTimerEnabled();
+
+                    irqPending.Value = true;
+                    UpdateInterrupts();
                 };
             }
-        }
-
-        public void ToggleIRQ()
-        {
-            IRQ.Toggle();
         }
 
         public GPIO IRQ { get; }
 
         public long Size => 0x44;
 
-
         protected override void DefineRegisters()
         {
-            Registers.TIMER_EV_PENDING_ADDR.Define32(this)
-                .WithFlag(0, writeCallback: (_, val) =>
-                    {
-                        if(!val)
-                        {
-                            return;
-                        }
-
-                        IRQ.Set(false);
-                    })
-            ;
-
-            Registers.TIMER_EV_ENABLE_ADDR.Define32(this)
-                .WithFlag(0, out irqEnabled, name: "EV_ENABLE");
-
             for(var i = 0; i < TimersCount; i++)
             {
                 var j = i;
 
                 ((Registers)(Registers.Timer0Load + i * 0x4)).Define32(this)
                     .WithValueField(0, 32, name: "LOAD",
-                    writeCallback: (_, val) =>
-                    {
-                        innerTimer[j].Value = val;
-                        innerTimer[j].Limit = val;
-                    });
+                        valueProviderCallback: _ => checked((uint)innerTimer[j].Value),
+                        writeCallback: (_, val) =>
+                        {
+                            // as `AutoUpdate` is set, changing `Limit` automatically sets `Value` as well
+                            innerTimer[j].Limit = val;
+                            UpdateTimerEnabled();
+                        })
+                ;
 
                 ((Registers)(Registers.Timer0Reload + i * 0x4)).Define32(this)
-                    .WithValueField(0, 32, out reloadValue[j], name: "RELOAD");
-
+                    .WithValueField(0, 32, out reloadValue[j], name: "RELOAD")
+                ;
             }
 
-            Registers.Timer0Enable.Define32(this)
-                .WithFlag(0, writeCallback: (_, val) =>
-                {
-                    foreach(var timer in innerTimer)
-                    {
-                        if(timer.Value != 0 && timer.Limit != 0)
-                        {
-                            timer.Enabled = val;
-                        }
-                    }
-                });
+            Registers.TimerEnable.Define32(this)
+                .WithFlag(0, out timerEnabled, name: "ENABLE", writeCallback: (_, val) => UpdateTimerEnabled())
+            ;
+
+            Registers.EventPending.Define32(this)
+                .WithFlag(0, out irqPending, FieldMode.Read | FieldMode.WriteOneToClear, name: "EV_PENDING", changeCallback: (_, __) => UpdateInterrupts())
+            ;
+
+            Registers.EventEnable.Define32(this)
+                .WithFlag(0, out irqEnabled, name: "EV_ENABLE", changeCallback: (_, __) => UpdateInterrupts())
+            ;
         }
 
+        private void UpdateTimerEnabled()
+        {
+            foreach(var timer in innerTimer)
+            {
+                timer.Enabled = timerEnabled.Value && (timer.Limit != 0);
+            }
+        }
+
+        private void UpdateInterrupts()
+        {
+            IRQ.Set(irqPending.Value && irqEnabled.Value);
+        }
+
+        // TODO: this field be set here (and not in the constructor), as `DefineRegisters` method uses it; think about the architecture of `BasicDoubleWordPeripheral`
         private IValueRegisterField[] reloadValue = new IValueRegisterField[TimersCount];
         private IFlagRegisterField irqEnabled;
+        private IFlagRegisterField irqPending;
+        private IFlagRegisterField timerEnabled;
 
         private LimitTimer[] innerTimer;
 
@@ -103,9 +96,9 @@ namespace Antmicro.Renode.Peripherals.Timers
         {
             Timer0Load = 0x0,
             Timer0Reload = 0x10,
-            Timer0Enable = 0x20,
-            TIMER_EV_PENDING_ADDR = 0x3c,
-            TIMER_EV_ENABLE_ADDR = 0x40
+            TimerEnable = 0x20,
+            EventPending = 0x3c,
+            EventEnable = 0x40
         }
     }
 }
