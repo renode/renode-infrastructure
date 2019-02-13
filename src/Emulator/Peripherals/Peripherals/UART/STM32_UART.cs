8ec38315e0c61098d4e1b50cab41af7fa6869109
Piotr Zierhoffer <pzierhoffer@antmicro.com>
2019-02-13 16:53:45 +0100

    [#12982] Reimplement STM32_UART.

--- src/Emulator/Peripherals/Peripherals/UART/STM32_UART.cs
+++ src/Emulator/Peripherals/Peripherals/UART/STM32_UART.cs
@@ -1,6 +1,5 @@
 //
-// Copyright (c) 2010-2018 Antmicro
-// Copyright (c) 2011-2015 Realtime Embedded
+// Copyright (c) 2010-2019 Antmicro
 //
 // This file is licensed under the MIT License.
 // Full license text is available in 'licenses/MIT.txt'.
@@ -11,187 +10,214 @@ using Antmicro.Renode.Logging;
 using Antmicro.Renode.Peripherals.Bus;
 using System.Collections.Generic;
 using Antmicro.Migrant;
+using Antmicro.Renode.Core.Structure.Registers;
 
 namespace Antmicro.Renode.Peripherals.UART
 {
     [AllowedTranslations(AllowedTranslation.WordToDoubleWord | AllowedTranslation.ByteToDoubleWord)]
-    public class STM32_UART :  IDoubleWordPeripheral, IUART
+    public class STM32_UART : BasicDoubleWordPeripheral, IUART
     {
-        public STM32_UART(Machine machine)
+        public STM32_UART(Machine machine, uint frequency = 8000000) : base(machine)
         {
-            this.machine = machine;
-            IRQ = new GPIO();
-            charFifo = new Queue<byte>();
-            Reset();
+            this.frequency = frequency;
         }
-
-        public GPIO IRQ { get; private set; }
-
-        [field: Transient]
-        public event Action<byte> CharReceived;
 
         public void WriteChar(byte value)
         {
-            lock(charFifo)
+            if(!usartEnabled.Value && !receiverEnabled.Value)
             {
-                charFifo.Enqueue(value);
-                Update();
+                this.Log(LogLevel.Warning, "Received a character, but the receiver is not enabled, dropping.");
+                return;
             }
+            receiveFifo.Enqueue(value);
+            readFifoNotEmpty.Value = true;
+            Update();
         }
-    
-        public void WriteDoubleWord(long address, uint value)
-        {   
-            lock(charFifo)
-            {
-                switch((Register)address)
-                {
-                case Register.Status:
-                    transmissionComplete &= (value & (1u << 6)) != 0;
-                    return;
-                case Register.Data:
-                    transmissionComplete = true;
-                    var handler = CharReceived;
-                    if(handler != null)
-                    {
-                        handler((byte)(value & 0xFF));
-                    }
-                    return;
-                case Register.Control1:
-                    controlRegister1 = value;
-                    Update();
-                    return;
-                case Register.Control2:
-                    controlRegister2 = value;
-                    return;
-                case Register.BaudRate:
-                    baudRate = value;
-                    return;
-                default:
-                    this.LogUnhandledWrite(address, value);
-                    return;
-                }
-            }
-        }
-        
-        public uint ReadDoubleWord(long offset)
+
+        public override void Reset()
         {
-            lock(charFifo)
-            {
-                switch((Register)offset)
-                {
-                    case Register.Status:
-                        return charFifo.Count > 0 ? (1u << 5 | 1u << 7 | (transmissionComplete ? 1u : 0u) << 6) : (1 << 7 | (transmissionComplete ? 1u : 0u) << 6);
-
-                    case Register.Data:
-                        var returnValue = 0u;
-                        if(charFifo.Count > 0)
-                        {
-                            returnValue = charFifo.Dequeue();
-                        }
-                        Update();
-                        return returnValue;
-                    case Register.Control1:
-                        return controlRegister1;
-                    default:
-                        this.LogUnhandledRead(offset);
-                        return 0u;
-                }
-            }
-        }
-        
-        public void Reset()
-        {
-            // TODO!
-            transmissionComplete = true;
-        }
-
-        private void Update()
-        {
-            IRQ.Set((controlRegister1 & (uint)ControlRegister1.TXEInterruptEnable) != 0 || charFifo.Count > 0);
-        }
-
-        private readonly Queue<byte> charFifo;
-        private readonly Machine machine;
-
-        private bool transmissionComplete;
-        private uint controlRegister1;
-        private uint controlRegister2;
-        private uint baudRate;
-
-        private enum Register : long
-        {
-            Status   = 0x00,
-            Data     = 0x04,
-            Control1 = 0x0C,
-            Control2 = 0x10,
-            BaudRate = 0x08
-        }
-
-        [Flags]
-        private enum ControlRegister1 : uint
-        {
-            TXEInterruptEnable  = (1u << 7),
-            ParitySelection     = (1u << 9),
-            ParityControlEnable = (1u << 10),
-            OversamplingMode    = (1u << 15)
-        }
-
-        [Flags]
-        private enum ControlRegister2 : uint
-        {
-            StopBitsH = (1u << 13),
-            StopBitsL = (1u << 12)
-        }
-
-        public Bits StopBits
-        {
-            get
-            {
-                var stops = (controlRegister2 & ((uint)ControlRegister2.StopBitsH | (uint)ControlRegister2.StopBitsL) >> 12);
-                switch (stops)
-                {
-                case 0:
-                    return Bits.One;
-                case 1:
-                    return Bits.Half;
-                case 2:
-                    return Bits.Two;
-                case 3:
-                    return Bits.OneAndAHalf;
-                default:
-                    return Bits.None;
-                }
-            }
-        }
-
-        public Parity ParityBit
-        {
-            get
-            {
-                if ((controlRegister1 & (uint)ControlRegister1.ParityControlEnable) == 0)
-                {
-                    return Parity.None;
-                }
-                else
-                {
-                    return (controlRegister1 & (uint)ControlRegister1.ParitySelection) == 0 ? Parity.Even : Parity.Odd;
-                }
-            }
+            base.Reset();
+            receiveFifo.Clear();
         }
 
         public uint BaudRate
         {
             get
             {
-                var over8 = ((controlRegister1 & (uint)ControlRegister1.OversamplingMode) != 0) ? 1 : 0;
-                var mantisa = baudRate >> 4;
-                var fraction = baudRate & 0xF;
-                var divisor = (8 * (2 - over8) * (mantisa + fraction / 16));
-                return divisor == 0 ? 0 : (uint)(UARTClockFrequency / divisor);
+                //OversamplingMode.By8 means we ignore the oldest bit of dividerFraction.Value
+                var fraction = oversamplingMode.Value == OversamplingMode.By16 ? dividerFraction.Value : dividerFraction.Value & 0b111;
+
+                var divisor = 8 * (2 - (int)oversamplingMode.Value) * (dividerMantissa.Value + fraction / 16.0);
+                return divisor == 0 ? 0 : (uint)(frequency / divisor);
             }
         }
 
-        private const uint UARTClockFrequency = 0;
+        public Bits StopBits
+        {
+            get
+            {
+                switch(stopBits.Value)
+                {
+                case StopBitsValues.Half:
+                    return Bits.Half;
+                case StopBitsValues.One:
+                    return Bits.One;
+                case StopBitsValues.OneAndAHalf:
+                    return Bits.OneAndAHalf;
+                case StopBitsValues.Two:
+                    return Bits.Two;
+                default:
+                    throw new ArgumentException("Invalid stop bits value");
+                }
+            }
+        }
+
+        public Parity ParityBit => parityControlEnabled.Value ?
+                                    (paritySelection.Value == ParitySelection.Even ?
+                                        Parity.Even :
+                                        Parity.Odd) :
+                                    Parity.None;
+
+        public GPIO IRQ { get; } = new GPIO();
+
+        public event Action<byte> CharReceived;
+
+        protected override void DefineRegisters()
+        {
+            Register.Status.Define(this, 0xC0, "USART_SR")
+                .WithTaggedFlag("PE", 0)
+                .WithTaggedFlag("FE", 1)
+                .WithTaggedFlag("NF", 2)
+                .WithFlag(3, FieldMode.Read, valueProviderCallback: _ => false, name: "ORE") // we assume no receive overruns
+                .WithTaggedFlag("IDLE", 4)
+                .WithFlag(5, out readFifoNotEmpty, FieldMode.Read | FieldMode.WriteZeroToClear, name: "RXNE") // as these two flags are WZTC, we cannot just calculate their results
+                .WithFlag(6, out transmissionComplete, FieldMode.Read | FieldMode.WriteZeroToClear, name: "TC")
+                .WithFlag(7, FieldMode.Read, valueProviderCallback: _ => true, name: "TXE") // we always assume "transmit data register empty"
+                .WithTaggedFlag("LBD", 8)
+                .WithTaggedFlag("CTS", 9)
+                .WithReservedBits(10, 22)
+                .WithWriteCallback((_, __) => Update())
+            ;
+            Register.Data.Define(this, name: "USART_DR")
+                .WithValueField(0, 9, valueProviderCallback: _ =>
+                    {
+                        uint value = 0;
+                        if(receiveFifo.Count > 0)
+                        {
+                            value = receiveFifo.Dequeue();
+                        }
+                        readFifoNotEmpty.Value = receiveFifo.Count > 0;
+                        Update();
+                        return value;
+                    }, writeCallback: (_, value) =>
+                    {
+                        if(!usartEnabled.Value && !transmitterEnabled.Value)
+                        {
+                            this.Log(LogLevel.Warning, "Trying to transmit a character, but the transmitter is not enabled. dropping.");
+                            return;
+                        }
+                        CharReceived?.Invoke((byte)value);
+                        transmissionComplete.Value = true;
+                        Update();
+                    }, name: "DR"
+                )
+            ;
+            Register.BaudRate.Define(this, name: "USART_BRR")
+                .WithValueField(0, 4, out dividerFraction, name: "DIV_Fraction")
+                .WithValueField(4, 12, out dividerMantissa, name: "DIV_Mantissa")
+            ;
+            Register.Control1.Define(this, name: "USART_CR1")
+                .WithTaggedFlag("SBK", 0)
+                .WithTaggedFlag("RWU", 1)
+                .WithFlag(2, out receiverEnabled, name: "RE")
+                .WithFlag(3, out transmitterEnabled, name: "TE")
+                .WithTaggedFlag("IDLEIE", 4)
+                .WithFlag(5, out receiverNotEmptyInterruptEnabled, name: "RXNEIE")
+                .WithFlag(6, out transmissionCompleteInterruptEnabled, name: "TCIE")
+                .WithFlag(7, out transmitDataRegisterEmptyInterruptEnabled, name: "TXEIE")
+                .WithTaggedFlag("PEIE", 8)
+                .WithEnumField(9, 1, out paritySelection, name: "PS")
+                .WithFlag(10, out parityControlEnabled, name: "PCE")
+                .WithTaggedFlag("WAKE", 11)
+                .WithTaggedFlag("M", 12)
+                .WithFlag(13, out usartEnabled, name: "UE")
+                .WithReservedBits(14, 1)
+                .WithEnumField(15, 1, out oversamplingMode, name: "OVER8")
+                .WithReservedBits(16, 16)
+                .WithWriteCallback((_, __) => Update())
+            ;
+            Register.Control2.Define(this, name: "USART_CR2")
+                .WithTag("ADD", 0, 4)
+                .WithReservedBits(5, 1)
+                .WithTaggedFlag("LBDIE", 6)
+                .WithReservedBits(7, 1)
+                .WithTaggedFlag("LBCL", 8)
+                .WithTaggedFlag("CPHA", 9)
+                .WithTaggedFlag("CPOL", 10)
+                .WithTaggedFlag("CLKEN", 11)
+                .WithEnumField(12, 2, out stopBits, name: "STOP")
+                .WithTaggedFlag("LINEN", 14)
+                .WithReservedBits(15, 17)
+            ;
+        }
+
+        private void Update()
+        {
+            IRQ.Set(
+                (receiverNotEmptyInterruptEnabled.Value && readFifoNotEmpty.Value) ||
+                (transmitDataRegisterEmptyInterruptEnabled.Value) || // TXE is assumed to be true
+                (transmissionCompleteInterruptEnabled.Value && transmissionComplete.Value)
+            );
+        }
+
+        private readonly uint frequency;
+
+        private IEnumRegisterField<OversamplingMode> oversamplingMode;
+        private IEnumRegisterField<StopBitsValues> stopBits;
+        private IFlagRegisterField usartEnabled;
+        private IFlagRegisterField parityControlEnabled;
+        private IEnumRegisterField<ParitySelection> paritySelection;
+        private IFlagRegisterField transmissionCompleteInterruptEnabled;
+        private IFlagRegisterField transmitDataRegisterEmptyInterruptEnabled;
+        private IFlagRegisterField receiverNotEmptyInterruptEnabled;
+        private IFlagRegisterField receiverEnabled;
+        private IFlagRegisterField transmitterEnabled;
+        private IFlagRegisterField readFifoNotEmpty;
+        private IFlagRegisterField transmissionComplete;
+        private IValueRegisterField dividerMantissa;
+        private IValueRegisterField dividerFraction;
+
+        private readonly Queue<byte> receiveFifo = new Queue<byte>();
+
+        private enum OversamplingMode
+        {
+            By16 = 0,
+            By8 = 1
+        }
+
+        private enum StopBitsValues
+        {
+            One = 0,
+            Half = 1,
+            Two = 2,
+            OneAndAHalf = 3
+        }
+
+        private enum ParitySelection
+        {
+            Even = 0,
+            Odd = 1
+        }
+
+        private enum Register : long
+        {
+            Status = 0x00,
+            Data = 0x04,
+            BaudRate = 0x08,
+            Control1 = 0x0C,
+            Control2 = 0x10,
+            Control3 = 0x14,
+            GuardTimeAndPrescaler = 0x18
+        }
     }
 }
-
