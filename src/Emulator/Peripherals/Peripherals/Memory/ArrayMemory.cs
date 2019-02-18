8bede8366d0455fc94314a5f4edbd92697582ed1
Mateusz Hołenko <mholenko@antmicro.com>
2019-02-18 16:00:02 +0100

    [#12416] Implement `IMultibyteWritePeripheral` interface in `ArrayMemory`.

--- src/Emulator/Peripherals/Peripherals/Memory/ArrayMemory.cs
+++ src/Emulator/Peripherals/Peripherals/Memory/ArrayMemory.cs
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2010-2018 Antmicro
+// Copyright (c) 2010-2019 Antmicro
 // Copyright (c) 2011-2015 Realtime Embedded
 //
 // This file is licensed under the MIT License.
@@ -14,7 +14,7 @@ using System.Collections.Generic;
 
 namespace Antmicro.Renode.Peripherals.Memory
 {
-    public class ArrayMemory : IBytePeripheral, IWordPeripheral, IDoubleWordPeripheral, IKnownSize, IMemory
+    public class ArrayMemory : IBytePeripheral, IWordPeripheral, IDoubleWordPeripheral, IKnownSize, IMemory, IMultibyteWritePeripheral
     {		
         public ArrayMemory(byte[] source)
         {
@@ -70,6 +70,18 @@ namespace Antmicro.Renode.Peripherals.Memory
         { 
             var intOffset = (int)offset;
             array[intOffset] = value;
+        }
+
+        public byte[] ReadBytes(long offset, int count)
+        {
+            var result = new byte[count];
+            Array.Copy(array, offset, result, 0, count);
+            return result;
+        }
+
+        public void WriteBytes(long offset, byte[] bytes, int startingIndex, int count)
+        {
+            Array.Copy(bytes, startingIndex, array, offset, count);
         }
 
         public long Size
