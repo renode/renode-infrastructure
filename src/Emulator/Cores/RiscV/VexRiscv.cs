89c2c863a23fad5769bdb7f43a1766f405987d7c
Mateusz Holenko <mholenko@antmicro.com>
2019-05-13 14:16:50 +0200

    [#13777] vexriscv: Allow to choose supported instruction extensions.

--- src/Emulator/Cores/RiscV/VexRiscv.cs
+++ src/Emulator/Cores/RiscV/VexRiscv.cs
@@ -14,7 +14,7 @@ namespace Antmicro.Renode.Peripherals.CPU
 {
     public partial class VexRiscv : RiscV32
     {
-        public VexRiscv(Core.Machine machine, uint hartId = 0, IRiscVTimeProvider timeProvider = null, PrivilegeArchitecture privilegeArchitecture = PrivilegeArchitecture.Priv1_09) : base(timeProvider, "rv32im", machine, hartId, privilegeArchitecture, Endianess.LittleEndian)
+        public VexRiscv(Core.Machine machine, uint hartId = 0, IRiscVTimeProvider timeProvider = null, PrivilegeArchitecture privilegeArchitecture = PrivilegeArchitecture.Priv1_09, string cpuType = "rv32im") : base(timeProvider, cpuType, machine, hartId, privilegeArchitecture, Endianess.LittleEndian)
         {
             TlibSetCsrValidation(0);
 
