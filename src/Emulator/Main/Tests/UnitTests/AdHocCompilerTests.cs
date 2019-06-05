0c8f3ae2c1bd6943c9f52abd6e2214d014a8bfd0
Maria Murach <mmurach@internships.antmicro.com>
2019-06-05 12:37:26 +0200

    [#11947] Add assembly loading tests.

---
+++ src/Emulator/Main/Tests/UnitTests/AdHocCompilerTests.cs
@@ -0,0 +1,50 @@
+﻿//
+// Copyright (c) 2010-2019 Antmicro
+//
+//  This file is licensed under the MIT License.
+//  Full license text is available in 'licenses/MIT.txt'.
+//
+using System.Linq;
+using System;
+using NUnit.Framework;
+
+namespace Antmicro.Renode.Utilities
+{
+    [TestFixture]
+    public class AdHocCompilerTests
+    {
+        [SetUp]
+        public void Init()
+        {
+            adhoc = new AdHocCompiler();
+            manager = TypeManager.Instance;
+        }
+
+        [Test]
+        public void ShouldNotThrowOnEmptyFile()
+        {
+            var extension = TemporaryFilesManager.Instance.GetTemporaryFile();
+            Assert.DoesNotThrow(() => { file = adhoc.Compile(extension); });
+            Assert.IsTrue(manager.ScanFile(file));
+        }
+
+        [Test]
+        public void ShouldCompileCsFile()
+        {
+            var extension = GetType().Assembly.FromResourceToTemporaryFile("MockExtension.cs");
+            var methodsBefore = manager.GetExtensionMethods(typeof(IInterestingType));
+            Assert.IsFalse(methodsBefore.Any(x => x.Name == "GetMockString"));
+            Assert.DoesNotThrow(() => { file = adhoc.Compile(extension); });
+            Assert.IsTrue(manager.ScanFile(file));
+            var methodsAfter = manager.GetExtensionMethods(typeof(IInterestingType));
+            Assert.IsNotEmpty(methodsAfter);
+            Assert.IsTrue(methodsAfter.Any(x => x.Name == "GetMockString"));
+            var result = manager.TryGetTypeByName("Antmicro.Renode.Utilities.MockExtension");
+            Assert.IsNotNull(result);
+        }
+
+        private AdHocCompiler adhoc;
+        private TypeManager manager;
+        private string file; 
+    }
+}
