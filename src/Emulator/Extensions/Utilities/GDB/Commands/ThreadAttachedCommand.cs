002b0ad1c47e8426796f5865078582a2c09cecc5
Michał Szprejda <mszprejda@antmicro.com>
2019-07-25 13:02:25 +0200

    [#13752] Add GDB command reporting connection to a thread.

---
+++ src/Emulator/Extensions/Utilities/GDB/Commands/ThreadAttachedCommand.cs
@@ -0,0 +1,24 @@
+﻿//
+// Copyright (c) 2010-2019 Antmicro
+//
+//  This file is licensed under the MIT License.
+//  Full license text is available in 'licenses/MIT.txt'.
+//
+using Antmicro.Renode.Utilities.GDB;
+
+namespace Antmicro.Renode.Extensions.Utilities.GDB.Commands
+{
+    public class ThreadAttachedCommand : Command, IMultithreadCommand
+    {
+        public ThreadAttachedCommand(CommandsManager manager) : base(manager)
+        {
+        }
+
+        [Execute("qAttached")]
+        public PacketData Execute()
+        {
+            // return information to gdb that it has successfully attached to an existing process
+            return new PacketData("1");
+        }
+    }
+}
