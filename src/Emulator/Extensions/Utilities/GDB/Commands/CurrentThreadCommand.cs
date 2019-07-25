8fc388781cfdf386bf63f3065cd971e6b3c4026a
Michał Szprejda <mszprejda@antmicro.com>
2019-07-25 12:57:43 +0200

    [#13752] Add GDB command returning ID of current thread.

---
+++ src/Emulator/Extensions/Utilities/GDB/Commands/CurrentThreadCommand.cs
@@ -0,0 +1,24 @@
+﻿//
+// Copyright (c) 2010-2019 Antmicro
+//
+//  This file is licensed under the MIT License.
+//  Full license text is available in 'licenses/MIT.txt'.
+//
+using System.Linq;
+using Antmicro.Renode.Utilities.GDB;
+
+namespace Antmicro.Renode.Extensions.Utilities.GDB.Commands
+{
+    public class CurrentThreadCommand : Command, IMultithreadCommand
+    {
+        public CurrentThreadCommand(CommandsManager manager) : base(manager)
+        {
+        }
+
+        [Execute("qC")]
+        public PacketData Execute()
+        {
+            return new PacketData(string.Format("QC{0}", manager.ManagedCpus.Single(x => x.Value == manager.Cpu).Key));
+        }
+    }
+}
