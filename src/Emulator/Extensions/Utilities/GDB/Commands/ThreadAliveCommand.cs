617563da594ce90ba834511b638794479f3fd952
Michał Szprejda <mszprejda@antmicro.com>
2019-07-25 13:00:37 +0200

    [#13752] Add GDB command reporting whether thread is alive.

---
+++ src/Emulator/Extensions/Utilities/GDB/Commands/ThreadAliveCommand.cs
@@ -0,0 +1,24 @@
+﻿//
+// Copyright (c) 2010-2019 Antmicro
+//
+//  This file is licensed under the MIT License.
+//  Full license text is available in 'licenses/MIT.txt'.
+//
+using Antmicro.Renode.Utilities.GDB;
+
+namespace Antmicro.Renode.Extensions.Utilities.GDB.Commands
+{
+    public class ThreadAliveCommand : Command, IMultithreadCommand
+    {
+        public ThreadAliveCommand(CommandsManager manager) : base(manager)
+        {
+        }
+
+        [Execute("T")]
+        public PacketData Execute()
+        {
+            // return information to gdb that the thread is still alive
+            return new PacketData("OK");
+        }
+    }
+}
