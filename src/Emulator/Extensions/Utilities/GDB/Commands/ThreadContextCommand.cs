c75aa4cec6d9b36794664546b7f93a5ae008a251
Michał Szprejda <mszprejda@antmicro.com>
2019-07-25 13:04:21 +0200

    [#13752] Add GDB command for switching thread context.

---
+++ src/Emulator/Extensions/Utilities/GDB/Commands/ThreadContextCommand.cs
@@ -0,0 +1,24 @@
+﻿//
+// Copyright (c) 2010-2019 Antmicro
+//
+//  This file is licensed under the MIT License.
+//  Full license text is available in 'licenses/MIT.txt'.
+//
+using Antmicro.Renode.Utilities.GDB;
+
+namespace Antmicro.Renode.Extensions.Utilities.GDB.Commands
+{
+    public class ThreadContextCommand : Command, IMultithreadCommand
+    {
+        public ThreadContextCommand(CommandsManager manager) : base(manager)
+        {
+        }
+
+        [Execute("Hg")]
+        public PacketData Execute([Argument(Encoding = ArgumentAttribute.ArgumentEncoding.HexNumber)]uint id)
+        {
+            manager.SelectCpuForDebugging(id);
+            return new PacketData("OK");
+        }
+    }
+}
