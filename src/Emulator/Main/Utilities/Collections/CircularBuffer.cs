a5c226c15a9ae550cf435fa870ccd945bf8a8d64
Piotr Zierhoffer <pzierhoffer@antmicro.com>
2019-07-26 10:57:28 +0200

    [#14721] Various CircularBuffer improvements.

--- src/Emulator/Main/Utilities/Collections/CircularBuffer.cs
+++ src/Emulator/Main/Utilities/Collections/CircularBuffer.cs
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2010-2018 Antmicro
+// Copyright (c) 2010-2019 Antmicro
 // Copyright (c) 2011-2015 Realtime Embedded
 //
 // This file is licensed under the MIT License.
@@ -16,56 +16,87 @@ namespace Antmicro.Renode.Utilities.Collections
         public CircularBuffer(int size)
         {
             buffer = new T[size];
+            Clear();
         }
 
-        public void Clear() 
+        public void Clear()
         {
-            isWrapped = false;
-            lastPosition = 0;
+            IsWrapped = false;
+            LastPosition = buffer.Length - 1;
+            FirstPosition = 0;
+            IsEmpty = true;
         }
 
         public void Enqueue(T element)
         {
-            buffer[lastPosition] = element;
-            UpdateIndex();
+            if(!IsEmpty && ((LastPosition + 1) % buffer.Length) == FirstPosition)
+            {
+                MoveFirst();
+            }
+            MoveLast();
+            buffer[LastPosition] = element;
+            IsEmpty = false;
+        }
+
+        public bool TryDequeue(out T result)
+        {
+            result = default(T);
+            if(IsEmpty)
+            {
+                return false;
+            }
+            result = buffer[FirstPosition];
+            if(FirstPosition == LastPosition)
+            {
+                IsEmpty = true;
+            }
+            MoveFirst();
+            return true;
+        }
+
+        public IEnumerable<T> DequeueAll()
+        {
+            while(TryDequeue(out var value))
+            {
+                yield return value;
+            }
         }
 
         public void CopyTo(T[] array, int arrayIndex)
         {
-            if(!isWrapped)
+            if(IsEmpty)
             {
-                Array.Copy(buffer, array, lastPosition);
                 return;
             }
-            var start = lastPosition + 1;
+            if(!IsWrapped)
+            {
+                Array.Copy(buffer, FirstPosition, array, arrayIndex, LastPosition + 1 - FirstPosition);
+                return;
+            }
+            var start = FirstPosition;
             var rightSideLength = buffer.Length - start;
             Array.Copy(buffer, start, array, arrayIndex, rightSideLength);
-            Array.Copy(buffer, 0, array, arrayIndex + rightSideLength, start - 1);
+            Array.Copy(buffer, 0, array, arrayIndex + rightSideLength, LastPosition + 1);
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            if(isWrapped)
+            if(IsEmpty)
             {
-                var end = lastPosition;
-                var currentYield = lastPosition + 1;
-                while(currentYield != end)
+                yield break;
+            }
+            var end = LastPosition;
+            var currentYield = FirstPosition;
+            do
+            {
+                yield return buffer[currentYield];
+                currentYield++;
+                if(currentYield == buffer.Length)
                 {
-                    yield return buffer[currentYield];
-                    currentYield++;
-                    if(currentYield == buffer.Length)
-                    {
-                        currentYield = 0;
-                    }
+                    currentYield = 0;
                 }
             }
-            else
-            {
-                for(var i = 0; i < lastPosition; i++)
-                {
-                    yield return buffer[i];
-                }
-            }
+            while(currentYield != (end + 1 % buffer.Length));
         }
 
         public T this[int i]
@@ -80,6 +111,16 @@ namespace Antmicro.Renode.Utilities.Collections
             }
         }
 
+        private int LastPosition { get; set; }
+        private int FirstPosition { get; set; }
+        private bool IsEmpty { get; set; }
+        public bool IsWrapped { get; set; }
+        public int Capacity => buffer.Length;
+        public int Count => IsEmpty ? 0
+            : (IsWrapped
+                ? LastPosition + buffer.Length + 1 - FirstPosition
+                : LastPosition + 1 - FirstPosition);
+
         IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
@@ -87,17 +128,28 @@ namespace Antmicro.Renode.Utilities.Collections
 
         private void UpdateIndex()
         {
-            lastPosition++;
-            if(lastPosition == buffer.Length)
+        }
+
+        private void MoveFirst()
+        {
+            FirstPosition++;
+            if(FirstPosition == buffer.Length)
             {
-                isWrapped = true;
-                lastPosition = 0;
+                IsWrapped = false;
+                FirstPosition = 0;
+            }
+        }
+        private void MoveLast()
+        {
+            LastPosition++;
+            if(LastPosition == buffer.Length)
+            {
+                IsWrapped = !IsEmpty; // usually we should isWrapped=true here, unless there is nothing in the collection. Then lastPosition should jump to 0, but there is no wrapping
+                LastPosition = 0;
             }
         }
 
         private readonly T[] buffer;
-        private int lastPosition;
-        private bool isWrapped;
     }
 }
 
