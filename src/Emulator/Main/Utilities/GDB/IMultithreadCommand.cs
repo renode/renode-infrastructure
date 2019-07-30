58375ea6a829f65332005b2db5a59d5b241d3f98
Michał Szprejda <mszprejda@antmicro.com>
2019-07-30 12:30:56 +0200

    [#13752] Add IMultithreadCommand interface.

---
+++ src/Emulator/Main/Utilities/GDB/IMultithreadCommand.cs
@@ -0,0 +1,13 @@
+﻿//
+// Copyright (c) 2010-2019 Antmicro
+//
+//  This file is licensed under the MIT License.
+//  Full license text is available in 'licenses/MIT.txt'.
+//
+
+namespace Antmicro.Renode.Utilities.GDB
+{
+    public interface IMultithreadCommand
+    {
+    }
+}
