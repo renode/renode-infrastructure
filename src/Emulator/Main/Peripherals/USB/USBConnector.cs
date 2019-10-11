0c5949a05ad50752daa674bdbe23fd9707680684
Mateusz Holenko <mholenko@antmicro.com>
2019-10-11 11:27:50 +0200

    [#16036] Add USB connector
    This external allows to connect two machines using
    a USB connection.
    
    Note that this kind of connection does not create
    a deterministic communication channel.

---
+++ src/Emulator/Main/Peripherals/USB/USBConnector.cs
@@ -0,0 +1,45 @@
+//
+// Copyright (c) 2010-2019 Antmicro
+//
+//  This file is licensed under the MIT License.
+//  Full license text is available in 'licenses/MIT.txt'.
+//
+
+using Antmicro.Renode.Core.Structure;
+
+namespace Antmicro.Renode.Core.USB
+{
+    public static class USBConnectorExtensions
+    {
+        public static void CreateUSBConnector(this Emulation emulation, string name)
+        {
+            emulation.ExternalsManager.AddExternal(new USBConnector(), name);
+        }
+    }
+
+    public class USBConnector : IExternal, IConnectable<IUSBDevice>, IUSBDevice
+    {
+        public void RegisterInController(SimpleContainer<IUSBDevice> controller, int address = 1)
+        {
+            controller.Register(dev, new NumberRegistrationPoint<int>(address));
+        }
+
+        public void AttachTo(IUSBDevice obj)
+        {
+            dev = obj;
+        }
+
+        public void DetachFrom(IUSBDevice obj)
+        {
+            dev = null;
+        }
+
+        public void Reset()
+        {
+        }
+
+        public USBDeviceCore USBCore => dev?.USBCore;
+
+        private IUSBDevice dev;
+    }
+}
