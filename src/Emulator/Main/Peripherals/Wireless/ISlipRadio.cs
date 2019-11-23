77a4a066a549316789c76a45bafa3cfd28036d07
Dawid Wojciechowski <dwojciechowski@antmicro.com>
2019-11-24 00:13:29 +0100

    [#15102] Add ISlipRadio interface

---
+++ src/Emulator/Main/Peripherals/Wireless/ISlipRadio.cs
@@ -0,0 +1,15 @@
+﻿//
+// Copyright (c) 2010-2019 Antmicro
+//
+//  This file is licensed under the MIT License.
+//  Full license text is available in 'licenses/MIT.txt'.
+//
+using System;
+using Antmicro.Renode.Core;
+
+namespace Antmicro.Renode.Peripherals.Wireless
+{
+    public interface ISlipRadio : IRadio, IExternal, IDisposable
+    {
+    }
+}
