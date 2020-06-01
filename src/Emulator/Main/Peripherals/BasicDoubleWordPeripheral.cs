d592be173f9be6ed52c215367c8fc77abc0e1adb
Mateusz Holenko <mholenko@antmicro.com>
2020-06-01 11:14:06 +0200

    [#19285] BasicDoubleWordPeripheral: Define registers in any collection
    This adds more helpers allowing to define registers, many registers
    for peripherals providing many DoubleWordRegisterCollection objects.

--- src/Emulator/Main/Peripherals/BasicDoubleWordPeripheral.cs
+++ src/Emulator/Main/Peripherals/BasicDoubleWordPeripheral.cs
@@ -53,7 +53,7 @@ namespace Antmicro.Renode.Peripherals
             DefineMany(o, p, count, setup, stepInBytes, resetValue, name);
         }
 
-        public static void DefineMany(this IConvertible o, IProvidesRegisterCollection<DoubleWordRegisterCollection> p, uint count, Action<DoubleWordRegister, int> setup, uint stepInBytes = 4, uint resetValue = 0, string name = "")
+        public static void DefineMany(this IConvertible o, DoubleWordRegisterCollection c, uint count, Action<DoubleWordRegister, int> setup, uint stepInBytes = 4, uint resetValue = 0, string name = "")
         {
             if(!o.GetType().IsEnum)
             {
@@ -63,9 +63,14 @@ namespace Antmicro.Renode.Peripherals
             var baseAddress = Convert.ToInt64(o);
             for(var i = 0; i < count; i++)
             {
-                var register = p.RegistersCollection.DefineRegister(baseAddress + i * stepInBytes, resetValue);
+                var register = c.DefineRegister(baseAddress + i * stepInBytes, resetValue);
                 setup(register, i);
             }
+        }
+
+        public static void DefineMany(this IConvertible o, IProvidesRegisterCollection<DoubleWordRegisterCollection> p, uint count, Action<DoubleWordRegister, int> setup, uint stepInBytes = 4, uint resetValue = 0, string name = "")
+        {
+            DefineMany(o, p.RegistersCollection, count, setup, stepInBytes, resetValue, name);
         }
 
         // this method it for easier use in peripherals implementing registers of different width
@@ -75,14 +80,20 @@ namespace Antmicro.Renode.Peripherals
         }
 
         // this method should be visible for enums only, but... it's not possible in C#
-        public static DoubleWordRegister Define(this IConvertible o, IProvidesRegisterCollection<DoubleWordRegisterCollection> p, uint resetValue = 0, string name = "")
+        public static DoubleWordRegister Define(this IConvertible o, DoubleWordRegisterCollection c, uint resetValue = 0, string name = "")
         {
             if(!o.GetType().IsEnum)
             {
                 throw new ArgumentException("This method should be called on enumerated type");
             }
 
-            return p.RegistersCollection.DefineRegister(Convert.ToInt64(o), resetValue);
+            return c.DefineRegister(Convert.ToInt64(o), resetValue);
+        }
+
+        // this method should be visible for enums only, but... it's not possible in C#
+        public static DoubleWordRegister Define(this IConvertible o, IProvidesRegisterCollection<DoubleWordRegisterCollection> p, uint resetValue = 0, string name = "")
+        {
+            return Define(o, p.RegistersCollection, resetValue, name);
         }
 
         public static DoubleWordRegister Bind(this IConvertible o, IProvidesRegisterCollection<DoubleWordRegisterCollection> p, DoubleWordRegister reg)
