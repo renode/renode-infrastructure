ec8570090a7a031eb6f95e3cf85548b524f42ef4
Mateusz Holenko <mholenko@antmicro.com>
2020-06-12 16:47:10 +0200

    [#19800] MPFS_I2C: Remove unused fields/registers

--- src/Emulator/Peripherals/Peripherals/I2C/MPFS_I2C.cs
+++ src/Emulator/Peripherals/Peripherals/I2C/MPFS_I2C.cs
@@ -257,11 +257,11 @@ namespace Antmicro.Renode.Peripherals.I2C
                     });
 
             Registers.Slave0Address.Define(this)
-                .WithValueField(1, 7, out slave0Address, name: "adr")
+                .WithValueField(1, 7, name: "adr")
                 .WithFlag(0, name: "gc");
 
             Registers.Slave1Address.Define(this)
-                .WithValueField(1, 7, out slave1Address, name: "adr")
+                .WithValueField(1, 7, name: "adr")
                 .WithFlag(0, name: "gc");
         }
 
@@ -286,8 +286,6 @@ namespace Antmicro.Renode.Peripherals.I2C
         private II2CPeripheral selectedSlave;
         private IFlagRegisterField serialInterruptFlag;
         private IFlagRegisterField isCoreEnabled;
-        private IValueRegisterField slave0Address;
-        private IValueRegisterField slave1Address;
         private bool pendingMasterTransaction;
         private bool isReadOperation;
         private int irqResubmitCounter;
