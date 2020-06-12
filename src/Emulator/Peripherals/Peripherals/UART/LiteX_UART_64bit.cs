0a37fe5c84f63faaa55c55fe14b16e2b414cb34c
Mateusz Holenko <mholenko@antmicro.com>
2020-06-12 16:33:37 +0200

    [#19800] LiteX_UART_64bit: Remove unused `txEventEnabled` field

--- src/Emulator/Peripherals/Peripherals/UART/LiteX_UART_64bit.cs
+++ src/Emulator/Peripherals/Peripherals/UART/LiteX_UART_64bit.cs
@@ -56,7 +56,7 @@ namespace Antmicro.Renode.Peripherals.UART
                     .WithReservedBits(0, 32) // simulating an upper half of a 64bit register, never used bits
                 },
                 {(long)Registers.EventEnable, new DoubleWordRegister(this)
-                    .WithFlag(0, out txEventEnabled)
+                    .WithFlag(0, name: "txEventEnabled")
                     .WithFlag(1, out rxEventEnabled)
                     .WithWriteCallback((_, __) => UpdateInterrupts())
                 },
@@ -139,7 +139,6 @@ namespace Antmicro.Renode.Peripherals.UART
             IRQ.Set(eventPending);
         }
 
-        private IFlagRegisterField txEventEnabled;
         private IFlagRegisterField rxEventEnabled;
         private IFlagRegisterField rxEventPending;
         private readonly DoubleWordRegisterCollection registers;
