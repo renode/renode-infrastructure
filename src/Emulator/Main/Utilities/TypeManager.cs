3b24d160c9a1648de6755441d1e3044f6a3dbf3c
Michał Szprejda <mszprejda@antmicro.com>
2020-06-23 21:26:35 +0200

    [#19777] TypeManager: Allow to decide how to handle BadImageException

--- src/Emulator/Main/Utilities/TypeManager.cs
+++ src/Emulator/Main/Utilities/TypeManager.cs
@@ -15,6 +15,7 @@ using Mono.Cecil;
 using System.Reflection;
 using System.Collections.Generic;
 using Antmicro.Renode.Plugins;
+using Antmicro.Renode.Exceptions;
 
 namespace Antmicro.Renode.Utilities
 {
@@ -265,7 +266,7 @@ namespace Antmicro.Renode.Utilities
             // TODO: case insensitive
             foreach(var assembly in Directory.GetFiles(path, "*.dll").Union(Directory.GetFiles(path, "*.exe")))
             {
-                AnalyzeAssembly(assembly);
+                AnalyzeAssembly(assembly, throwOnBadImage: false);
             }
             if(recursive)
             {
@@ -468,7 +469,7 @@ namespace Antmicro.Renode.Utilities
             return false;
         }
 
-        private bool AnalyzeAssembly(string path, bool abortOnDuplicatedAssembly = false, bool bundled = false)
+        private bool AnalyzeAssembly(string path, bool abortOnDuplicatedAssembly = false, bool bundled = false, bool throwOnBadImage = true)
         {
             Logger.LogAs(this, LogLevel.Noisy, "Analyzing assembly {0}.", path);
             if(assemblyFromAssemblyName.Values.Any(x => x.Path == path))
@@ -495,9 +496,14 @@ namespace Antmicro.Renode.Utilities
             }
             catch(BadImageFormatException)
             {
+                var message = string.Format("File {0} could not be analyzed due to invalid format.", path);
+                if(throwOnBadImage)
+                {
+                    throw new RecoverableException(message);
+                }
                 // we hush this log because it is issued in binary Windows packages - we look for DLL files, but we
                 // also bundle libgcc etc.
-                Logger.LogAs(this, LogLevel.Noisy, "File {0} could not be analyzed due to invalid format.", path);
+                Logger.LogAs(this, LogLevel.Noisy, message);
                 return false;
             }
             var assemblyName = assembly.FullName;
