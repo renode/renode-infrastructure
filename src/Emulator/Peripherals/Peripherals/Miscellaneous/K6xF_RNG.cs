6153033ed9d29008ffee3f604f6bc73c57fa3962
Mateusz Holenko <mholenko@antmicro.com>
2020-07-17 12:14:56 +0200

    K6xF_RNG: Fix formatting

--- src/Emulator/Peripherals/Peripherals/Miscellaneous/K6xF_RNG.cs
+++ src/Emulator/Peripherals/Peripherals/Miscellaneous/K6xF_RNG.cs
@@ -18,55 +18,36 @@ namespace Antmicro.Renode.Peripherals.Miscellaneous
         public K6xF_RNG(Machine machine)
         {
             IRQ = new GPIO();
+
             var registerMap = new Dictionary<long, DoubleWordRegister>
             {
                 {(long)Registers.Control, new DoubleWordRegister(this)
-                .WithReservedBits(5, 26)
-                .WithFlag(4, out sleep, name: "SLP")
-                .WithTaggedFlag("CLRI", 3)
-                .WithTaggedFlag("INTM", 2)
-                .WithTaggedFlag("HA", 1)
-                .WithFlag(0, out enable, name: "GO")
+                    .WithReservedBits(5, 26)
+                    .WithFlag(4, out sleep, name: "SLP")
+                    .WithTaggedFlag("CLRI", 3)
+                    .WithTaggedFlag("INTM", 2)
+                    .WithTaggedFlag("HA", 1)
+                    .WithFlag(0, out enable, name: "GO")
                 },
                 {(long)Registers.Status, new DoubleWordRegister(this)
-                .WithReservedBits(24, 8)
-                .WithValueField(16, 8, name: "OREG_SIZE")
-                .WithValueField(8, 8, FieldMode.Read, valueProviderCallback: _ =>
-                {
-                    if(enable.Value)
-                    {
-                        return 1u;
-                    }
-                    else
-                    {
-                        return 0u;
-                    }
-                }, name: "OREG_LVL")
-                .WithReservedBits(5, 3)
-                .WithTaggedFlag("SLP", 4)
-                .WithTaggedFlag("ERRI", 3)
-                .WithTaggedFlag("ORU", 2)
-                .WithTaggedFlag("LRS", 1)
-                .WithTaggedFlag("SECV", 0)
+                    .WithReservedBits(24, 8)
+                    .WithValueField(16, 8, name: "OREG_SIZE")
+                    .WithValueField(8, 8, FieldMode.Read, valueProviderCallback: _ => (enable.Value) ? 1u : 0u, name: "OREG_LVL")
+                    .WithReservedBits(5, 3)
+                    .WithTaggedFlag("SLP", 4)
+                    .WithTaggedFlag("ERRI", 3)
+                    .WithTaggedFlag("ORU", 2)
+                    .WithTaggedFlag("LRS", 1)
+                    .WithTaggedFlag("SECV", 0)
                 },
                 {(long)Registers.Entropy, new DoubleWordRegister(this)
-                .WithValueField(0, 32, name:"EXT_ENT")
+                    .WithValueField(0, 32, name:"EXT_ENT")
                 },
                 {(long)Registers.Output, new DoubleWordRegister(this)
-                .WithValueField(0, 32, FieldMode.Read, valueProviderCallback: _ =>
-                {
-                    if(enable.Value)
-                    {
-                        return unchecked((uint)rng.Next());
-                    }
-                    else
-                    {
-                        return 0;
-                    }
-                }, name: "RNDOUT")
-
-                },
+                    .WithValueField(0, 32, FieldMode.Read, valueProviderCallback: _ => (enable.Value) ? unchecked((uint)rng.Next()) : 0u, name: "RNDOUT")
+                }
             };
+
             registers = new DoubleWordRegisterCollection(this, registerMap);
         }
 
@@ -101,6 +82,5 @@ namespace Antmicro.Renode.Peripherals.Miscellaneous
             Entropy = 0x8,
             Output  = 0xC
         }
-
     }
 }
