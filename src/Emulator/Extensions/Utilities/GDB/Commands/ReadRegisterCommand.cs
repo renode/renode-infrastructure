56348270825ad27bdda5a37fd408ebb9e7ab88f4
Esteban Volentini <evolentini@equiser.com.ar>
2020-10-08 15:43:48 +0200

    [GH#50] ReadRegisterCommand: Handle reading of declared, but not implemented registers
    This implements returning of the properly encoded default value (0) for all registers
    that were declared in the GDB features list, but are not actually implemented in the simulation.

--- src/Emulator/Extensions/Utilities/GDB/Commands/ReadRegisterCommand.cs
+++ src/Emulator/Extensions/Utilities/GDB/Commands/ReadRegisterCommand.cs
@@ -20,17 +20,35 @@ namespace Antmicro.Renode.Utilities.GDB.Commands
         public PacketData Execute(
             [Argument(Encoding = ArgumentAttribute.ArgumentEncoding.HexNumber)]int registerNumber)
         {
-            if(!manager.Cpu.GetRegisters().Any(x => x.Index == registerNumber))
-            {
-                return PacketData.ErrorReply(1);
+            var content = new StringBuilder();
+
+            // if register exists in emulated core return current value of this
+            if(manager.Cpu.GetRegisters().Any(x => x.Index == registerNumber))
+            { 
+                foreach(var b in manager.Cpu.GetRegisterUnsafe(registerNumber).GetBytes())
+                {
+                    content.AppendFormat("{0:x2}", b);
+                }
+                return new PacketData(content.ToString());
             }
 
-            var content = new StringBuilder();
-            foreach(var b in manager.Cpu.GetRegisterUnsafe(registerNumber).GetBytes())
+            // if register no exists in emulated core but is defined in a feature
+            // return a cero value with number of bytes defined in the feature
+            var selectedRegisters = manager.Cpu.GDBFeatures.SelectMany(f => f.Registers)
+                .Where(r => r.Number == registerNumber);
+
+            if(selectedRegisters.Any())
             {
-                content.AppendFormat("{0:x2}", b);
+                for(var b = 0; b < selectedRegisters.First().Size / 8; b++)
+                {
+                    content.AppendFormat("00");
+                }
+                return new PacketData(content.ToString());
             }
-            return new PacketData(content.ToString());
+
+            // else return error
+            return PacketData.ErrorReply(1);
+
         }
     }
 }
