df577600e5825b5bca799c15e94a0c9029e65ba0
Piotr Zierhoffer <pzierhoffer@antmicro.com>
2020-11-02 03:02:53 +0100

    [#22303] Add proper error message when instantiating InterruptManager too early

--- src/Emulator/Main/Utilities/InterruptManager.cs
+++ src/Emulator/Main/Utilities/InterruptManager.cs
@@ -51,7 +51,12 @@ namespace Antmicro.Renode.Utilities
                         continue;
                     }
 
-                    var gpioField = member.GetMethod.Invoke(master, new object[0]) as IGPIO;
+                    var field = member.GetMethod.Invoke(master, new object[0]);
+                    if(field == null)
+                    {
+                        throw new ArgumentException("Trying to create the InterruptManager instance, but the IrqProvider object is not initialized");
+                    }
+                    var gpioField = field as IGPIO;
                     if(gpioField == null)
                     {
                         throw new ArgumentException("IrqProviderAttribute can only be used on properties of type IGPIO.");
