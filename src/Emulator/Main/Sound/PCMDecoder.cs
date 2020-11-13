7a9a0b0d25a4f6b5d95fe286a8d29106025a015f
Jakub Jatczak <jjatczak@antmicro.com>
2020-11-13 15:09:21 +0100

    [#22005] Add GetSingleSample method to PCMDecoder

--- src/Emulator/Main/Sound/PCMDecoder.cs
+++ src/Emulator/Main/Sound/PCMDecoder.cs
@@ -81,6 +81,11 @@ namespace Antmicro.Renode.Peripherals.Sound
             }
         }
 
+        public uint GetSingleSample()
+        {
+            return samples.TryDequeue(out var sample) ? sample : 0u;
+        }
+
         public IEnumerable<uint> GetSamplesByCount(uint samplesCountPerChannel)
         {
             lock(samples)
