86318da16b739d32c24d365d3f68f6f8b08743fa
Mateusz Holenko <mholenko@antmicro.com>
2020-11-13 15:16:40 +0100

    [#22009] NRF52840_UART: Fix handling RX stop in non-dma version

--- src/Emulator/Peripherals/Peripherals/UART/NRF52840_UART.cs
+++ src/Emulator/Peripherals/Peripherals/UART/NRF52840_UART.cs
@@ -322,7 +322,7 @@ namespace Antmicro.Renode.Peripherals.UART
 
         private void StopRx()
         {
-            if(rxAmount.Value < rxMaximumCount.Value)
+            if(easyDMA && rxAmount.Value < rxMaximumCount.Value)
             {
                 // we have not generater ENDRX yet, but it's guaranteed to appear before RXTO
                 interruptManager.SetInterrupt(Interrupts.EndReceive);
