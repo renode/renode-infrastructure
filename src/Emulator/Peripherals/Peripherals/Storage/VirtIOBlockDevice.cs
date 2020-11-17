455ce2d6e5c72bf84408de26b57bcd1e8e28b287
Marcin Jablonski <mjablonski@internships.antmicro.com>
2020-11-17 09:00:00 +0100

    [#27433] virtio: Initial stub

---
+++ src/Emulator/Peripherals/Peripherals/Storage/VirtIOBlockDevice.cs
@@ -0,0 +1,17 @@
+//
+// Copyright (c) 2010-2020 Antmicro
+//
+// This file is licensed under the MIT License.
+// Full license text is available in 'licenses/MIT.txt'.
+//
+using Antmicro.Renode.Core;
+
+namespace Antmicro.Renode.Peripherals.Storage
+{
+    public class VirtIOBlockDevice
+    {
+        public VirtIOBlockDevice()
+        {
+        } 
+    }
+}
