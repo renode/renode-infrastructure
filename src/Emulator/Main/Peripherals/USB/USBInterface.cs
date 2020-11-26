bccc7c4833170637d408f031c406eac9b55f834f
Mateusz Holenko <mholenko@antmicro.com>
2020-11-26 14:18:57 +0100

    [#22664] USBInterface: Allow to set endpoint id
    This is necessary for devices with multiple interfaces
    as endpoint ids should not overlap between them.

--- src/Emulator/Main/Peripherals/USB/USBInterface.cs
+++ src/Emulator/Main/Peripherals/USB/USBInterface.cs
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2010-2018 Antmicro
+// Copyright (c) 2010-2020 Antmicro
 //
 //  This file is licensed under the MIT License.
 //  Full license text is available in 'licenses/MIT.txt'.
@@ -38,14 +38,20 @@ namespace Antmicro.Renode.Core.USB
             return BitStream.Empty;
         }
 
-        public USBInterface WithEndpoint(Direction direction, EndpointTransferType transferType, short maximumPacketSize, byte interval, out USBEndpoint createdEndpoint)
+        public USBInterface WithEndpoint(Direction direction, EndpointTransferType transferType, short maximumPacketSize, byte interval, out USBEndpoint createdEndpoint, byte? id = null)
         {
-            if(endpoints.Count == byte.MaxValue)
+            if(!id.HasValue && endpoints.Count == byte.MaxValue)
             {
                 throw new ConstructionException("The maximal number of endpoints reached");
             }
 
-            createdEndpoint = new USBEndpoint(device, (byte)(endpoints.Count + 1), direction, transferType, maximumPacketSize, interval);
+            var localId = id ?? (byte)(endpoints.Count + 1);
+            if(endpoints.Any(x => x.Identifier == localId && x.Direction == direction))
+            {
+                throw new ConstructionException($"Endpoint with id {localId} in direction {direction} already definied");
+            }
+
+            createdEndpoint = new USBEndpoint(device, localId, direction, transferType, maximumPacketSize, interval);
             endpoints.Add(createdEndpoint);
             return this;
         }
