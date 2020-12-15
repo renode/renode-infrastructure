6b46d1169ba3812026398377cd21e0f28d18d666
Peter Gielda <pgielda@antmicro.com>
2020-12-15 16:32:39 +0100

    [#22688] Use -nostdlib if using AdHoc compiler in portable

--- src/Emulator/Main/Utilities/AdHocCompiler.cs
+++ src/Emulator/Main/Utilities/AdHocCompiler.cs
@@ -19,18 +19,17 @@ namespace Antmicro.Renode.Utilities
         {
             using(var provider = CodeDomProvider.CreateProvider("CSharp"))
             {
-                var blacklist = new List<string> { "mscorlib", "System." };
                 var outputFileName = TemporaryFilesManager.Instance.GetTemporaryFile();
                 var parameters = new CompilerParameters { GenerateInMemory = false, GenerateExecutable = false, OutputAssembly = outputFileName };
+                parameters.CompilerOptions = "";
 #if PLATFORM_LINUX
-                parameters.CompilerOptions = "/langversion:experimental";
+                parameters.CompilerOptions = parameters.CompilerOptions + " /langversion:experimental";
 #endif
                 var locations = AssemblyHelper.GetAssembliesLocations();
                 if(AssemblyHelper.BundledAssembliesCount > 0)
                 {
-                    // Assigning any non-empty string to this property prevents the compiler from referencing mscorlib.dll
-                    parameters.CoreAssemblyFileName = "some bogus string";
-                    locations = locations.Where(x => !blacklist.Any(x.Contains));
+                    // portable already has all the libs included
+                    parameters.CompilerOptions = parameters.CompilerOptions + " -nostdlib";
                 }
                 foreach(var location in locations)
                 {
