a801bf2d1f1e61d4d5f28378f4500f4e7689e0dc
Dawid Wojciechowski <dwojciechowski@antmicro.com>
2020-12-29 22:06:40 +0100

    [#21228] Add the displayImage monitor command

---
+++ src/Emulator/Extensions/UserInterface/Commands/DisplayImageCommand.cs
@@ -0,0 +1,74 @@
+﻿//
+// Copyright (c) 2010-2020 Antmicro
+//
+//  This file is licensed under the MIT License.
+//  Full license text is available in 'licenses/MIT.txt'.
+//
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using Antmicro.Renode.UserInterface.Tokenizer;
+using AntShell.Commands;
+using TermSharp.Misc;
+using Antmicro.Renode.Exceptions;
+
+namespace Antmicro.Renode.UserInterface.Commands
+{
+    public class DisplayImageCommand : AutoLoadCommand
+    {
+        public override void PrintHelp(ICommandInteraction writer)
+        {
+            base.PrintHelp(writer);
+            writer.WriteLine();
+            writer.WriteLine("Usage:");
+            writer.WriteLine($"{Name} width height");
+            writer.WriteLine("Generates testing image");
+            writer.WriteLine();
+            writer.WriteLine($"{Name} path_to_image");
+            writer.WriteLine("Supported file formats:");
+            writer.WriteLine("jpeg");
+            writer.WriteLine("png");
+        }
+
+        [Runnable]
+        public void Run(ICommandInteraction writer, PathToken pathToImage)
+        {
+            if(!File.Exists(pathToImage.Value))
+            {
+                writer.WriteError($"No such file {pathToImage.Value}");
+                return;
+            }
+
+            var image = new Bitmap(pathToImage.Value);
+            if(!ImageFormat.Jpeg.Equals(image.RawFormat) && !ImageFormat.Png.Equals(image.RawFormat))
+            {
+                writer.WriteError("Bad image format. Supported formats: jpeg, png");
+                return;
+            }
+            writer.WriteRaw(InlineImage.Encode(image));
+        }
+
+        [Runnable]
+        public void Run(ICommandInteraction writer, DecimalIntegerToken width, DecimalIntegerToken height)
+        {
+            if(width.Value <= 0 || height.Value <= 0)
+            {
+                throw new RecoverableException("Width and height must be positive values");
+            }
+
+            var image = new Bitmap((int)width.Value, (int)height.Value);
+            using(var graphics = Graphics.FromImage(image))
+            {
+                var imageSize = new Rectangle(0, 0, (int)width.Value, (int)height.Value);
+                graphics.FillRectangle(Brushes.White, imageSize);
+            }
+            writer.WriteRaw(InlineImage.Encode(image));
+        }
+
+        public DisplayImageCommand(Monitor monitor)
+            : base(monitor, "displayImage", "Displays image in Monitor")
+        {
+        }
+    }
+}
