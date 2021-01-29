d3da0e5e2ce5d702756383876b044a6119088aee
Benjamin Cabé <kartben@users.noreply.github.com>
2021-01-29 15:47:14 +0100

    Add support for blending mode.

--- src/Emulator/Main/Backends/Display/IPixelBlender.cs
+++ src/Emulator/Main/Backends/Display/IPixelBlender.cs
@@ -1,6 +1,7 @@
 //
 // Copyright (c) 2010-2018 Antmicro
 // Copyright (c) 2011-2015 Realtime Embedded
+// Copyright (c) 2020-2021 Microsoft
 //
 // This file is licensed under the MIT License.
 // Full license text is available in 'licenses/MIT.txt'.
@@ -11,12 +12,18 @@ namespace Antmicro.Renode.Backends.Display
 {
     public interface IPixelBlender
     {
-        void Blend(byte[] backBuffer, byte[] frontBuffer, ref byte[] output, Pixel background = null, byte backBufferAlphaMultiplayer = 0xFF, byte frontBufferAlphaMultiplayer = 0xFF);
-        void Blend(byte[] backBuffer, byte[] backClutBuffer, byte[] frontBuffer, byte[] frontClutBuffer, ref byte[] output, Pixel background = null, byte backBufferAlphaMultiplayer = 0xFF, byte frontBufferAlphaMultiplayer = 0xFF);
+        void Blend(byte[] backBuffer, byte[] frontBuffer, ref byte[] output, Pixel background = null, byte backBufferAlphaMultiplayer = 0xFF, PixelBlendingMode backgroundBlendingMode = PixelBlendingMode.Multiply, byte frontBufferAlphaMultiplayer = 0xFF, PixelBlendingMode foregroundBlendingMode = PixelBlendingMode.Multiply);
+        void Blend(byte[] backBuffer, byte[] backClutBuffer, byte[] frontBuffer, byte[] frontClutBuffer, ref byte[] output, Pixel background = null, byte backBufferAlphaMultiplayer = 0xFF, PixelBlendingMode bgBlendingMode = default(PixelBlendingMode), byte frontBufferAlphaMultiplayer = 0xFF, PixelBlendingMode foregroundBlendingMode = PixelBlendingMode.Multiply);
 
         PixelFormat BackBuffer { get; }
         PixelFormat FrontBuffer { get; }
         PixelFormat Output { get; }
+    }
+    public enum PixelBlendingMode
+    {
+        NoModification,
+        Replace,
+        Multiply
     }
 }
 
