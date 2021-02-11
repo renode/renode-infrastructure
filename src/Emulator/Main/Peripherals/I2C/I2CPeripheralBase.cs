f14187bec12bc4ba35761eb8e2577c0bb305925f
Mateusz Holenko <mholenko@antmicro.com>
2021-02-11 12:43:11 +0100

    [#22404] I2C: Extract the I2CPeripheralBase class

---
+++ src/Emulator/Main/Peripherals/I2C/I2CPeripheralBase.cs
@@ -0,0 +1,134 @@
+﻿//
+// Copyright (c) 2010-2021 Antmicro
+//
+//  This file is licensed under the MIT License.
+//  Full license text is available in 'licenses/MIT.txt'.
+//  
+
+// Uncomment the following line and rebuild in order to see packet dumps in the log
+// #define DEBUG_PACKETS
+
+using System;
+using Antmicro.Renode.Utilities;
+using Antmicro.Renode.Utilities.Collections;
+using Antmicro.Renode.Logging;
+using Antmicro.Renode.Core.Structure.Registers;
+
+namespace Antmicro.Renode.Peripherals.I2C
+{
+    public abstract class I2CPeripheralBase<T> : II2CPeripheral, IProvidesRegisterCollection<ByteRegisterCollection> where T : IConvertible
+    {
+        public I2CPeripheralBase(int addressLength)
+        {
+            this.addressLength = addressLength;
+
+            cache = new SimpleCache();
+            RegistersCollection = new ByteRegisterCollection(this);
+            DefineRegisters();
+
+            Reset();
+        }
+
+        public void Write(byte[] data)
+        {
+#if DEBUG_PACKETS
+            this.Log(LogLevel.Noisy, "Written {0} bytes: {1}", data.Length, Misc.PrettyPrintCollectionHex(data));
+#else
+            this.Log(LogLevel.Noisy, "Written {0} bytes", data.Length);
+#endif
+            foreach(var b in data)
+            {
+                WriteByte(b);
+            }
+        }
+
+        public byte[] Read(int count)
+        {
+            var result = RegistersCollection.Read(address);
+            this.NoisyLog("Reading register {0} (0x{1:X}) from device: 0x{2:X}", cache.Get(address, x => Enum.GetName(typeof(T), x)), address, result);
+
+            return new byte [] { result };
+        }
+
+        public void FinishTransmission()
+        {
+            this.NoisyLog("Finishing transmission, going to the idle state");
+            ResetState();
+        }
+
+        public virtual void Reset()
+        {
+            ResetState();
+        }
+
+        public ByteRegisterCollection RegistersCollection { get; }
+
+        protected abstract void DefineRegisters();
+
+        private void ResetState()
+        {
+            // clearing of the address variable itself
+            // is deferred to the moment when
+            // it will be overwritten in WriteByte;
+            // this way ReadByte method can access it
+            addressBitsLeft = addressLength; 
+            state = State.CollectingAddress;
+        }
+
+        private void WriteByte(byte b)
+        {
+            switch(state)
+            {
+                case State.CollectingAddress:
+                {
+                    if(addressBitsLeft == addressLength)
+                    {
+                        // if this is the first write after
+                        // resetting state, clear the address
+                        // to make sure there are no stale bits
+                        // (address can be shorter than 8-bits)
+                        address = 0;
+                    }
+
+                    // address length is configurable to any number of bits,
+                    // so we need to handle all possible cases - including
+                    // values non-divisable by 8
+                    var position = Math.Max(0, addressBitsLeft - 8);
+                    var width = Math.Min(8, addressBitsLeft);
+
+                    address = BitHelper.SetBitsFrom(address, b, position, width);
+                    addressBitsLeft -= width;
+
+                    if(addressBitsLeft == 0)
+                    {
+                        this.Log(LogLevel.Noisy, "Setting register address to {0} (0x{1:X})", cache.Get(address, x => Enum.GetName(typeof(T), x)), address);
+                        state = State.Processing;
+                    }
+                    break;
+                }
+
+                case State.Processing:
+                    this.Log(LogLevel.Noisy, "Writing value 0x{0:X} to register {1} (0x{2:X})", b, cache.Get(address, x => Enum.GetName(typeof(T), x)), address);
+                    RegistersCollection.Write(address, b);
+                    break;
+
+                default:
+                    throw new ArgumentException($"Unexpected state: {state}");
+            }
+        }
+
+        private int addressBitsLeft;
+
+        private uint address;
+        private State state;
+
+        private readonly int addressLength;
+        private readonly SimpleCache cache;
+
+        private enum State
+        {
+            CollectingAddress,
+            Processing
+        }
+    }
+}
--- src/Emulator/Peripherals/Peripherals/Sensors/ST_I2CSensorBase.cs
+++ src/Emulator/Peripherals/Peripherals/Sensors/ST_I2CSensorBase.cs
@@ -1,5 +1,5 @@
 ﻿//
-// Copyright (c) 2010-2020 Antmicro
+// Copyright (c) 2010-2021 Antmicro
 //
 //  This file is licensed under the MIT License.
 //  Full license text is available in 'licenses/MIT.txt'.
@@ -20,44 +20,11 @@ namespace Antmicro.Renode.Peripherals.Sensors
     // support 7-bit-length registers addresses.
 
     // it should be enum instead of IConvertible, but earlier versions of C# do not support this
-    public abstract class ST_I2CSensorBase<T> : II2CPeripheral, IProvidesRegisterCollection<ByteRegisterCollection> where T : IConvertible
+    public abstract class ST_I2CSensorBase<T> : I2CPeripheralBase<T> where T : IConvertible
     {
-        public ST_I2CSensorBase()
+        public ST_I2CSensorBase() : base(7)
         {
-            RegistersCollection = new ByteRegisterCollection(this);
-            DefineRegisters();
         }
-
-        public void Write(byte[] data)
-        {
-            this.Log(LogLevel.Noisy, "Written {0} bytes: {1}", data.Length, Misc.PrettyPrintCollectionHex(data));
-            foreach(var b in data)
-            {
-                WriteByte(b);
-            }
-        }
-
-        public byte[] Read(int count)
-        {
-            var result = RegistersCollection.Read(address);
-            this.NoisyLog("Reading register {1} (0x{1:X}) from device: 0x{0:X}", result, Enum.GetName(typeof(T), address));
-
-            return new byte [] { result };
-        }
-
-        public void FinishTransmission()
-        {
-            this.NoisyLog("Finishing transmission, going to the Idle state");
-            state = State.Idle;
-        }
-
-        public virtual void Reset()
-        {
-            address = 0;
-            state = State.Idle;
-        }
-
-        public ByteRegisterCollection RegistersCollection { get; }
 
         protected byte GetScaledValue(decimal value, short sensitivity, bool upperByte)
         {
@@ -71,37 +38,6 @@ namespace Antmicro.Renode.Peripherals.Sensors
         {
             var range = maxVal - minVal;
             return (short)(((1 << width) / range) - 1);
-        }
-
-        protected abstract void DefineRegisters();
-
-        private void WriteByte(byte b)
-        {
-            switch(state)
-            {
-                case State.Idle:
-                    address = BitHelper.GetValue(b, offset: 0, size: 7);
-                    this.Log(LogLevel.Noisy, "Setting register address to {0} (0x{0:X})", Enum.GetName(typeof(T), address));
-                    state = State.Processing;
-                    break;
-
-                case State.Processing:
-                    this.Log(LogLevel.Noisy, "Writing value 0x{0:X} to register {1} (0x{1:X})", b, Enum.GetName(typeof(T), address));
-                    RegistersCollection.Write(address, b);
-                    break;
-
-                default:
-                    throw new ArgumentException($"Unexpected state: {state}");
-            }
-        }
-
-        private byte address;
-        private State state;
-
-        private enum State
-        {
-            Idle,
-            Processing
         }
     }
 }
