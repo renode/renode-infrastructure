92426ac381dfdce456ee43885507cc0efeb9b426
Mateusz Holenko <mholenko@antmicro.com>
2021-03-02 16:07:54 +0100

    [#24742] RegisterValue: Add helper for manual creation of RegisterValue structs

--- src/Emulator/Main/Peripherals/CPU/RegisterValue.cs
+++ src/Emulator/Main/Peripherals/CPU/RegisterValue.cs
@@ -1,5 +1,5 @@
 ﻿//
-// Copyright (c) 2010-2018 Antmicro
+// Copyright (c) 2010-2021 Antmicro
 //
 //  This file is licensed under the MIT License.
 //  Full license text is available in 'licenses/MIT.txt'.
@@ -92,6 +92,11 @@ namespace Antmicro.Renode.Peripherals.CPU
             }
 
             return (Byte)v.RawValue;
+        }
+
+        public static RegisterValue Create(ulong rawValue, uint bits)
+        {
+            return new RegisterValue { RawValue = rawValue, Bits = bits };
         }
     }
 }
