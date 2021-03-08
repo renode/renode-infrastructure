9abe6e4bccb5f13c1016f0426168c92ad3d9c69f
Mateusz Holenko <mholenko@antmicro.com>
2021-03-08 16:23:29 +0100

    [#24887] OpenTitan_VerilatorSwTestStatus: Fix formatting

--- src/Emulator/Peripherals/Peripherals/Miscellaneous/OpenTitan_VerilatorSwTestStatus.cs
+++ src/Emulator/Peripherals/Peripherals/Miscellaneous/OpenTitan_VerilatorSwTestStatus.cs
@@ -8,64 +8,64 @@ using Antmicro.Renode.Core;
 using Antmicro.Renode.Core.Structure.Registers;
 using Antmicro.Renode.Logging;
 
-
 namespace Antmicro.Renode.Peripherals.Miscellaneous
 {
     public class OpenTitan_VerilatorSwTestStatus : BasicDoubleWordPeripheral, IKnownSize
     {
         public OpenTitan_VerilatorSwTestStatus(Machine machine) : base(machine)
         {
-                DefineRegisters();
+            DefineRegisters();
         }
 
         public long Size =>  0x8;
 
         private void DefineRegisters()
         {
-                Registers.SoftwareTestStatus.Define(this, 0x0)
-                    .WithValueField(0, 16, name: "software test status", writeCallback: (_, value) =>
+            Registers.SoftwareTestStatus.Define(this, 0x0)
+                .WithValueField(0, 16, name: "software test status", writeCallback: (_, value) =>
+                {
+                    this.Log(LogLevel.Info, "Opentitan Software test status set to 0x{0:x}", value);
+                    bool enumTestStatisIsDefined = System.Enum.IsDefined(typeof(SoftwareTestStatusCode), value);
+                    if(enumTestStatisIsDefined)
                     {
-                        this.Log(LogLevel.Info, "Opentitan Software test status set to 0x{0:x}", value);
-                        bool enumTestStatisIsDefined = System.Enum.IsDefined(typeof(SoftwareTestStatusCode), value);
-                        if (enumTestStatisIsDefined) {
-                                SoftwareTestStatusCode statusCode = (SoftwareTestStatusCode)value;
-                                switch (statusCode) {
-                                        case SoftwareTestStatusCode.kTestStatusInBootRom: 
-                                                this.Log(LogLevel.Info, "Opentitan in boot ROM");
-                                                break;
-                                        case SoftwareTestStatusCode.kTestStatusInTest:
-                                                this.Log(LogLevel.Info, "Opentitan in test");
-                                                break;
-                                        case SoftwareTestStatusCode.kTestStatusInWfi:
-                                                this.Log(LogLevel.Info, "Opentitan in WFI");
-                                                break;
-                                        case SoftwareTestStatusCode.kTestStatusPassed:
-                                                this.Log(LogLevel.Info, "Opentitan PASSED Test");
-                                                break;
-                                        case SoftwareTestStatusCode.kTestStatusFailed:
-                                                this.Log(LogLevel.Info, "Opentitan FAILED Test");
-                                                break;
-                                }
+                        SoftwareTestStatusCode statusCode = (SoftwareTestStatusCode)value;
+                        switch(statusCode)
+                        {
+                            case SoftwareTestStatusCode.kTestStatusInBootRom: 
+                                this.Log(LogLevel.Info, "Opentitan in boot ROM");
+                                break;
+                            case SoftwareTestStatusCode.kTestStatusInTest:
+                                this.Log(LogLevel.Info, "Opentitan in test");
+                                break;
+                            case SoftwareTestStatusCode.kTestStatusInWfi:
+                                this.Log(LogLevel.Info, "Opentitan in WFI");
+                                break;
+                            case SoftwareTestStatusCode.kTestStatusPassed:
+                                this.Log(LogLevel.Info, "Opentitan PASSED Test");
+                                break;
+                            case SoftwareTestStatusCode.kTestStatusFailed:
+                                this.Log(LogLevel.Info, "Opentitan FAILED Test");
+                                break;
                         }
-                    })
-                    .WithIgnoredBits(16,16)
-                ;
+                    }
+                })
+                .WithIgnoredBits(16,16)
+            ;
         }
 
         private enum SoftwareTestStatusCode : uint
         {
-                kTestStatusDefault = 0x0000,
-                kTestStatusInBootRom = 0xb090,  // 'bogo', BOotrom GO
-                kTestStatusInTest = 0x4354,  // 'test'
-                kTestStatusInWfi = 0x1d1e,  // 'idle'
-                kTestStatusPassed = 0x900d,  // 'good'
-                kTestStatusFailed = 0xbaad  // 'baad'
+            kTestStatusDefault = 0x0000,
+            kTestStatusInBootRom = 0xb090,  // 'bogo', BOotrom GO
+            kTestStatusInTest = 0x4354,  // 'test'
+            kTestStatusInWfi = 0x1d1e,  // 'idle'
+            kTestStatusPassed = 0x900d,  // 'good'
+            kTestStatusFailed = 0xbaad  // 'baad'
         }
 
         private enum Registers
         {
             SoftwareTestStatus = 0x0
         }
-
     }
 }
