61d40dd1370986e4e9af57f43f89423927f5a9f3
Mateusz Holenko <mholenko@antmicro.com>
2021-03-18 14:33:59 +0100

    [#25232] Add LiteX SoC Controller model in 32-bit CSR configuration

---
+++ src/Emulator/Peripherals/Peripherals/Miscellaneous/LiteX_SoC_Controller_CSR32.cs
@@ -0,0 +1,45 @@
+//
+// Copyright (c) 2010-2021 Antmicro
+//
+// This file is licensed under the MIT License.
+// Full license text is available in 'licenses/MIT.txt'.
+//
+
+using Antmicro.Renode.Core;
+using Antmicro.Renode.Core.Structure.Registers;
+
+namespace Antmicro.Renode.Peripherals.Miscellaneous
+{
+    public class LiteX_SoC_Controller_CSR32 : BasicDoubleWordPeripheral, IKnownSize
+    {
+        public LiteX_SoC_Controller_CSR32(Machine machine) : base(machine)
+        {
+            DefineRegisters();
+        }
+
+        public long Size => 0xC;
+
+        private void DefineRegisters()
+        {
+            Registers.Reset.Define(this)
+                .WithTag("reset", 0, 1)
+                .WithReservedBits(1, 31)
+            ;
+
+            Registers.Scratch.Define(this, 0x12345678)
+                .WithValueField(0, 32, name: "scratch")
+            ;
+
+            Registers.BusErrors.Define(this)
+                .WithTag("bus errors", 0, 32)
+            ;
+        }
+
+        private enum Registers
+        {
+            Reset = 0x0,
+            Scratch = 0x4,
+            BusErrors = 0x8,
+        }
+    }
+}
