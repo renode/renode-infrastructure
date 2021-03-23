cb15671f5849ff07bd78cf1edeb187c1c60f6628
Benjamin Cabé <kartben@users.noreply.github.com>
2021-03-23 10:37:29 +0100

    [GH#31] Add PixelBlendingMode concept

--- src/Emulator/Main/Backends/Display/IPixelBlender.cs
+++ src/Emulator/Main/Backends/Display/IPixelBlender.cs
@@ -1,6 +1,7 @@
 //
-// Copyright (c) 2010-2018 Antmicro
+// Copyright (c) 2010-2021 Antmicro
 // Copyright (c) 2011-2015 Realtime Embedded
+// Copyright (c) 2020-2021 Microsoft
 //
 // This file is licensed under the MIT License.
 // Full license text is available in 'licenses/MIT.txt'.
@@ -17,6 +18,13 @@ namespace Antmicro.Renode.Backends.Display
         PixelFormat BackBuffer { get; }
         PixelFormat FrontBuffer { get; }
         PixelFormat Output { get; }
+    }
+
+    public enum PixelBlendingMode
+    {
+        NoModification,
+        Replace,
+        Multiply
     }
 }
 
