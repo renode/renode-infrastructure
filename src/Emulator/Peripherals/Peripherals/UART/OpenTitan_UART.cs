07e07ade3e565e6797c024fb72991cd03740bc0f
Henry Herman <henryherman@google.com>
2021-03-24 20:13:09 +0000

    OpenTitan_UART: Improve tx_empty logic to only assert when transmit not ongoing.
    Change-Id: I9b242c7b0f19c07b280cec19056b9b30a94b2a4a

--- src/Emulator/Peripherals/Peripherals/UART/OpenTitan_UART.cs
+++ src/Emulator/Peripherals/Peripherals/UART/OpenTitan_UART.cs
@@ -26,6 +26,8 @@ namespace Antmicro.Renode.Peripherals.UART
             RxTimeoutIRQ = new GPIO();
             RxParityErrorIRQ = new GPIO();
 
+            tx_ongoing = false;
+
             registers = new DoubleWordRegisterCollection(this, BuildRegisterMap());
             txQueue = new Queue<byte>();
         }
@@ -44,6 +46,7 @@ namespace Antmicro.Renode.Peripherals.UART
         {
             if(lineLoopbackEnabled.Value)
             {
+                tx_ongoing = true;
                 TransmitCharacter(value);
                 this.Log(LogLevel.Noisy, "Line Loopback Enabled, byte echoed by hardware.");
             }
@@ -156,6 +159,7 @@ namespace Antmicro.Renode.Peripherals.UART
                             {
                                 foreach(byte value in txQueue)
                                 {
+                                    tx_ongoing = true;
                                     TransmitCharacter(value);
                                 }
                             }
@@ -206,6 +210,7 @@ namespace Antmicro.Renode.Peripherals.UART
 
                         if(txEnabled.Value)
                         {
+                            tx_ongoing = true;
                             TransmitCharacter((byte)val);
                         }
                         else if(txQueue.Count < txFIFOCapacity)
@@ -268,8 +273,12 @@ namespace Antmicro.Renode.Peripherals.UART
         {
             txWatermarkPending.Value |= TxWatermarkValue > txQueue.Count;
             rxWatermarkPending.Value |= RxWatermarkValue <= Count;
-            txEmptyPending.Value |= txQueue.Count == 0;
-
+            if (tx_ongoing) {
+                if (txQueue.Count == 0) {
+                    tx_ongoing = false;    
+                }
+                txEmptyPending.Value |= txQueue.Count == 0;
+            }
             TxWatermarkIRQ.Set(txWatermarkPending.Value && txWatermarkEnabled.Value);
             RxWatermarkIRQ.Set(rxWatermarkPending.Value && rxWatermarkEnabled.Value);
             TxEmptyIRQ.Set(txEmptyPending.Value && txEmptyEnabled.Value);
@@ -360,6 +369,8 @@ namespace Antmicro.Renode.Peripherals.UART
         private const int rxFIFOCapacity = 32;
         private const int txFIFOCapacity = 32;
         private const ulong fixedClockFrequency = 50000000;
+
+        private bool tx_ongoing;
 
         private enum ParityType
         {
