ba70282a1165a98caac68780968aa29c22215071
Mateusz Holenko <mholenko@antmicro.com>
2021-06-16 12:30:27 +0200

    [GH#194] STM32_ADC: Refactor

--- src/Emulator/Peripherals/Peripherals/Analog/STM32_ADC.cs
+++ src/Emulator/Peripherals/Peripherals/Analog/STM32_ADC.cs
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2010-2020 Antmicro
+// Copyright (c) 2010-2021 Antmicro
 //
 //  This file is licensed under the MIT License.
 //  Full license text is available in 'licenses/MIT.txt'.
@@ -42,17 +42,15 @@ namespace Antmicro.Renode.Peripherals.Analog
    // * Multi-ADC (i.e. Dual/Triple) mode
    public class STM32_ADC : BasicDoubleWordPeripheral, IKnownSize
    {
-      public const int NUM_CHANNELS = 19;
-
       public STM32_ADC(Machine machine) : base(machine)
       {
-         channels = Enumerable.Range(0, NUM_CHANNELS).Select(x => new ADCChannel(this, x)).ToArray();
+         channels = Enumerable.Range(0, NumberOfChannels).Select(x => new ADCChannel(this, x)).ToArray();
 
          DefineRegisters();
 
          // Sampling time fixed
          samplingTimer = new LimitTimer(
-               machine.ClockSource, 1000000, this, String.Format("samplingClock"),
+               machine.ClockSource, 1000000, this, "samplingClock",
                limit: 100,
                eventEnabled: true,
                direction: Direction.Ascending,
@@ -64,7 +62,7 @@ namespace Antmicro.Renode.Peripherals.Analog
 
       public void FeedSample(uint value, uint channelIdx, int repeat = 1)
       {
-         if (IsValidChannel(channelIdx))
+         if(IsValidChannel(channelIdx))
          {
             channels[channelIdx].FeedSample(value, repeat);
          }
@@ -72,7 +70,7 @@ namespace Antmicro.Renode.Peripherals.Analog
 
       public void FeedSample(string path, uint channelIdx, int repeat = 1)
       {
-         if (IsValidChannel(channelIdx))
+         if(IsValidChannel(channelIdx))
          {
             var parsedSamples = ADCChannel.ParseSamplesFile(path);
             channels[channelIdx].FeedSample(parsedSamples, repeat);
@@ -81,20 +79,16 @@ namespace Antmicro.Renode.Peripherals.Analog
 
       private bool IsValidChannel(uint channelIdx)
       {
-         bool ret = false;
-         if (channelIdx < NUM_CHANNELS)
-         {
-            ret = true;
-         }
-         else
+         if(channelIdx >= NumberOfChannels)
          {
             throw new RecoverableException("Only channels 0/1 are supported");
          }
-         return ret;
+         return true;
       }
 
       public override void Reset()
       {
+         base.Reset();
          foreach(var c in channels)
          {
             c.Reset();
@@ -114,7 +108,8 @@ namespace Antmicro.Renode.Peripherals.Analog
             .WithTaggedFlag("Injected channel end of conversion", 2)
             .WithTaggedFlag("Injected channel start flag", 3)
             .WithTaggedFlag("Regular channel start flag", 4)
-            .WithTaggedFlag("Overrun", 5);
+            .WithTaggedFlag("Overrun", 5)
+            .WithReservedBits(6, 26);
 
          Registers.Control1.Define(this)
             .WithTag("Analog watchdog channel select bits", 0, 5)
@@ -137,7 +132,7 @@ namespace Antmicro.Renode.Peripherals.Analog
          Registers.Control2.Define(this, name: "Control2")
             .WithFlag(0, out adcOn,
                   name: "A/D Converter ON/OFF",
-                  writeCallback: (oldVal, val) => {if (oldVal != val) {EnableADC(val);}})
+                  changeCallback: (_, val) => { if(val) { EnableADC(); }})
             .WithFlag(1, out continuousConversion, name: "Continous conversion")
             .WithReservedBits(2, 6)
             .WithFlag(8, out dmaEnabled, name: "Direct memory access mode")
@@ -153,128 +148,115 @@ namespace Antmicro.Renode.Peripherals.Analog
             .WithTag("External trigger enable for regular channels", 28, 2)
             .WithFlag(30,
                   name: "Start Conversion Of Regular Channels",
-                  writeCallback: (_, value) => StartConversion(value),
+                  writeCallback: (_, value) => { if(value) StartConversion(); },
                   valueProviderCallback: _ => false)
             .WithReservedBits(31, 1);
 
          Registers.SampleTime1.Define(this)
-            .WithReservedBits(27, 5)
-            .WithTag("Channel 18 sampling time", 24, 3)
-            .WithTag("Channel 17 sampling time", 21, 3)
-            .WithTag("Channel 16 sampling time", 18, 3)
-            .WithTag("Channel 15 sampling time", 15, 3)
-            .WithTag("Channel 14 sampling time", 12, 3)
-            .WithTag("Channel 13 sampling time", 9, 3)
-            .WithTag("Channel 12 sampling time", 6, 3)
+            .WithTag("Channel 10 sampling time", 0, 3)
             .WithTag("Channel 11 sampling time", 3, 3)
-            .WithTag("Channel 10 sampling time", 0, 3);
+            .WithTag("Channel 12 sampling time", 6, 3)
+            .WithTag("Channel 13 sampling time", 9, 3)
+            .WithTag("Channel 14 sampling time", 12, 3)
+            .WithTag("Channel 15 sampling time", 15, 3)
+            .WithTag("Channel 16 sampling time", 18, 3)
+            .WithTag("Channel 17 sampling time", 21, 3)
+            .WithTag("Channel 18 sampling time", 24, 3)
+            .WithReservedBits(27, 5);
 
          Registers.SampleTime2.Define(this)
-            .WithReservedBits(30, 2)
-            .WithTag("Channel 9 sampling time", 27, 3)
-            .WithTag("Channel 8 sampling time", 24, 3)
-            .WithTag("Channel 7 sampling time", 21, 3)
-            .WithTag("Channel 6 sampling time", 18, 3)
-            .WithTag("Channel 5 sampling time", 15, 3)
-            .WithTag("Channel 4 sampling time", 12, 3)
-            .WithTag("Channel 3 sampling time", 9, 3)
-            .WithTag("Channel 2 sampling time", 6, 3)
+            .WithTag("Channel 0 sampling time", 0, 3)
             .WithTag("Channel 1 sampling time", 3, 3)
-            .WithTag("Channel 0 sampling time", 0, 3);
+            .WithTag("Channel 2 sampling time", 6, 3)
+            .WithTag("Channel 3 sampling time", 9, 3)
+            .WithTag("Channel 4 sampling time", 12, 3)
+            .WithTag("Channel 5 sampling time", 15, 3)
+            .WithTag("Channel 6 sampling time", 18, 3)
+            .WithTag("Channel 7 sampling time", 21, 3)
+            .WithTag("Channel 8 sampling time", 24, 3)
+            .WithTag("Channel 9 sampling time", 27, 3)
+            .WithReservedBits(30, 2);
 
          Registers.InjectedChannelDataOffset1.Define(this)
-            .WithReservedBits(12, 20)
-            .WithTag("Data offset for injected channel 1", 0, 12);
+            .WithTag("Data offset for injected channel 1", 0, 12)
+            .WithReservedBits(12, 20);
          Registers.InjectedChannelDataOffset2.Define(this)
-            .WithReservedBits(12, 20)
-            .WithTag("Data offset for injected channel 2", 0, 12);
+            .WithTag("Data offset for injected channel 2", 0, 12)
+            .WithReservedBits(12, 20);
          Registers.InjectedChannelDataOffset3.Define(this)
-            .WithReservedBits(12, 20)
-            .WithTag("Data offset for injected channel 3", 0, 12);
+            .WithTag("Data offset for injected channel 3", 0, 12)
+            .WithReservedBits(12, 20);
          Registers.InjectedChannelDataOffset4.Define(this)
-            .WithReservedBits(12, 20)
-            .WithTag("Data offset for injected channel 4", 0, 12);
+            .WithTag("Data offset for injected channel 4", 0, 12)
+            .WithReservedBits(12, 20);
 
          Registers.RegularSequence1.Define(this)
-            .WithValueField(20, 4, writeCallback: (_,val) => { regularSequenceLen = val + 1; }, name: "Regular channel sequence length")
-            .WithValueField(15, 5, out regularSequence[15], name: "16th conversion in regular sequence")
-            .WithValueField(10, 5, out regularSequence[14], name: "15th conversion in regular sequence")
+            .WithValueField(0, 5, out regularSequence[12], name: "13th conversion in regular sequence")
             .WithValueField(5, 5, out regularSequence[13], name: "14th conversion in regular sequence")
-            .WithValueField(0, 5, out regularSequence[12], name: "13th conversion in regular sequence");
+            .WithValueField(10, 5, out regularSequence[14], name: "15th conversion in regular sequence")
+            .WithValueField(15, 5, out regularSequence[15], name: "16th conversion in regular sequence")
+            .WithValueField(20, 4, writeCallback: (_, val) => { regularSequenceLen = val + 1; }, name: "Regular channel sequence length");
 
          Registers.RegularSequence2.Define(this)
-            .WithValueField(25, 5, out regularSequence[11], name: "12th conversion in regular sequence")
-            .WithValueField(20, 5, out regularSequence[10], name: "11th conversion in regular sequence")
-            .WithValueField(15, 5, out regularSequence[9], name: "10th conversion in regular sequence")
-            .WithValueField(10, 5, out regularSequence[8], name: "9th conversion in regular sequence")
+            .WithValueField(0, 5, out regularSequence[6], name: "7th conversion in regular sequence")
             .WithValueField(5, 5, out regularSequence[7], name: "8th conversion in regular sequence")
-            .WithValueField(0, 5, out regularSequence[6], name: "7th conversion in regular sequence");
+            .WithValueField(10, 5, out regularSequence[8], name: "9th conversion in regular sequence")
+            .WithValueField(15, 5, out regularSequence[9], name: "10th conversion in regular sequence")
+            .WithValueField(20, 5, out regularSequence[10], name: "11th conversion in regular sequence")
+            .WithValueField(25, 5, out regularSequence[11], name: "12th conversion in regular sequence");
 
          Registers.RegularSequence3.Define(this)
-            .WithValueField(25, 5, out regularSequence[5], name: "6th conversion in regular sequence")
-            .WithValueField(20, 5, out regularSequence[4], name: "5th conversion in regular sequence")
-            .WithValueField(15, 5, out regularSequence[3], name: "4th conversion in regular sequence")
-            .WithValueField(10, 5, out regularSequence[2], name: "3rd conversion in regular sequence")
+            .WithValueField(0, 5, out regularSequence[0], name: "1st conversion in regular sequence")
             .WithValueField(5, 5, out regularSequence[1], name: "2nd conversion in regular sequence")
-            .WithValueField(0, 5, out regularSequence[0], name: "1st conversion in regular sequence");
+            .WithValueField(10, 5, out regularSequence[2], name: "3rd conversion in regular sequence")
+            .WithValueField(15, 5, out regularSequence[3], name: "4th conversion in regular sequence")
+            .WithValueField(20, 5, out regularSequence[4], name: "5th conversion in regular sequence")
+            .WithValueField(25, 5, out regularSequence[5], name: "6th conversion in regular sequence");
 
          // Data register
          Registers.RegularData.Define(this)
             .WithValueField(0, 32,
-                  valueProviderCallback: (_) => {
-                  this.Log(LogLevel.Info, "Reading ADC data {0}", adcData);
-                  return adcData;
-                  })
-         .WithReadCallback((_,__) => ReadData());
+                  valueProviderCallback: _ =>
+                  {
+                      this.Log(LogLevel.Debug, "Reading ADC data {0}", adcData);
+                      // Reading ADC_DR should clear EOC
+                      endOfConversion.Value = false;
+                      IRQ.Set(false);
+                      return adcData;
+                  });
       }
 
-      private void ReadData()
+      private void EnableADC()
       {
-         // Reading ADC_DR should clear EOC
-         endOfConversion.Value = false;
-         IRQ.Set(false);
+          currentChannel = channels[regularSequence[currentChannelIdx].Value];
       }
 
-      private void EnableADC(bool on)
+      private void StartConversion()
       {
-         if (on)
+         if(adcOn.Value)
          {
-            currentChannel = channels[regularSequence[currentChannelIdx].Value];
-         }
-      }
+             this.Log(LogLevel.Debug, "Starting conversion time={0}",
+                   machine.ElapsedVirtualTime.TimeElapsed);
 
-      private void StartConversion(bool start)
-      {
-         if (adcOn.Value)
-         {
-            if (start)
-            {
-               this.Log(LogLevel.Info, "Starting conversion time={0}",
-                     machine.ElapsedVirtualTime.TimeElapsed);
-
-               // Enable timer, which will simulate conversion being performed.
-               samplingTimer.Enabled = true;
-            }
+             // Enable timer, which will simulate conversion being performed.
+             samplingTimer.Enabled = true;
          }
          else
          {
-            if (start)
-            {
-               this.Log(LogLevel.Warning, "Trying to start conversion while ADC off");
-            }
+             this.Log(LogLevel.Warning, "Trying to start conversion while ADC off");
          }
       }
 
       private void OnConversionFinished()
       {
-         this.Log(LogLevel.Info, "OnConversionFinished: time={0} channel={1}",
+         this.Log(LogLevel.Debug, "OnConversionFinished: time={0} channel={1}",
                machine.ElapsedVirtualTime.TimeElapsed,
                currentChannelIdx);
 
          // Set data register and trigger DMA request
          currentChannel.PrepareSample();
          adcData = currentChannel.GetSample();
-         if (dmaEnabled.Value && dmaIssueRequest.Value)
+         if(dmaEnabled.Value && dmaIssueRequest.Value)
          {
             // Issue DMA peripheral request, which when mapped to DMA
             // controller will trigger a peripheral to memory transfer
@@ -282,8 +264,8 @@ namespace Antmicro.Renode.Peripherals.Analog
             DMARequest.Unset();
          }
 
-         bool scanModeActive = scanMode.Value && currentChannelIdx < regularSequenceLen - 1;
-         bool scanModeFinished = scanMode.Value && currentChannelIdx == regularSequenceLen - 1;
+         var scanModeActive = scanMode.Value && currentChannelIdx < regularSequenceLen - 1;
+         var scanModeFinished = scanMode.Value && currentChannelIdx == regularSequenceLen - 1;
 
          // Signal EOC if EOCS set or we finished scanning regular group
          endOfConversion.Value = endOfConversionSelect.Value || scanModeFinished;
@@ -296,9 +278,9 @@ namespace Antmicro.Renode.Peripherals.Analog
          samplingTimer.Enabled = scanModeActive || continuousConversion.Value;
 
          // Trigger EOC interrupt
-         if (endOfConversion.Value && eocInterruptEnable.Value)
+         if(endOfConversion.Value && eocInterruptEnable.Value)
          {
-            this.Log(LogLevel.Info, "OnConversionFinished: Set IRQ");
+            this.Log(LogLevel.Debug, "OnConversionFinished: Set IRQ");
             IRQ.Set(true);
          }
       }
@@ -316,20 +298,22 @@ namespace Antmicro.Renode.Peripherals.Analog
 
       // Sampling timer. Provides time-based event for driving conversion of
       // regular channel sequence.
-      private LimitTimer samplingTimer;
+      private readonly LimitTimer samplingTimer;
 
       // Data sample to be returned from data register when read.
-      private uint adcData = 0;
+      private uint adcData;
 
       // Regular sequence settings, i.e. the channels and order of channels
       // for performing conversion
       private uint regularSequenceLen;
-      private IValueRegisterField[] regularSequence = new IValueRegisterField[19];
+      private readonly IValueRegisterField[] regularSequence = new IValueRegisterField[19];
 
       // Channel objects, for managing input test data
-      private uint currentChannelIdx = 0;
+      private uint currentChannelIdx;
       private ADCChannel currentChannel;
       private readonly ADCChannel[] channels;
+
+      public const int NumberOfChannels = 19;
 
       private enum Registers
       {
