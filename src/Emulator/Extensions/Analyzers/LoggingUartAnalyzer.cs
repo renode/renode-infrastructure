1aeb262fec631d2129a3c45f58cdb2e65fba139b
Mateusz Holenko <mholenko@antmicro.com>
2021-09-06 13:10:19 +0200

    [#28541] LoggingUartAnalyzer: Control timestamp format and visibility

--- src/Emulator/Extensions/Analyzers/LoggingUartAnalyzer.cs
+++ src/Emulator/Extensions/Analyzers/LoggingUartAnalyzer.cs
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2010-2018 Antmicro
+// Copyright (c) 2010-2021 Antmicro
 // Copyright (c) 2011-2015 Realtime Embedded
 //
 // This file is licensed under the MIT License.
@@ -18,8 +18,8 @@ using Antmicro.Renode.Time;
 
 namespace Antmicro.Renode.Analyzers
 {
-    // This class is marked as `IExternal` to allow access to `LogLevel`
-    // property from monitor. In order to do this one must create analyzer
+    // This class is marked as `IExternal` to allow access to
+    // properties from Monitor. In order to do this one must create analyzer
     // and add it as external using command below:
     //
     // showAnalyzer "ExternalName" sysbus.uart_name Antmicro.Renode.Analyzers.LoggingUartAnalyzer
@@ -31,6 +31,8 @@ namespace Antmicro.Renode.Analyzers
         {
             line = new StringBuilder(InitialCapacity);
             LogLevel = LogLevel.Info;
+
+            TimestampFormat = TimestampType.Full;
         }
 
         public override void AttachTo(UARTBackend backend)
@@ -47,7 +49,8 @@ namespace Antmicro.Renode.Analyzers
 
         public override void Show()
         {
-            lastLineStampHost = CustomDateTime.Now;
+            baseStampHost = CustomDateTime.Now;
+            lastLineStampHost = baseStampHost;
             lastLineStampVirtual = machine.ElapsedVirtualTime.TimeElapsed;
 
             uart.CharReceived += WriteChar;
@@ -64,21 +67,76 @@ namespace Antmicro.Renode.Analyzers
 
         public LogLevel LogLevel { get; set; }
 
+        public TimestampType TimestampFormat { get; set; }
+
         private void WriteChar(byte value)
         {
             if(value == 10)
             {
-                var now = CustomDateTime.Now;
-                var virtualNow = machine.ElapsedVirtualTime.TimeElapsed;
-                uart.Log(LogLevel, "[+{0}s host +{1}s virt {2}s virt from start] {3}",
-                    Misc.NormalizeDecimal((now - lastLineStampHost).TotalSeconds),
-                    Misc.NormalizeDecimal((virtualNow - lastLineStampVirtual).TotalSeconds),
-                    Misc.NormalizeDecimal(machine.ElapsedVirtualTime.TimeElapsed.TotalSeconds),
-                    line.ToString());
-                lastLineStampHost = now;
+                var hostNow = CustomDateTime.Now;
+                var virtualNow = machine.LocalTimeSource.ElapsedVirtualTime;
+
+                var logLineBuilder = new StringBuilder();
+                logLineBuilder.Append("[");
+
+                var anythingAdded = false;
+                if(TimestampFormat == TimestampType.Full || TimestampFormat == TimestampType.Host)
+                {
+                    anythingAdded = true;
+
+                    var hostTimestamp = string.Format("{0}s (+{1}s)",
+                        Misc.NormalizeDecimal((hostNow - baseStampHost).TotalSeconds),
+                        Misc.NormalizeDecimal((hostNow - lastLineStampHost).TotalSeconds));
+
+                    if(hostTimestamp.Length < maxHostTimestampLength)
+                    {
+                        hostTimestamp = hostTimestamp.PadLeft(maxHostTimestampLength, ' ');
+                    }
+                    else
+                    {
+                        maxHostTimestampLength = hostTimestamp.Length;
+                    }
+
+                    logLineBuilder.AppendFormat("host: {0}", hostTimestamp);
+                }
+
+                if(TimestampFormat == TimestampType.Full || TimestampFormat == TimestampType.Virtual)
+                {
+                    if(anythingAdded)
+                    {
+                        logLineBuilder.Append("|");
+                    }
+                    anythingAdded = true;
+
+                    var virtTimestamp = string.Format("{0}s (+{1}s)",
+                        Misc.NormalizeDecimal(virtualNow.TotalSeconds),
+                        Misc.NormalizeDecimal((virtualNow - lastLineStampVirtual).TotalSeconds));
+
+                    if(virtTimestamp.Length < maxVirtTimestampLength)
+                    {
+                        virtTimestamp = virtTimestamp.PadLeft(maxVirtTimestampLength, ' ');
+                    }
+                    else
+                    {
+                        maxVirtTimestampLength = virtTimestamp.Length;
+                    }
+
+                    logLineBuilder.AppendFormat("virt: {0}", virtTimestamp);
+                }
+
+                if(!anythingAdded)
+                {
+                    logLineBuilder.Append("output");
+                }
+
+                logLineBuilder.AppendFormat("] {0}", line.ToString());
+
+                uart.Log(LogLevel, logLineBuilder.ToString());
+
+                lastLineStampHost = hostNow;
                 lastLineStampVirtual = virtualNow;
                 line.Clear();
-                line.Append("  ");
+
                 return;
             }
             if(char.IsControl((char)value))
@@ -90,14 +148,26 @@ namespace Antmicro.Renode.Analyzers
             line.Append(nextCharacter);
         }
 
+        private DateTime baseStampHost;
         private DateTime lastLineStampHost;
         private TimeInterval lastLineStampVirtual;
         private IUART uart;
         private Machine machine;
 
+        private int maxHostTimestampLength;
+        private int maxVirtTimestampLength;
+
         private readonly StringBuilder line;
 
         private const int InitialCapacity = 120;
+
+        public enum TimestampType
+        {
+            None,
+            Virtual,
+            Host,
+            Full
+        }
     }
 }
 
