2b8f2f801583d5c7f1dfb2fb6eb21b6ac19889b5
Jakub Jatczak <jjatczak@antmicro.com>
2021-09-16 11:35:39 +0200

    [#28774] OpenTitan_AES: Add peripheral and unit tests

---
+++ src/Emulator/Peripherals/Peripherals/Miscellaneous/OpenTitan_AES.cs
@@ -0,0 +1,341 @@
+//
+// Copyright (c) 2021 Antmicro
+//
+//  This file is licensed under the MIT License.
+//  Full license text is available in 'licenses/MIT.txt'.
+using System;
+using System.Linq;
+using System.Security.Cryptography;
+using Antmicro.Renode.Core;
+using Antmicro.Renode.Core.Structure.Registers;
+using Antmicro.Renode.Debugging;
+using Antmicro.Renode.Logging;
+using Antmicro.Renode.Utilities;
+using Antmicro.Renode.Peripherals;
+using Antmicro.Renode.Peripherals.Utilities;
+
+namespace Antmicro.Renode.Peripherals.Miscellaneous
+{
+    // OpenTitan HMAC AES as per https://docs.opentitan.org/hw/ip/aes/doc/ (16.09.2021)
+    public class OpenTitan_AES : BasicDoubleWordPeripheral, IKnownSize
+    {
+        public OpenTitan_AES(Machine machine) : base(machine)
+        {
+            DefineRegisters();
+            initializationVector = new ByteArrayWithAccessTracking(this, InitializationVectorLengthInBytes / sizeof(uint), sizeof(uint), "IV");
+            initialKeyShare_0 = new ByteArrayWithAccessTracking(this, InitialKeyShareLengthInBytes / sizeof(uint), sizeof(uint), "initialKeyShare_0");
+            initialKeyShare_1 = new ByteArrayWithAccessTracking(this, InitialKeyShareLengthInBytes / sizeof(uint), sizeof(uint), "initialKeyShare_1");
+            inputData = new ByteArrayWithAccessTracking(this, DataLengthInBytes / sizeof(uint), sizeof(uint), "DATA_IN");
+            outputData = new ByteArrayWithAccessTracking(this, DataLengthInBytes / sizeof(uint), sizeof(uint), "DATA_OUT");
+
+            key = new byte[InitialKeyShareLengthInBytes];
+            aes = new AesManaged();
+            random = new PseudorandomNumberGenerator();
+            Reset();
+        }
+
+        public override void Reset()
+        {
+            base.Reset();
+            readyForInputWrite = true;
+            initializationVector.Reset();
+            initialKeyShare_0.Reset();
+            initialKeyShare_1.Reset();
+            inputData.Reset();
+            outputData.Reset();
+            Array.Clear(key, 0, InitialKeyShareLengthInBytes);
+        }
+
+        public long Size => 0x1000;
+
+        public bool InputDataReady => inputData.AllDataWritten;
+
+        private void DefineRegisters()
+        {
+            // As the opentitan software often writes 1's to unused register fields they are defined as `IgnoredBits` to avoid flooding the logs
+            Registers.AlertTest.Define(this)
+                .WithTaggedFlag("recov_ctrl_update_err", 0)
+                .WithTaggedFlag("fatal_fault", 1)
+                .WithIgnoredBits(2, 30);
+
+            Registers.InitialKeyShare0_0.DefineMany(this, InitialKeyShareLengthInBytes / sizeof(uint), (register, idx) =>
+            {
+                var part = (uint)idx;
+                register.WithValueField(0, 32, FieldMode.Write, writeCallback: (_, value) =>
+                {
+                    initialKeyShare_0.SetPart(part, value);
+                    TryPrepareKey();
+                }, name: $"key_share_0_{part}");
+            });
+
+            Registers.InitialKeyShare1_0.DefineMany(this, InitialKeyShareLengthInBytes / sizeof(uint), (register, idx) =>
+            {
+                var part = (uint)idx;
+                register.WithValueField(0, 32, FieldMode.Write, writeCallback: (_, value) =>
+                {
+                    initialKeyShare_1.SetPart(part, value);
+                    TryPrepareKey();
+                }, name: $"key_share_1_{part}");
+            });
+
+            Registers.InitializationVector_0.DefineMany(this, InitializationVectorLengthInBytes / sizeof(uint), (register, idx) =>
+            {
+                var part = (uint)idx;
+                register.WithValueField(0, 32, FieldMode.Write, writeCallback: (_, value) => initializationVector.SetPart(part, value), name: $"iv_{part}");
+            });
+
+            Registers.InputData_0.DefineMany(this, DataLengthInBytes / sizeof(uint), (register, idx) =>
+            {
+                var part = (uint)idx;
+                register.WithValueField(0, 32, FieldMode.Write, writeCallback: (_, value) =>
+                {
+                    inputData.SetPart(part, value);
+                    if(inputData.AllDataWritten && !manualOperation.Value)
+                    {
+                        ProcessInput();
+                    }
+                }, name: $"data_in_{part}");
+            });
+
+            Registers.OutputData_0.DefineMany(this, DataLengthInBytes / sizeof(uint), (register, idx) =>
+            {
+                var part = (uint)idx;
+                register.WithValueField(0, 32, FieldMode.Read, valueProviderCallback: _ =>
+                {
+                    var val = outputData.GetPartAsDoubleWord(part);
+                    if(outputData.AllDataRead)
+                    {
+                        readyForInputWrite = true;
+                        outputValid.Value = false;
+                    }
+                    return val;
+                }, name: $"data_out_{part}");
+            });
+
+            Registers.Control.Define(this, 0xc0)
+                .WithFlag(0, out decryptionMode, name: "OPERATION")
+                .WithEnumField<DoubleWordRegister, OperationMode>(1, 6, out operationMode, name: "MODE")
+                .WithEnumField<DoubleWordRegister, KeyLength>(7, 3, out keyLength, name: "KEY_LEN")
+                .WithTaggedFlag("SIDELOAD", 10)
+                .WithFlag(11, out manualOperation, name: "MANUAL_OPERATION")
+                .WithTaggedFlag("FORCE_ZERO_MASKS", 12)
+                .WithIgnoredBits(13, 19)
+                .WithWriteCallback((_, val) =>
+                {
+                    this.Log(LogLevel.Debug, "New configuration:\n\tOPERATION = {0}\n\tMODE = {1}\n\tKEY_LEN = {2}\n\tMANUAL_OPERATION = {3}",
+                             decryptionMode.Value, operationMode.Value, keyLength.Value, manualOperation.Value);
+                });
+
+            Registers.Trigger.Define(this, 0xe)
+                .WithFlag(0, FieldMode.Write, writeCallback: (_, val) =>
+                          {
+                              if(val && manualOperation.Value)
+                              {
+                                  ProcessInput();
+                              }
+                              else
+                              {
+                                  this.Log(LogLevel.Warning, "Received the 'START' trigger while not in manual mode - ignoring");
+                              }
+                          }, name: "START")
+                .WithFlag(1, FieldMode.Write, writeCallback: (_, val) =>
+                    {
+                        if(val)
+                        {
+                            this.Log(LogLevel.Debug, "Randomizing input registers ('IV', 'INPUT_DATA', 'KEY_SHARE_')");
+                            initializationVector.SetArrayTo(GetRandomArrayOfLength(InitializationVectorLengthInBytes), trackAccess: false);
+                            initialKeyShare_0.SetArrayTo(GetRandomArrayOfLength(InitialKeyShareLengthInBytes), trackAccess: false);
+                            initialKeyShare_1.SetArrayTo(GetRandomArrayOfLength(InitialKeyShareLengthInBytes), trackAccess: false);
+                            inputData.SetArrayTo(GetRandomArrayOfLength(DataLengthInBytes), trackAccess: false);
+                        }
+                    }, name: "KEY_IV_DATA_IN_CLEAR")
+                .WithFlag(2, FieldMode.Write, writeCallback: (_, val) =>
+                    {
+                        if(val)
+                        {
+                            this.Log(LogLevel.Debug, "Randomizing output registers");
+                            outputData.SetArrayTo(GetRandomArrayOfLength(DataLengthInBytes), trackAccess: false);
+                        }
+                    }, name: "DATA_OUT_CLEAR")
+                .WithTaggedFlag("PRNG_RESEED", 3)
+                .WithIgnoredBits(4, 28);
+
+            Registers.Status.Define(this, 0x1)
+                .WithFlag(0, out statusIdle, FieldMode.Read, name: "IDLE")
+                .WithTaggedFlag("STALL", 1)
+                .WithTaggedFlag("OUTPUT_LOST", 2)
+                .WithFlag(3, out outputValid, FieldMode.Read, name: "OUTPUT_VALID")
+                .WithFlag(4, FieldMode.Read, valueProviderCallback: _ => readyForInputWrite && !manualOperation.Value, name: "INPUT_READY")
+                .WithTaggedFlag("ALERT_RECOV_CTRL_UPDATE_ERR", 5)
+                .WithTaggedFlag("ALERT_FATAL_FAULT", 6)
+                .WithIgnoredBits(7, 25);
+        }
+
+        private byte[] GetRandomArrayOfLength(int lengthInBytes)
+        {
+            var randomized = new byte[lengthInBytes];
+            random.NextBytes(randomized);
+            return randomized;
+        }
+
+        private void TryPrepareKey()
+        {
+            if(initialKeyShare_0.AllDataWritten && initialKeyShare_1.AllDataWritten)
+            {
+                var key0 = initialKeyShare_0.RetriveData(trackAccess: false);
+                var key1 = initialKeyShare_1.RetriveData(trackAccess: false);
+
+                for(var i = 0; i < InitialKeyShareLengthInBytes; i++)
+                {
+                    key[i] = (byte)(((int)key0[i]) ^ ((int)key1[i]));
+                }
+            }
+        }
+
+        private bool ConfigureAES()
+        {
+            switch(keyLength.Value)
+            {
+                case KeyLength.Bits128:
+                    aes.KeySize = 128;
+                    break;
+                case KeyLength.Bits192:
+                    aes.KeySize = 192;
+                    break;
+                case KeyLength.Bits256:
+                    aes.KeySize = 256;
+                    break;
+                default:
+                    // This kind of misconfiguration is possible, behaviour is undefined
+                    this.Log(LogLevel.Error, "Invalid KEY_LEN size: '0x{1:x}'. Undefined behavior ahead!", keyLength.Value);
+                    return false;
+            }
+            aes.Key = key;
+#if DEBUG
+            this.Log(LogLevel.Debug, "Generated key: {0}", Misc.Stringify(key, " "));
+#endif
+            switch(operationMode.Value)
+            {
+                case OperationMode.ElectronicCodebook:
+                    aes.Mode = CipherMode.ECB;
+                    break;
+                default:
+                    this.Log(LogLevel.Error, "Encryption Mode {0} is not supported yet", operationMode.Value);
+                    return false;
+            }
+            return true;
+        }
+
+        private bool TransformData()
+        {
+            var output = new byte[DataLengthInBytes];
+            var transformMethod = decryptionMode.Value ? aes.CreateDecryptor() : aes.CreateEncryptor();
+            var data = inputData.RetriveData(trackAccess: false);
+            int bytesProcessed = 0;
+
+            if(data.Length == 0)
+            {
+                this.Log(LogLevel.Error, "Started transforming data before full input was available. No output will be generated");
+                return false;
+            }
+            // This is a workaround for the problem with zero bytes transformation on first call while decrypting;
+            // doesn't happen while encrypting and found no plausible explanation
+            while(bytesProcessed == 0)
+            {
+                bytesProcessed = transformMethod.TransformBlock(data, 0, DataLengthInBytes, output, 0);
+            }
+            outputData.SetArrayTo(output, trackAccess: false);
+            this.Log(LogLevel.Debug, "Generated 'DATA_OUT' : {0}", Misc.Stringify(output, " "));
+            return true;
+        }
+
+        private void ProcessInput()
+        {
+            var configurationOk = ConfigureAES();
+            if(!configurationOk)
+            {
+                return;
+            }
+
+            var transformationSucessfull = TransformData();
+            if(transformationSucessfull)
+            {
+                outputValid.Value = true;
+            }
+        }
+
+        private readonly byte[] key;
+        private bool readyForInputWrite;
+        private readonly ByteArrayWithAccessTracking initialKeyShare_0;
+        private readonly ByteArrayWithAccessTracking initialKeyShare_1;
+        private readonly ByteArrayWithAccessTracking initializationVector;
+        private readonly ByteArrayWithAccessTracking inputData;
+        private readonly ByteArrayWithAccessTracking outputData;
+
+        private IFlagRegisterField decryptionMode;
+        private IFlagRegisterField manualOperation;
+        private IFlagRegisterField statusIdle;
+        private IFlagRegisterField outputValid;
+        private IEnumRegisterField<OperationMode> operationMode;
+        private IEnumRegisterField<KeyLength> keyLength;
+
+        private readonly AesManaged aes;
+        private readonly PseudorandomNumberGenerator random;
+
+        private const int InitialKeyShareLengthInBytes = 32;
+        private const int InitializationVectorLengthInBytes = 16;
+        private const int DataLengthInBytes = 16;
+
+        private enum OperationMode
+        {
+            ElectronicCodebook = 0b000001,  // ECB mode
+            CipherBlockChaining = 0b000010, // CBC mode
+            CipherFeedback = 0b000100,      // CFB mode
+            OutputFeedback = 0b001000,      // OFB mode
+            Counter = 0b010000,             // CTR mode
+        }
+
+        private enum KeyLength
+        {
+            Bits128 = 0b001,
+            Bits192 = 0b010,
+            Bits256 = 0b100,
+        }
+
+        public enum Registers
+        {
+            AlertTest = 0x0,
+            InitialKeyShare0_0 = 0x4,
+            InitialKeyShare0_1 = 0x8,
+            InitialKeyShare0_2 = 0xc,
+            InitialKeyShare0_3 = 0x10,
+            InitialKeyShare0_4 = 0x14,
+            InitialKeyShare0_5 = 0x18,
+            InitialKeyShare0_6 = 0x1c,
+            InitialKeyShare0_7 = 0x20,
+            InitialKeyShare1_0 = 0x24,
+            InitialKeyShare1_1 = 0x28,
+            InitialKeyShare1_2 = 0x2c,
+            InitialKeyShare1_3 = 0x30,
+            InitialKeyShare1_4 = 0x34,
+            InitialKeyShare1_5 = 0x38,
+            InitialKeyShare1_6 = 0x3c,
+            InitialKeyShare1_7 = 0x40,
+            InitializationVector_0 = 0x44,
+            InitializationVector_1 = 0x48,
+            InitializationVector_2 = 0x4c,
+            InitializationVector_3 = 0x50,
+            InputData_0 = 0x54,
+            InputData_1 = 0x58,
+            InputData_2 = 0x5c,
+            InputData_3 = 0x60,
+            OutputData_0 = 0x64,
+            OutputData_1 = 0x68,
+            OutputData_2 = 0x6c,
+            OutputData_3 = 0x70,
+            Control = 0x74,
+            Trigger = 0x78,
+            Status = 0x7c,
+        }
+    }
+}
---
+++ src/Emulator/Peripherals/Test/PeripheralsTests/OpenTitan_AES_Test.cs
@@ -0,0 +1,182 @@
+//
+// Copyright (c) 2021 Antmicro
+//
+// This file is licensed under the MIT License.
+// Full license text is available in 'licenses/MIT.txt'.
+//
+using System;
+using NUnit.Framework;
+using Antmicro.Renode.Core;
+using Antmicro.Renode.Debugging;
+using Antmicro.Renode.Peripherals.Miscellaneous;
+using System.Collections.Generic;
+
+namespace Antmicro.Renode.PeripheralsTests
+{
+    [TestFixture]
+    public class OpenTitan_AES_Tests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            peripheral = new OpenTitan_AES(new Machine());
+        }
+
+        [Test]
+        public void ShouldStartInIdleMode()
+        {
+            var status = peripheral.ReadDoubleWord((long)OpenTitan_AES.Registers.Status);
+            Assert.AreEqual(0x1, status & 0x1);
+        }
+
+        [Test]
+        public void ShouldChangeStatusWhenSetToAutomaticMode()
+        {
+            peripheral.WriteDoubleWord((long)OpenTitan_AES.Registers.Control, 0x8);
+            var status = peripheral.ReadDoubleWord((long)OpenTitan_AES.Registers.Status);
+            Assert.AreEqual(0x10, status & 0x10);
+        }
+
+        [Test]
+        public void ShouldKeepTrackOfDataWrites()
+        {
+            peripheral.WriteDoubleWord((long)OpenTitan_AES.Registers.InputData_0, 0x1234);
+
+            Assert.IsFalse(peripheral.InputDataReady);
+            WriteInputData(encrypted);
+            Assert.IsTrue(peripheral.InputDataReady);
+        }
+
+        [Test]
+        //This test is based on OpenTitan AES smoketest binary
+        public void ShouldGiveCorrectEncryptionOutput()
+        {
+            SetKeyShare();
+
+            peripheral.WriteDoubleWord((long)OpenTitan_AES.Registers.Control, 0x202);
+            WriteInputData(decrypted);
+
+            for(int i = 0; i < 4; i++)
+            {
+                var actual = peripheral.ReadDoubleWord((long)OpenTitan_AES.Registers.OutputData_0 + 4 * i);
+                Assert.AreEqual(encrypted[i], actual, "DATA_OUT_{0}: Expected: 0x{1:X}, Got: 0x{2:x}", i, encrypted[i], actual);
+            }
+        }
+
+        [Test]
+        //This test is based on OpenTitan AES smoketest binary
+        public void ShouldWorkInManualModeToo()
+        {
+            SetKeyShare();
+
+            peripheral.WriteDoubleWord((long)OpenTitan_AES.Registers.Control, 0xA02);
+            WriteInputData(decrypted);
+
+            // Before trigger
+            for(int i = 0; i < 4; i++)
+            {
+                var actual = peripheral.ReadDoubleWord((long)OpenTitan_AES.Registers.OutputData_0 + 4 * i);
+                Assert.AreEqual(0x0, actual, "DATA_OUT_{0}: Expected to be zero before trigger, Got: 0x{1:x}", i, actual);
+            }
+
+            peripheral.WriteDoubleWord((long)OpenTitan_AES.Registers.Trigger, 0x1);
+
+            // After trigger
+            for(int i = 0; i < 4; i++)
+            {
+                var actual = peripheral.ReadDoubleWord((long)OpenTitan_AES.Registers.OutputData_0 + 4 * i);
+                Assert.AreEqual(encrypted[i], actual, "DATA_OUT_{0}: Expected: 0x{1:X}, Got: 0x{2:x}", i, encrypted[i], actual);
+            }
+
+        }
+
+        [Test]
+        //This test is based on OpenTitan AES smoketest binary
+        public void ShouldGiveCorrectDecryptionOutput()
+        {
+            SetKeyShare();
+
+            peripheral.WriteDoubleWord((long)OpenTitan_AES.Registers.Control, 0x203);
+
+            WriteInputData(encrypted);
+
+            for(int i = 0; i < 4; i++)
+            {
+                var actual = peripheral.ReadDoubleWord((long)OpenTitan_AES.Registers.OutputData_0+ 4 * i);
+                Assert.AreEqual(decrypted[i], actual, "DATA_OUT_{0}: Expected: 0x{1:X}, Got: 0x{2:x}", i, decrypted[i], actual);
+            }
+        }
+
+        [Test]
+        public void ShouldRandomizeDataOutputOnTrigger()
+        {
+            peripheral.WriteDoubleWord((long)OpenTitan_AES.Registers.Trigger, 0x4);
+            uint sum = 0;
+            for(int i = 0; i < 4; i++)
+            {
+                sum += peripheral.ReadDoubleWord((long)OpenTitan_AES.Registers.OutputData_0 + 4 * i);
+            }
+            Assert.AreNotEqual(0x0, sum);
+        }
+
+        [Test]
+        public void ShouldRandomizeInputRegistersOnTrigger()
+        {
+            peripheral.WriteDoubleWord((long)OpenTitan_AES.Registers.Control, 0xA02);
+
+            // We only test if the output is different after randomizing
+            WriteInputData(decrypted);
+            peripheral.WriteDoubleWord((long)OpenTitan_AES.Registers.Trigger, 0x6);
+            peripheral.WriteDoubleWord((long)OpenTitan_AES.Registers.Trigger, 0x1);
+
+            for( var i = 0; i < 4; i++)
+            {
+                var actual = peripheral.ReadDoubleWord((long)OpenTitan_AES.Registers.OutputData_0 + 4 * i);
+                Assert.AreNotEqual(encrypted[i], actual);
+            }
+        }
+
+        [Test]
+        public void ShouldBeAbleToDoMultipleTransformationsWithASingleKey()
+        {
+            SetKeyShare();
+
+            peripheral.WriteDoubleWord((long)OpenTitan_AES.Registers.Control, 0x203);
+
+            WriteInputData(decrypted);
+            WriteInputData(encrypted);
+
+            for(int i = 0; i < 4; i++)
+            {
+                var actual = peripheral.ReadDoubleWord((long)OpenTitan_AES.Registers.OutputData_0 + 4 * i);
+                Assert.AreEqual(decrypted[i], actual, "DATA_OUT_{0}: Expected: 0x{1:X}, Got: 0x{2:x}", i, decrypted[i], actual);
+            }
+        }
+
+        private void WriteInputData(uint[] data)
+        {
+            DebugHelper.Assert(data.Length == 4);
+
+            for(var i = 0; i < 4; i++)
+            {
+                peripheral.WriteDoubleWord((long)OpenTitan_AES.Registers.InputData_0 + 4 * i, data[i]);
+            }
+        }
+
+        private void SetKeyShare()
+        {
+            for(int i = 0; i < 8; i++)
+            {
+                peripheral.WriteDoubleWord((long)OpenTitan_AES.Registers.InitialKeyShare0_0 + i * 4, keyShare0[i]);
+                peripheral.WriteDoubleWord((long)OpenTitan_AES.Registers.InitialKeyShare1_0 + i * 4, keyShare1[i]);
+            }
+        }
+
+        private OpenTitan_AES peripheral;
+
+        private readonly uint[] keyShare0 = { 0x3C2D1E0F, 0x78695A4B, 0xB4A59687, 0xF0E1D2C3, 0x29380B1A, 0x6D7C4F5E, 0xA1B08392, 0xE5F4C7D6 };
+        private readonly uint[] keyShare1 = { 0x3F2F1F0F, 0x7F6F5F4F, 0xBFAF9F8F, 0xFFEFDFCF, 0x3A2A1A0A, 0x7A6A5A4A, 0xBAAA9A8A, 0xFAEADACA };
+        private readonly uint[] encrypted = { 0xCAB7A28E, 0xBF456751, 0x9049FCEA, 0x8960494B };
+        private readonly uint[] decrypted = { 0x33221100, 0x77665544, 0xBBAA9988, 0xFFEEDDCC };
+    }
+}
