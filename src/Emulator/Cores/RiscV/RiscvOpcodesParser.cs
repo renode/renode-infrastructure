f4ad1ef5e2600ac50675ee9d7347e9efd17e351e
Mateusz Holenko <mholenko@antmicro.com>
2021-12-10 13:05:12 +0100

    [#30456] RiscVOpcodesParser: Move RiscVOpcodesParser class

--- src/Emulator/Cores/RiscV/RiscvOpcodesParser.cs
+++ src/Emulator/Cores/RiscV/RiscvOpcodesParser.cs
@@ -26,213 +26,213 @@ namespace Antmicro.Renode.Peripherals.CPU
 
             cpu.EnableOpcodesCounting = true;
         }
+    }
     
-        public static class RiscVOpcodesParser
+    public static class RiscVOpcodesParser
+    {
+        public static IEnumerable<Tuple<string, string>> Parse(string file)
         {
-            public static IEnumerable<Tuple<string, string>> Parse(string file)
+            var result = new List<Tuple<string, string>>();
+            
+            try
             {
-                var result = new List<Tuple<string, string>>();
-                
-                try
+                foreach(var line in File.ReadLines(file).Select(x => RemoveComments(x)))
                 {
-                    foreach(var line in File.ReadLines(file).Select(x => RemoveComments(x)))
+                    if(line.Length == 0)
                     {
-                        if(line.Length == 0)
-                        {
-                            continue;
-                        }
-                        
-                        result.Add(ParseLine(line));
-                    }
-                }
-                catch(IOException e)
-                {
-                    throw new RecoverableException($"There was na error when parsing RISC-V opcodes from {file}:\n{e.Message}");
-                }
-                
-                return result;
-            }
-
-            private static string RemoveComments(string line)
-            {
-                var pos = line.IndexOf("#");
-                if(pos != -1)
-                {
-                    line = line.Remove(pos);
-                }
-                return line.Trim();
-            }
-
-            private static Tuple<string, string> ParseLine(string lineContent, int opcodeLength = 32)
-            {
-                var pattern = new StringBuilder(new String('_', opcodeLength));
-                
-                var elems = lineContent.Split(LineSplitPatterns, StringSplitOptions.RemoveEmptyEntries);
-                if(elems.Length < 2)
-                {
-                    throw new RecoverableException($"Couldn't split line: {lineContent}");
-                }
-
-                var instructionName = elems[0];
-                foreach(var elem in elems.Skip(1))
-                {
-                    var parts = elem.Split(PartSplitPatterns);
-                    if(parts.Length != 2)
-                    {
-                        // let's ignore all non-explicit ranges
                         continue;
                     }
-
-                    if(!BitsRange.TryParse(parts[0], out var range))
-                    {
-                        throw new RecoverableException($"Couldn't parse range: {parts[0]}");
-                    }
-
-                    if(!BitsValue.TryParse(parts[1], out var value))
-                    {
-                        throw new RecoverableException($"Couldn't parse value: {parts[1]}");
-                    }
-
-                    if(!range.TryApply(pattern, value))
-                    {
-                        throw new RecoverableException($"Couldn't apply value {value} in range {range} to pattern {pattern}");
-                    }
+                    
+                    result.Add(ParseLine(line));
                 }
-
-                return Tuple.Create(instructionName, pattern.ToString());
+            }
+            catch(IOException e)
+            {
+                throw new RecoverableException($"There was na error when parsing RISC-V opcodes from {file}:\n{e.Message}");
             }
             
-            private static readonly char[] PartSplitPatterns = new [] { '=' };
-            private static readonly char[] LineSplitPatterns = new [] { ' ', '\t' };
-            private static readonly string[] BitRangeSplitPatterns = new [] { ".." };
+            return result;
+        }
 
-            private struct BitsValue
+        private static string RemoveComments(string line)
+        {
+            var pos = line.IndexOf("#");
+            if(pos != -1)
             {
-                public static bool TryParse(string s, out BitsValue bv)
+                line = line.Remove(pos);
+            }
+            return line.Trim();
+        }
+
+        private static Tuple<string, string> ParseLine(string lineContent, int opcodeLength = 32)
+        {
+            var pattern = new StringBuilder(new String('_', opcodeLength));
+            
+            var elems = lineContent.Split(LineSplitPatterns, StringSplitOptions.RemoveEmptyEntries);
+            if(elems.Length < 2)
+            {
+                throw new RecoverableException($"Couldn't split line: {lineContent}");
+            }
+
+            var instructionName = elems[0];
+            foreach(var elem in elems.Skip(1))
+            {
+                var parts = elem.Split(PartSplitPatterns);
+                if(parts.Length != 2)
                 {
-                    if(s == "ignore")
-                    {
-                        bv = new BitsValue(-1, ignored: true);
-                        return true;
-                    }
-                    
-                    if(SmartParser.Instance.TryParse(s, typeof(int), out var result))
-                    {
-                        bv = new BitsValue((int)result);
-                        return true;
-                    }
-                    
-                    bv = new BitsValue(-1);
-                    return false;
+                    // let's ignore all non-explicit ranges
+                    continue;
                 }
 
-                public bool TryGetBinaryPattern(int length, out string result)
+                if(!BitsRange.TryParse(parts[0], out var range))
                 {
-                    if(Ignored)
-                    {
-                        result = new String('x', length);
-                        return true;
-                    }
-                    
-                    result = Convert.ToString(Value, 2);
-                    if(result.Length > length)
-                    {
-                        return false;
-                    }
-
-                    result = result.PadLeft(length, '0');
-                    return true;
+                    throw new RecoverableException($"Couldn't parse range: {parts[0]}");
                 }
 
-                private BitsValue(int value, bool ignored = false)
+                if(!BitsValue.TryParse(parts[1], out var value))
                 {
-                    Value = value;
-                    Ignored = ignored;
+                    throw new RecoverableException($"Couldn't parse value: {parts[1]}");
                 }
 
-                public int Value { get; }
-                public bool Ignored { get; }
-
-                public override string ToString()
+                if(!range.TryApply(pattern, value))
                 {
-                    return $"[BitsValue: {Value}, ignored: {Ignored}]";
+                    throw new RecoverableException($"Couldn't apply value {value} in range {range} to pattern {pattern}");
                 }
             }
 
-            private struct BitsRange
+            return Tuple.Create(instructionName, pattern.ToString());
+        }
+        
+        private static readonly char[] PartSplitPatterns = new [] { '=' };
+        private static readonly char[] LineSplitPatterns = new [] { ' ', '\t' };
+        private static readonly string[] BitRangeSplitPatterns = new [] { ".." };
+
+        private struct BitsValue
+        {
+            public static bool TryParse(string s, out BitsValue bv)
             {
-                // There are two expected formats:
-                // * hi..lo (hi, lo being integers)
-                // * bit (bit being an integer; treated as bit..bit)
-                public static bool TryParse(string s, out BitsRange br)
+                if(s == "ignore")
                 {
-                    var x = s.Split(BitRangeSplitPatterns, StringSplitOptions.None);
-                    if(x.Length == 1)
-                    {
-                        if(int.TryParse(x[0], out var bit))
-                        {
-                            br = new BitsRange(bit, bit);
-                            return true;
-                        }
-                    }
-                    else if(x.Length == 2)
-                    {
-                        if(int.TryParse(x[0], out var upperBit)
-                            && int.TryParse(x[1], out var lowerBit))
-                        {
-                            br = new BitsRange(lowerBit, upperBit);
-                            return true;
-                        }
-                    }
-
-                    br = new BitsRange(-1, -1);
-                    return false;
-                }
-
-                public bool TryApply(StringBuilder s, BitsValue v)
-                {
-                    if(!v.TryGetBinaryPattern(this.Width, out var pattern))
-                    {
-                        return false;
-                    }
-
-                    if(Higher >= s.Length)
-                    {
-                        return false;
-                    }
-
-                    var pIdx = pattern.Length - 1;
-                    var sIdx = s.Length - Lower - 1;
-                    
-                    for(var i = Lower; i <= Higher; i++, pIdx--, sIdx--)
-                    {
-                        if(s[sIdx] == '1' || s[sIdx] == '0')
-                        {
-                            return false;
-                        }
-
-                        s[sIdx] = pattern[pIdx];
-                    }
-                    
+                    bv = new BitsValue(-1, ignored: true);
                     return true;
                 }
                 
-                public override string ToString()
+                if(SmartParser.Instance.TryParse(s, typeof(int), out var result))
                 {
-                    return $"[BitsRange: {Lower} - {Higher}]";
+                    bv = new BitsValue((int)result);
+                    return true;
                 }
-
-                private BitsRange(int lower, int higher)
-                {
-                    Lower = lower;
-                    Higher = higher;
-                }
-
-                public int Lower { get; }
-                public int Higher { get; }
-
-                public int Width => Higher - Lower + 1;
+                
+                bv = new BitsValue(-1);
+                return false;
             }
+
+            public bool TryGetBinaryPattern(int length, out string result)
+            {
+                if(Ignored)
+                {
+                    result = new String('x', length);
+                    return true;
+                }
+                
+                result = Convert.ToString(Value, 2);
+                if(result.Length > length)
+                {
+                    return false;
+                }
+
+                result = result.PadLeft(length, '0');
+                return true;
+            }
+
+            private BitsValue(int value, bool ignored = false)
+            {
+                Value = value;
+                Ignored = ignored;
+            }
+
+            public int Value { get; }
+            public bool Ignored { get; }
+
+            public override string ToString()
+            {
+                return $"[BitsValue: {Value}, ignored: {Ignored}]";
+            }
+        }
+
+        private struct BitsRange
+        {
+            // There are two expected formats:
+            // * hi..lo (hi, lo being integers)
+            // * bit (bit being an integer; treated as bit..bit)
+            public static bool TryParse(string s, out BitsRange br)
+            {
+                var x = s.Split(BitRangeSplitPatterns, StringSplitOptions.None);
+                if(x.Length == 1)
+                {
+                    if(int.TryParse(x[0], out var bit))
+                    {
+                        br = new BitsRange(bit, bit);
+                        return true;
+                    }
+                }
+                else if(x.Length == 2)
+                {
+                    if(int.TryParse(x[0], out var upperBit)
+                        && int.TryParse(x[1], out var lowerBit))
+                    {
+                        br = new BitsRange(lowerBit, upperBit);
+                        return true;
+                    }
+                }
+
+                br = new BitsRange(-1, -1);
+                return false;
+            }
+
+            public bool TryApply(StringBuilder s, BitsValue v)
+            {
+                if(!v.TryGetBinaryPattern(this.Width, out var pattern))
+                {
+                    return false;
+                }
+
+                if(Higher >= s.Length)
+                {
+                    return false;
+                }
+
+                var pIdx = pattern.Length - 1;
+                var sIdx = s.Length - Lower - 1;
+                
+                for(var i = Lower; i <= Higher; i++, pIdx--, sIdx--)
+                {
+                    if(s[sIdx] == '1' || s[sIdx] == '0')
+                    {
+                        return false;
+                    }
+
+                    s[sIdx] = pattern[pIdx];
+                }
+                
+                return true;
+            }
+            
+            public override string ToString()
+            {
+                return $"[BitsRange: {Lower} - {Higher}]";
+            }
+
+            private BitsRange(int lower, int higher)
+            {
+                Lower = lower;
+                Higher = higher;
+            }
+
+            public int Lower { get; }
+            public int Higher { get; }
+
+            public int Width => Higher - Lower + 1;
         }
     }
 }
