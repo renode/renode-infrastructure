98bd54961c513a6565b4da4b53f381cf3b2d4e2b
Philipp Finke <philipp.finke@sick.de>
2022-01-04 14:51:18 +0100

    [#284] Using TaggedFlags instead of Flags where applicable

--- src/Emulator/Peripherals/Peripherals/Miscellaneous/STM32_PWR.cs
+++ src/Emulator/Peripherals/Peripherals/Miscellaneous/STM32_PWR.cs
@@ -15,100 +15,72 @@ using Antmicro.Renode.Utilities;
 namespace Antmicro.Renode.Peripherals.Miscellaneous
 {
     [AllowedTranslations(AllowedTranslation.ByteToDoubleWord | AllowedTranslation.WordToDoubleWord)]
-    public sealed class STM32_PWR : IDoubleWordPeripheral, IKnownSize
+    public sealed class STM32_PWR : BasicDoubleWordPeripheral, IKnownSize
     {
-        public STM32_PWR(Machine machine)
+        public STM32_PWR(Machine machine) : base(machine)
         {
-            var registerDictionary = new Dictionary<long, DoubleWordRegister>
-            {
-                {(long)Registers.PowerControl, new DoubleWordRegister(this, 0xFCC00)
-                    .WithFlag(0, name: "LPDS")
-                    .WithFlag(1, name: "PDDS")
-                    .WithFlag(2, FieldMode.Read | FieldMode.WriteOneToClear, name: "CWUF", valueProviderCallback: _ => { return false; }, writeCallback: (_, value) => {
-                            if (value)
-                            {
-                                wufValue.Value = false;
-                            }
-                        })
-                    .WithFlag(3, FieldMode.Read | FieldMode.WriteOneToClear, name: "CSBF", valueProviderCallback: _ => { return false; }, writeCallback: (_, value) => {
-                            if (value)
-                            {
-                                sbfValue.Value = false;
-                            }
-                        })
-                    .WithFlag(4, name: "PVDE")
-                    .WithEnumField<DoubleWordRegister, PvdLevelSelection>(5, 3, name: "PLS")
-                    .WithFlag(8, name: "DBP")
-                    .WithFlag(9, name: "FPDS")
-                    .WithFlag(10, name: "LPUDS")
-                    .WithFlag(11, name: "MRUDS")
-                    .WithReservedBits(12, 1)
-                    .WithFlag(13, name: "ADCD1")
-                    .WithEnumField<DoubleWordRegister, RegulatorVoltageScalingOutputSelection>(14, 2, out vosValue, name: "VOS", writeCallback: (_, value) => {
-                            if (value == RegulatorVoltageScalingOutputSelection.Reserved)
-                            {
-                                vosValue.Value = RegulatorVoltageScalingOutputSelection.ScaleMode3;
-                            }
-                        })
-                    .WithFlag(16, name: "ODEN", writeCallback: (_, value) => {
-                            if (!value)
-                            {
-                                odswenValue.Value = false;
-                            }
+            DefineRegisters();
+        }
+        
+        private void DefineRegisters()
+        {
+            Registers.PowerControl.Define(this, 0xFCC00, "PWR_CR")
+                .WithTaggedFlag("LPDS", 0)
+                .WithTaggedFlag("PDDS", 1)
+                .WithTaggedFlag("CWUF", 2)
+                .WithTaggedFlag("CSBF", 3)
+                .WithTaggedFlag("PVDE", 4)
+                .WithEnumField<DoubleWordRegister, PvdLevelSelection>(5, 3, name: "PLS")
+                .WithTaggedFlag("DBP", 8)
+                .WithTaggedFlag("FPDS", 9)
+                .WithTaggedFlag("LPUDS", 10)
+                .WithTaggedFlag("MRUDS", 11)
+                .WithReservedBits(12, 1)
+                .WithTaggedFlag("ADCD1", 13)
+                .WithEnumField<DoubleWordRegister, RegulatorVoltageScalingOutputSelection>(14, 2, out vosValue, name: "VOS", writeCallback: (_, value) => {
+                        if (value == RegulatorVoltageScalingOutputSelection.Reserved)
+                        {
+                            vosValue.Value = RegulatorVoltageScalingOutputSelection.ScaleMode3;
+                        }
+                    })
+                .WithFlag(16, name: "ODEN", writeCallback: (_, value) => {
+                        if (!value)
+                        {
+                            odswenValue.Value = false;
+                        }
 
-                            odrdyValue.Value = value;
-                        })
-                    .WithFlag(17, out odswenValue, name: "ODSWEN", writeCallback: (_, value) => { odswrdyValue.Value = value; })
-                    .WithEnumField<DoubleWordRegister, UnderDriveEnableInStopMode>(18, 2, name: "UDEN")
-                    .WithReservedBits(20, 12)
-                },
-                {(long)Registers.PowerControlStatus, new DoubleWordRegister(this)
-                    .WithFlag(0, out wufValue, FieldMode.Read, name: "WUF")
-                    .WithFlag(1, out sbfValue, FieldMode.Read, name: "SBF")
-                    .WithFlag(2, FieldMode.Read, name: "PVDO")
-                    .WithFlag(3, FieldMode.Read, name: "BRR")
-                    .WithReservedBits(4, 4)
-                    .WithFlag(8, name: "EWUP")
-                    .WithFlag(9, name: "BER")
-                    .WithReservedBits(10, 4)
-                    .WithFlag(14, FieldMode.Read, name: "VOSRDY")
-                    .WithReservedBits(15, 1)
-                    .WithFlag(16, out odrdyValue, FieldMode.Read, name: "ODRDY")
-                    .WithFlag(17, out odswrdyValue, FieldMode.Read, name: "ODSWRDY")
-                    .WithEnumField<DoubleWordRegister, UnderDriveReady>(18, 2, FieldMode.Read | FieldMode.WriteOneToClear, name: "UDRDY")
-                    .WithReservedBits(20, 12)
-                }
-            };
-
-            registers = new DoubleWordRegisterCollection(this, registerDictionary);
+                        odrdyValue.Value = value;
+                    })
+                .WithFlag(17, out odswenValue, name: "ODSWEN", writeCallback: (_, value) => { odswrdyValue.Value = value; })
+                .WithEnumField<DoubleWordRegister, UnderDriveEnableInStopMode>(18, 2, name: "UDEN")
+                .WithReservedBits(20, 12);
+                
+            Registers.PowerControlStatus.Define(this, name: "PWR_CSR")
+                .WithTaggedFlag("WUF", 0)
+                .WithTaggedFlag("SBF", 1)
+                .WithTaggedFlag("PVDO", 2)
+                .WithTaggedFlag("BRR", 3)
+                .WithReservedBits(4, 4)
+                .WithTaggedFlag("EWUP", 8)
+                .WithTaggedFlag("BER", 9)
+                .WithReservedBits(10, 4)
+                .WithTaggedFlag("VOSRDY", 14)
+                .WithReservedBits(15, 1)
+                .WithFlag(16, out odrdyValue, FieldMode.Read, name: "ODRDY")
+                .WithFlag(17, out odswrdyValue, FieldMode.Read, name: "ODSWRDY")
+                .WithEnumField<DoubleWordRegister, UnderDriveReady>(18, 2, FieldMode.Read | FieldMode.WriteOneToClear, name: "UDRDY")
+                .WithReservedBits(20, 12);
         }
 
         public long Size => 0x400;
 
-        public uint ReadDoubleWord(long offset)
-        {
-            return registers.Read(offset);
-        }
-
-        public void WriteDoubleWord(long offset, uint value)
-        {
-            registers.Write(offset, value);
-        }
-
-        public void Reset()
-        {
-            registers.Reset();
-        }
-
         private readonly DoubleWordRegisterCollection registers;
         private IEnumRegisterField<RegulatorVoltageScalingOutputSelection> vosValue;
         private IFlagRegisterField odswenValue;
-        private IFlagRegisterField wufValue;
-        private IFlagRegisterField sbfValue;
         private IFlagRegisterField odrdyValue;
         private IFlagRegisterField odswrdyValue;
 
-        private enum Registers : long
+        private enum Registers
         {
             PowerControl = 0x0,
             PowerControlStatus = 0x4
