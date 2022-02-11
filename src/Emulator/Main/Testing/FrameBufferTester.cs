9a4af3e3cc95b4216a825dd448c5f38d32e1f225
Kamil Galik <kgalik@internships.antmicro.com>
2022-02-11 13:57:40 +0100

    [#31899] FrameBufferTester: Add region of interest support

--- src/Emulator/Main/Testing/FrameBufferTester.cs
+++ src/Emulator/Main/Testing/FrameBufferTester.cs
@@ -112,22 +112,78 @@ namespace Antmicro.Renode.Testing
             throw new ArgumentException();
         }
 
-        private void HandleConfigurationChange(int width, int height, Backends.Display.PixelFormat newFormat, ELFSharp.ELF.Endianess newEndianess)
+        public FrameBufferTester WaitForFrameROI(string fileName, uint startX, uint startY, uint width, uint height, float? timeout = null)
+        {
+            var image = Image.FromFile(fileName);
+            var bytes = BitmapToByteArray((Bitmap)image);
+            return WaitForFrameROI(bytes, startX, startY, width, height, timeout.HasValue ? TimeSpan.FromSeconds(timeout.Value) : (TimeSpan?)null);
+        }
+
+        public FrameBufferTester WaitForFrameROI(byte[] frame, uint startX, uint startY, uint width, uint height, TimeSpan? timeout = null)
+        {
+            if(width > frameWidth || startX > frameWidth - width || height > frameHeight || startY > frameHeight - height)
+            {
+                throw new ArgumentException("Region of interest doesn't fit in the frame");
+            }
+
+            if(height == 0 || width == 0)
+            {
+                throw new ArgumentException("Width and height can't be equal to 0.");
+            }
+            
+            var machine = video.GetMachine();
+            var finalTimeout = timeout ?? globalTimeout;
+            var timeoutEvent = machine.LocalTimeSource.EnqueueTimeoutEvent((ulong)finalTimeout.TotalMilliseconds);
+
+            do
+            {
+                if(framesQueue.TryTake(out var queuedFrame)
+                        && queuedFrame.Length == frame.Length)
+                {
+                    bool roiEqual = true;
+                    for(uint i = startY; roiEqual && i < startY + height; i++)
+                    {
+                        for(uint j = startX; roiEqual && j < startX + width; j++)
+                        {
+                            for(uint k = 0; roiEqual && k < 4; k++)
+                            {
+                                int index = (int)(i*frameWidth*4 + j*4 + k);
+                                if(frame[index] != queuedFrame[index])
+                                {
+                                    roiEqual = false;
+                                }
+                            }
+                        }
+                    }
+                    if(roiEqual)
+                    {
+                        return this;
+                    }
+                }
+                WaitHandle.WaitAny(new [] { timeoutEvent.WaitHandle, newFrameEvent });
+            }
+            while(!timeoutEvent.IsTriggered);
+
+            throw new ArgumentException();
+        }
+
+        private void HandleConfigurationChange(int width, int height, Backends.Display.PixelFormat format, ELFSharp.ELF.Endianess endianess)
         {
             if(width == 0 || height == 0)
             {
                 return;
             }
             
-            format = newFormat;
-            endianess = newEndianess;
+            this.format = format;
+            this.endianess = endianess;
             InitConverter();
-            frameSize = width * height * 4;
+            frameWidth = width;
+            frameHeight = height;
         }
 
         private void HandleNewFrame(byte[] obj)
         {
-            var buffer = new byte[frameSize];
+            var buffer = new byte[frameWidth * frameHeight * 4];
             converter.Convert(obj, ref buffer);
             framesQueue.Add(buffer);
             newFrameEvent.Set();
@@ -145,7 +201,8 @@ namespace Antmicro.Renode.Testing
         [Transient]
         private IPixelConverter converter;
 
-        private int frameSize;
+        private int frameWidth;
+        private int frameHeight;
         private IVideo video;
         private Backends.Display.PixelFormat? format;
         private ELFSharp.ELF.Endianess? endianess;
