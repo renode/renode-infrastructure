renode/renode-infrastructure

commit 0e43bfe4564e44ad3505ff9892c155ec44c2d2d3

[#32659] Options: Do not split commands to execute at Renode startup
This should handle errors better as the execution will
stop at the first failed command.

Previously all the commands were executed even when
one of them failed, which sometimes lead to an unexpected behaviour.

src/UI/CommandLineInterface.cs
@@ -220,7 +220,7 @@ namespace Antmicro.Renode.UI
 
             if(options.Execute != null)
             {
-                shell.Started += s => s.InjectInput(string.Format("{0}\n", string.Join("\n", options.Execute)));
+                shell.Started += s => s.InjectInput(options.Execute + "\n");
             }
             else if(!string.IsNullOrEmpty(options.ScriptPath))
             {

src/UI/Options.cs
@@ -18,7 +18,7 @@ namespace Antmicro.Renode.UI
         public int Port { get; set; }
 
         [Name('e', "execute"), Description("Execute command on startup (this option is exclusive with startup script passed as an argument). May be used many times.")]
-        public string[] Execute { get; set; }
+        public string Execute { get; set; }
 
         [Name("config"), Description("Use the configuration file from the provided path, or create one if it does not exist")]
         public string ConfigFile { get; set; }

commit abc0e0ce7fef24c754adc5a30072adb9c2e26061

[#33175] Options: Join commands to execute at Renode startup
This is a fix to commit 0e43bfe4564e44ad3505ff9892c155ec44c2d2d3,
which broke the possibility of using multiple `--execute` options.

Thanks to joining the commands with a semicolon the execution
will stop at the first failed command.

src/UI/CommandLineInterface.cs
@@ -220,7 +220,7 @@ namespace Antmicro.Renode.UI
 
             if(options.Execute != null)
             {
-                shell.Started += s => s.InjectInput(options.Execute + "\n");
+                shell.Started += s => s.InjectInput(string.Format("{0}\n", string.Join("; ", options.Execute)));
             }
             else if(!string.IsNullOrEmpty(options.ScriptPath))
             {

src/UI/Options.cs
@@ -18,7 +18,7 @@ namespace Antmicro.Renode.UI
         public int Port { get; set; }
 
         [Name('e', "execute"), Description("Execute command on startup (this option is exclusive with startup script passed as an argument). May be used many times.")]
-        public string Execute { get; set; }
+        public string[] Execute { get; set; }
 
         [Name("config"), Description("Use the configuration file from the provided path, or create one if it does not exist")]
         public string ConfigFile { get; set; }