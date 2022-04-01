a403066de67f12c1e77e20a4b7e32463f65582db
Piotr Wojnarowski <pwojnarowski@antmicro.com>
2022-04-01 08:53:09 +0200

    [#32448] NativeBinder: Don't propagate C# exceptions through unmanaged code

--- src/Emulator/Main/Utilities/Binding/NativeBinder.cs
+++ src/Emulator/Main/Utilities/Binding/NativeBinder.cs
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2010-2018 Antmicro
+// Copyright (c) 2010-2022 Antmicro
 // Copyright (c) 2011-2015 Realtime Embedded
 //
 // This file is licensed under the MIT License.
@@ -10,6 +10,7 @@ using System.Linq;
 using Antmicro.Renode.Logging;
 using System.Runtime.InteropServices;
 using System.Reflection;
+using System.Reflection.Emit;
 using System.Collections.Generic;
 using Dynamitey;
 using Antmicro.Renode.Core;
@@ -22,6 +23,13 @@ namespace Antmicro.Renode.Utilities.Binding
     /// </summary>
     public sealed class NativeBinder : IDisposable
     {
+        static NativeBinder()
+        {
+            var assemblyBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(
+                new AssemblyName(nameof(NativeBinder)), AssemblyBuilderAccess.Run);
+            moduleBuilder = assemblyBuilder.DefineDynamicModule(nameof(NativeBinder));
+        }
+
         /// <summary>
         /// Initializes a new instance of the <see cref="Antmicro.Renode.Utilities.Runtime.NativeBinder"/> class
         /// and performs binding between the class and given library.
@@ -56,9 +64,12 @@ namespace Antmicro.Renode.Utilities.Binding
             this.classToBind = classToBind;
             libraryAddress = SharedLibraries.LoadLibrary(libraryFile);
             libraryFileName = libraryFile;
+            var importFields = classToBind.GetType().GetAllFields().Where(x => x.IsDefined(typeof(ImportAttribute), false)).ToList();
+            EnsureWrappersType(importFields);
+            wrappersObj = CreateWrappersObject();
             try
             {
-                ResolveCallsToNative();
+                ResolveCallsToNative(importFields);
                 ResolveCallsToManaged();
             }
             catch
@@ -94,20 +105,161 @@ namespace Antmicro.Renode.Utilities.Binding
             DisposeInner();
         }
 
-        private void ResolveCallsToNative()
+        private void EnsureWrappersType(List<FieldInfo> importFields)
+        {
+            // This type lives in our NativeBinder dynamic assembly (see the static constructor).
+            // This means that if we find such a type, it has to have been created by us and we can
+            // just use it without verifying its layout.
+            // Its name is derived from the full name of the class to bind (including its namespace): for example,
+            // the wrappers type for Some.Namespace.Class will be NativeBinder.Some.Namespace.ClassWrappers.
+            var wrappersTypeName = $"NativeBinder.{classToBind.GetType().FullName}Wrappers";
+
+            lock(moduleBuilder)
+            {
+                wrappersType = moduleBuilder.GetType(wrappersTypeName);
+
+                if(wrappersType == null)
+                {
+                    var typeBuilder = moduleBuilder.DefineType(wrappersTypeName);
+                    typeBuilder.DefineField(nameof(ExceptionKeeper), typeof(ExceptionKeeper), FieldAttributes.Public);
+                    typeBuilder.DefineField(nameof(classToBind), classToBind.GetType(), FieldAttributes.Public);
+
+                    foreach(var field in importFields)
+                    {
+                        var attribute = (ImportAttribute)field.GetCustomAttributes(false).Single(x => x is ImportAttribute);
+                        if(attribute.UseExceptionWrapper)
+                        {
+                            typeBuilder.DefineField(field.Name, field.FieldType, FieldAttributes.Public);
+                        }
+                    }
+
+                    wrappersType = typeBuilder.CreateType();
+                }
+
+                exceptionKeeperField = wrappersType.GetField(nameof(ExceptionKeeper));
+                instanceField = wrappersType.GetField(nameof(classToBind));
+            }
+        }
+
+        private object CreateWrappersObject()
+        {
+            var wrappers = Activator.CreateInstance(wrappersType);
+            exceptionKeeperField.SetValue(wrappers, new ExceptionKeeper());
+            instanceField.SetValue(wrappers, classToBind);
+            return wrappers;
+        }
+
+        private Delegate WrapImport(FieldInfo importField, FieldInfo innerField)
+        {
+            var throwExceptions = typeof(ExceptionKeeper).GetMethod(nameof(ExceptionKeeper.ThrowExceptions));
+            var importType = importField.FieldType;
+            var invoke = importType.GetMethod("Invoke");
+            Type[] paramTypes = invoke.GetParameters().Select(p => p.ParameterType).ToArray();
+            Type[] paramTypesWithWrappersType = new Type[] { wrappersType }.Concat(paramTypes).ToArray();
+            DynamicMethod method = new DynamicMethod(importField.Name, invoke.ReturnType, paramTypesWithWrappersType, wrappersType);
+            var il = method.GetILGenerator();
+
+            il.Emit(OpCodes.Ldarg_0); // wrappersType instance
+            il.Emit(OpCodes.Ldfld, innerField);
+            for(int i = 0; i < paramTypes.Length; ++i)
+            {
+                il.Emit(OpCodes.Ldarg, 1 + i);
+            }
+            il.EmitCall(OpCodes.Callvirt, invoke, null); // call inner
+
+            il.Emit(OpCodes.Ldarg_0);
+            il.Emit(OpCodes.Ldfld, exceptionKeeperField);
+            il.EmitCall(OpCodes.Call, throwExceptions, null); // call ExceptionKeeper.ThrowExceptions
+
+            il.Emit(OpCodes.Ret);
+
+            return method.CreateDelegate(importType, wrappersObj);
+        }
+
+        private void ResolveCallsToNative(List<FieldInfo> importFields)
         {
             classToBind.NoisyLog("Binding managed -> native calls.");
-            var fields = classToBind.GetType().GetAllFields().Where(x => x.IsDefined(typeof(ImportAttribute), false)).ToList(); // TODO: const it
             var staticContext = InvokeContext.CreateStatic;
-            foreach (var field in fields)
+
+            foreach (var field in importFields)
             {
                 var attribute = (ImportAttribute)field.GetCustomAttributes(false).First(x => x is ImportAttribute);
-                var cName = attribute.Name ?? GetCName(field.Name);
+                var cName = attribute.Name ?? GetCName(field.Name, attribute.UseExceptionWrapper);
                 classToBind.NoisyLog(string.Format("(NativeBinder) Binding {1} as {0}.", field.Name, cName));
                 var address = SharedLibraries.GetSymbolAddress(libraryAddress, cName);
                 var result = Dynamic.InvokeMember(staticContext(typeof(Marshal)), "GetDelegateForFunctionPointer", address, field.FieldType);
-                field.SetValue(classToBind, result);
+
+                if(attribute.UseExceptionWrapper)
+                {
+                    var innerField = wrappersType.GetField(field.Name);
+                    innerField.SetValue(wrappersObj, result);
+                    field.SetValue(classToBind, WrapImport(field, innerField));
+                }
+                else
+                {
+                    field.SetValue(classToBind, result);
+                }
             }
+        }
+
+        private Delegate WrapExport(Type delegateType, MethodInfo innerMethod)
+        {
+            var addException = typeof(ExceptionKeeper).GetMethod(nameof(ExceptionKeeper.AddException));
+            var nativeUnwind = classToBind.GetType().GetMethod(nameof(INativeUnwindable.NativeUnwind));
+
+            Type[] paramTypes = innerMethod.GetParameters().Select(p => p.ParameterType).ToArray();
+            Type[] paramTypesWithWrappersType = new Type[] { wrappersType }.Concat(paramTypes).ToArray();
+            // We need skipVisibility to be able to access superclass private methods.
+            var attacheeWrapper = new DynamicMethod(innerMethod.Name + "Wrapper", innerMethod.ReturnType, paramTypesWithWrappersType, wrappersType, skipVisibility: true);
+            var il = attacheeWrapper.GetILGenerator();
+            LocalBuilder retval = null;
+            if(innerMethod.ReturnType != typeof(void))
+            {
+                retval = il.DeclareLocal(innerMethod.ReturnType);
+            }
+            var exception = il.DeclareLocal(typeof(Exception));
+
+            Label @try = il.BeginExceptionBlock();
+            il.Emit(OpCodes.Ldarg_0);
+            il.Emit(OpCodes.Ldfld, instanceField);
+            for(int i = 0; i < paramTypes.Length; ++i)
+            {
+                il.Emit(OpCodes.Ldarg, 1 + i);
+            }
+
+            il.EmitCall(OpCodes.Call, innerMethod, null);
+            if(retval != null)
+            {
+                il.Emit(OpCodes.Stloc, retval);
+            }
+            il.Emit(OpCodes.Leave, @try);
+            il.BeginCatchBlock(typeof(Exception));
+            il.Emit(OpCodes.Stloc, exception); // We need this local because MSIL doesn't have an instruction
+            il.Emit(OpCodes.Ldarg_0);          // that swaps the top two values on the stack.
+            il.Emit(OpCodes.Ldfld, exceptionKeeperField);
+            il.Emit(OpCodes.Ldloc, exception);
+            il.EmitCall(OpCodes.Call, addException, null);
+            if(nativeUnwind != null)
+            {
+                il.Emit(OpCodes.Ldarg_0);
+                il.Emit(OpCodes.Ldfld, instanceField);
+                il.EmitCall(OpCodes.Call, nativeUnwind, null);
+            }
+            else
+            {
+                var typeName = classToBind.GetType().FullName;
+                il.EmitWriteLine($"Export to type '{typeName}' which is not unwindable threw an exception.");
+                il.Emit(OpCodes.Ldc_I4_1);
+                il.EmitCall(OpCodes.Call, typeof(Environment).GetMethod(nameof(Environment.Exit)), null);
+            }
+            il.EndExceptionBlock();
+            if(retval != null)
+            {
+                il.Emit(OpCodes.Ldloc, retval);
+            }
+            il.Emit(OpCodes.Ret);
+
+            return attacheeWrapper.CreateDelegate(delegateType, wrappersObj);
         }
 
         private void ResolveCallsToManaged()
@@ -143,7 +295,7 @@ namespace Antmicro.Renode.Utilities.Binding
                 Delegate attachee;
                 try
                 {
-                    attachee = Delegate.CreateDelegate(delegateType, classToBind, desiredMethodInfo);
+                    attachee = WrapExport(delegateType, desiredMethodInfo);
                 }
                 catch(ArgumentException e)
                 {
@@ -187,10 +339,10 @@ namespace Antmicro.Renode.Utilities.Binding
             return result;
         }
 
-        private static string GetCName(string name)
+        private static string GetCName(string name, bool useExceptionWrapper)
         {
             var lastCapitalChar = 0;
-            return name.GroupBy(x =>
+            var cName = name.GroupBy(x =>
             {
                 if (char.IsUpper(x))
                 {
@@ -199,6 +351,13 @@ namespace Antmicro.Renode.Utilities.Binding
                 return lastCapitalChar;
             }).Select(x => x.Aggregate(string.Empty, (y, z) => y + char.ToLower(z))).
                 Aggregate((x, y) => x + "_" + y);
+
+            if(useExceptionWrapper)
+            {
+                cName += "_ex"; // Bind to exception wrapper instead of inner function
+            }
+
+            return cName;
         }
 
         private static string GetCSharpName(string name)
@@ -222,9 +381,15 @@ namespace Antmicro.Renode.Utilities.Binding
             return result;
         }
 
+        private static readonly ModuleBuilder moduleBuilder;
+
         private IntPtr libraryAddress;
         private string libraryFileName;
         private IEmulationElement classToBind;
+        private Type wrappersType;
+        private FieldInfo instanceField;
+        private FieldInfo exceptionKeeperField;
+        private object wrappersObj;
 
         // the point of delegate store is to hold references to delegates
         // which would otherwise be garbage collected while native calls
