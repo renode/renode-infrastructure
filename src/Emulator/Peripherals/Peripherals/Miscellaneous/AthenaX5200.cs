be0526110d91806f80de0c8127560129e11ca1fb
Michał Szprejda <mszprejda@antmicro.com>
2022-04-07 12:41:36 +0200

    [#27111] AthenaX5200: Implement RNG core

--- src/Emulator/Peripherals/Peripherals/Miscellaneous/AthenaX5200.cs
+++ src/Emulator/Peripherals/Peripherals/Miscellaneous/AthenaX5200.cs
@@ -69,6 +69,9 @@ namespace Antmicro.Renode.Peripherals.Miscellaneous
             commands = new Dictionary<JumpTable, Action>
             {
                 { JumpTable.DetectFirmwareVersion, EmptyHandler },
+                { JumpTable.InstantiateDRBG, InstantiateDRBG },
+                { JumpTable.GenerateBlocksFromDRBG, GenerateBlocksWithDRBG },
+                { JumpTable.UninstantiateDRBG, UninstantiateDRBG },
             };
 
             Reset();
@@ -93,6 +96,7 @@ namespace Antmicro.Renode.Peripherals.Miscellaneous
         {
             memoryManager.ResetMemories();
             RegistersCollection.Reset();
+            randomGenerator?.Reset();
             isCompleted = false;
             coreReset = false;
         }
@@ -106,8 +110,39 @@ namespace Antmicro.Renode.Peripherals.Miscellaneous
             // but we want to report in the Status Register that it has completed.
         }
 
+        private void InstantiateDRBG()
+        {
+            if(randomGenerator != null)
+            {
+                this.Log(LogLevel.Error, "RNG subsystem already instantiated. Aborting!");
+                return;
+            }
+            randomGenerator = new PseudorandomBitGenerator(memoryManager);
+        }
+
+        private void GenerateBlocksWithDRBG()
+        {
+            if(randomGenerator == null)
+            {
+                this.Log(LogLevel.Error, "RNG subsystem is not instantiated. Aborting!");
+                return;
+            }
+            randomGenerator.Generate();
+        }
+
+        private void UninstantiateDRBG()
+        {
+            if(randomGenerator == null)
+            {
+                this.Log(LogLevel.Error, "RNG subsystem is not instantiated. Aborting!");
+                return;
+            }
+            randomGenerator = null;
+        }
+
         private bool coreReset;
         private bool isCompleted;
+        private PseudorandomBitGenerator randomGenerator;
 
         private readonly InternalMemoryManager memoryManager;
         private readonly Dictionary<JumpTable, Action> commands;
@@ -299,9 +334,78 @@ namespace Antmicro.Renode.Peripherals.Miscellaneous
             private readonly byte[] internalMemory;
         }
 
+        // This class exposes the functionality of the DRBG core (short for "Deterministic Random Bit Generator"),
+        // but because it is simplified, the name is also changed to be more adequate.
+        private class PseudorandomBitGenerator
+        {
+            public PseudorandomBitGenerator(InternalMemoryManager manager)
+            {
+                this.manager = manager;
+                Reset();
+            }
+
+            public void Generate()
+            {
+                manager.TryReadDoubleWord((long)DRBGRegisters.EntropyFactor, out reqLen);
+                manager.TryReadDoubleWord((long)DRBGRegisters.ReseedLimit, out var reseedLimit);
+                if(reseedLimit == 1 || reseedCounter == 0)
+                {
+                    Reseed(reseedLimit);
+                }
+                for(var i = 0; i < reqLen * 4; ++i)
+                {
+                    manager.TryWriteDoubleWord(
+                        (long)DRBGRegisters.ResponseDataAddress + (i * 4),
+                        (uint)EmulationManager.Instance.CurrentEmulation.RandomGenerator.Next()
+                    );
+                }
+                if(reseedCounter >= 0)
+                {
+                    reseedCounter--;
+                }
+            }
+
+            public void Reset()
+            {
+                reseedCounter = 0;
+                reqLen = 0;
+            }
+
+            private void Reseed(uint limit)
+            {
+                Logger.Log(LogLevel.Noisy, "Requested seed reset.");
+                // As a simplification, and to ensure execution determinism, we increment the existing seed by one.
+                EmulationManager.Instance.CurrentEmulation.RandomGenerator.ResetSeed(
+                    EmulationManager.Instance.CurrentEmulation.RandomGenerator.GetCurrentSeed() + 1
+                );
+                reseedCounter = limit;
+            }
+
+            private uint reseedCounter;
+            private uint reqLen;
+
+            private readonly InternalMemoryManager manager;
+
+            private enum DRBGRegisters
+            {
+                EntropyFactor = 0x8,
+                KeyOrdinal = 0xC,
+                ReseedLimit = 0x10,
+                IsTestInstantiation = 0x14,
+                AdditionalInputDataLength = 0x18,
+                PersonalizationStringAddress = 0x1C,
+                ContextAddress = 0x20,
+                AdditionalInputDataAddress = 0x188,
+                ResponseDataAddress = 0x8090,
+            }
+        }
+
         private enum JumpTable
         {
             // gaps in addressing - only a few commands are implemented
+            InstantiateDRBG = 0x2C,
+            GenerateBlocksFromDRBG = 0x30,
+            UninstantiateDRBG = 0x32,
             DetectFirmwareVersion = 0x5A
         }
 
