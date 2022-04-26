2e78b4cefa0ec9da802d9387958f7d497c43818c
Michał Szprejda <mszprejda@antmicro.com>
2022-04-26 14:54:29 +0200

    [#33278] Add MiV_CoreLevelInterruptor model

---
+++ src/Emulator/Cores/RiscV/MiV_CoreLevelInterruptor.cs
@@ -0,0 +1,27 @@
+//
+// Copyright (c) 2010-2022 Antmicro
+//
+// This file is licensed under the MIT License.
+// Full license text is available in 'licenses/MIT.txt'.
+//
+using Antmicro.Renode.Core;
+using Antmicro.Renode.Core.Structure.Registers;
+
+namespace Antmicro.Renode.Peripherals.IRQControllers
+{
+    public class MiV_CoreLevelInterruptor : CoreLevelInterruptor, IKnownSize
+    {
+        public MiV_CoreLevelInterruptor(Machine machine, long frequency, uint prescaler, int numberOfTargets = 1)
+            : base(machine, frequency, numberOfTargets)
+        {
+            // we are extending the existing register map defined by the base class
+            AddRegister((long)Registers.Prescaler,
+                new DoubleWordRegister(this).WithValueField(0, 32, FieldMode.Read, valueProviderCallback: _ => prescaler));
+        }
+
+        private enum Registers : long
+        {
+            Prescaler = 0x5000
+        }
+    }
+}
