3d98289c0d74b193604829157c566af6c633e29b
Michał Szprejda <mszprejda@antmicro.com>
2022-04-26 14:53:32 +0200

    [DEV] CLINT: Remove redundant using statements

--- src/Emulator/Cores/RiscV/CoreLevelInterruptor.cs
+++ src/Emulator/Cores/RiscV/CoreLevelInterruptor.cs
@@ -10,9 +10,7 @@ using Antmicro.Renode.Core;
 using Antmicro.Renode.Core.Structure.Registers;
 using Antmicro.Renode.Exceptions;
 using Antmicro.Renode.Peripherals.Bus;
-using Antmicro.Renode.Peripherals.CPU;
 using Antmicro.Renode.Peripherals.Timers;
-using Antmicro.Renode.Time;
 
 namespace Antmicro.Renode.Peripherals.IRQControllers
 {
