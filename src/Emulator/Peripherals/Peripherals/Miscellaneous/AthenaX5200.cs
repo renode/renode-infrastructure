3d2e2437f534e6351c65faedecc918111dbb3b65
Michał Szprejda <mszprejda@antmicro.com>
2022-05-12 15:35:34 +0200

    [#33486] AthenaX5200: Implement AES core

--- src/Emulator/Peripherals/Peripherals/Miscellaneous/AthenaX5200.cs
+++ src/Emulator/Peripherals/Peripherals/Miscellaneous/AthenaX5200.cs
@@ -8,6 +8,7 @@ using System;
 using System.Linq;
 using System.Numerics;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using Antmicro.Renode.Core;
 using Antmicro.Renode.Core.Structure.Registers;
 using Antmicro.Renode.Debugging;
@@ -24,6 +25,7 @@ namespace Antmicro.Renode.Peripherals.Miscellaneous
             memoryManager = new InternalMemoryManager();
             RegistersCollection = new DoubleWordRegisterCollection(this);
             rsaServiceProvider = new RSAServiceProvider(memoryManager);
+            aesServiceProvider = new AESServiceProvider(memoryManager, machine.SystemBus);
 
             Registers.CSR.Define(this)
                 .WithFlag(0,
@@ -78,6 +80,8 @@ namespace Antmicro.Renode.Peripherals.Miscellaneous
                 { JumpTable.ModularExponentationRSA, rsaServiceProvider.ModularExponentation },
                 { JumpTable.ModularReductionRSA, rsaServiceProvider.ModularReduction },
                 { JumpTable.DecryptCipherRSA, rsaServiceProvider.DecryptData },
+                { JumpTable.RunAES, aesServiceProvider.PerformAESOperation },
+                { JumpTable.RunAES_DMA, aesServiceProvider.PerformAESOperationDMA },
             };
 
             Reset();
@@ -155,6 +159,7 @@ namespace Antmicro.Renode.Peripherals.Miscellaneous
         private readonly IEnumRegisterField<JumpTable> operation;
         private readonly IFlagRegisterField coreExecuteCommand;
         private readonly RSAServiceProvider rsaServiceProvider;
+        private readonly AESServiceProvider aesServiceProvider;
 
         private class InternalMemoryManager
         {
@@ -508,6 +513,104 @@ namespace Antmicro.Renode.Peripherals.Miscellaneous
             }
         }
 
+        private class AESServiceProvider
+        {
+            public AESServiceProvider(InternalMemoryManager manager, SystemBus bus)
+            {
+                this.manager = manager;
+                this.bus = bus;
+            }
+
+            public void PerformAESOperation()
+            {
+                manager.TryReadBytes((long)AESRegisters.Key, KeyLengthByteCount, out var keyBytes);
+                manager.TryReadBytes((long)AESRegisters.InitVector, InitializationVectorByteCount, out var ivBytes);
+                manager.TryReadBytes((long)AESRegisters.InputData, InputDataByteCount, out var inputBytes);
+                manager.TryReadDoubleWord((long)AESRegisters.Config, out var config);
+                var operation = BitHelper.IsBitSet(config, 0)
+                       ? Operation.Decryption
+                       : Operation.Encryption;
+
+                // The memory in which Key bytes are stored is little-endian, but both Encryptor and Decryptor require big-endian data
+                keyBytes = Helpers.ChangeEndianness(keyBytes);
+                var result = GetResultBytes(keyBytes, ivBytes, inputBytes, operation);
+
+                manager.TryWriteBytes((long)AESRegisters.Cipher, result);
+            }
+
+            public void PerformAESOperationDMA()
+            {
+                manager.TryReadBytes((long)AESRegisters.Key, KeyLengthByteCount, out var keyBytes);
+                manager.TryReadBytes((long)AESRegisters.InitVectorDMA, InitializationVectorByteCount, out var ivBytes);
+                manager.TryReadDoubleWord((long)AESRegisters.InputDataAddrDMA, out var inputDataAddr);
+                manager.TryReadDoubleWord((long)AESRegisters.ResultDataAddrDMA, out var resultDataAddr);
+                manager.TryReadDoubleWord((long)AESRegisters.ConfigDMA, out var config);
+                var operation = BitHelper.IsBitSet(config, 0)
+                       ? Operation.Decryption
+                       : Operation.Encryption;
+
+                // The memory in which Key bytes are stored is little-endian, but both Encryptor and Decryptor require big-endian data
+                keyBytes = Helpers.ChangeEndianness(keyBytes);
+                bus.WriteBytes(GetResultBytes(keyBytes, ivBytes, bus.ReadBytes(inputDataAddr, CipherByteCount), operation), resultDataAddr);
+            }
+
+            private byte[] GetResultBytes(byte[] keyBytes, byte[] ivBytes, byte[] inputBytes, Operation operation)
+            {
+                return operation == Operation.Encryption
+                    ? Encryption(keyBytes, ivBytes, inputBytes)
+                    : Decryption(keyBytes, ivBytes, inputBytes);
+            }
+
+            private byte[] Encryption(byte[] key, byte[] iv, byte[] input)
+            {                
+                using(var aes = Aes.Create())
+                using(var encryptor = aes.CreateEncryptor(key, iv))
+                {
+                    return encryptor.TransformFinalBlock(input, 0, input.Length);
+                }
+            }
+
+            private byte[] Decryption(byte[] key, byte[] iv, byte[] input)
+            {
+                using(var aes = Aes.Create())
+                {
+                    aes.Padding = PaddingMode.None;
+                    using(var encryptor = aes.CreateDecryptor(key, iv))
+                    {
+                        return encryptor.TransformFinalBlock(input, 0, input.Length);
+                    }
+                }
+            }
+
+            private readonly SystemBus bus;
+            private readonly InternalMemoryManager manager;
+
+            // These values are taken directly from the driver because they are not written to the internal memories
+            private const int KeyLengthByteCount = 32;
+            private const int InitializationVectorByteCount = 16;
+            private const int InputDataByteCount = 16;
+            private const int CipherByteCount = 16;
+
+            private enum Operation
+            {
+                Encryption,
+                Decryption
+            }
+
+            private enum AESRegisters
+            {
+                Config = 0x20,
+                InputDataAddrDMA = 0x28,
+                ResultDataAddrDMA = 0x2C,
+                ConfigDMA = 0x3C,
+                InitVector = 0x8024,
+                InitVectorDMA = 0x8040,
+                Cipher = 0x8048,
+                InputData = 0x8058,
+                Key = 0x9000,
+            }
+        }
+
         private static class Helpers
         {
             public static byte[] ChangeEndianness(byte[] bytes)
@@ -540,8 +643,10 @@ namespace Antmicro.Renode.Peripherals.Miscellaneous
             PrecomputeValueRSA = 0x0,
             ModularExponentationRSA = 0x2,
             ModularReductionRSA = 0x12,
+            RunAES = 0x20,
             InstantiateDRBG = 0x2C,
             GenerateBlocksFromDRBG = 0x30,
+            RunAES_DMA = 0x38,
             UninstantiateDRBG = 0x32,
             DecryptCipherRSA = 0x4E,
             DetectFirmwareVersion = 0x5A
