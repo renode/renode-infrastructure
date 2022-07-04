101932e5e4e2bfc362f224946bccfe183154eecd
Mateusz Holenko <mholenko@antmicro.com>
2022-07-04 08:23:58 +0200

    [#28291] ICpuSupportingGdb: Remove the `Name` property
    This is not used anymore - the name of the CPU can be
    obtained from repl.

--- src/Emulator/Main/Peripherals/CPU/ICpuSupportingGdb.cs
+++ src/Emulator/Main/Peripherals/CPU/ICpuSupportingGdb.cs
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2010-2021 Antmicro
+// Copyright (c) 2010-2022 Antmicro
 //
 // This file is licensed under the MIT License.
 // Full license text is available in 'licenses/MIT.txt'.
@@ -203,7 +203,6 @@ namespace Antmicro.Renode.Peripherals.CPU
         List<GDBFeatureDescriptor> GDBFeatures { get; }
         bool DebuggerConnected { get; set; }
         uint Id { get; }
-        string Name { get; }
     }
 
     public enum MpuAccess
