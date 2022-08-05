9886705195b50183e1146388fb3eb8376193c790
Adam Jeliński <ajelinski@antmicro.com>
2022-08-05 11:33:10 +0200

    [DEV] TimeInterval: Improve properties' readability

--- src/Emulator/Main/Time/TimeInterval.cs
+++ src/Emulator/Main/Time/TimeInterval.cs
@@ -216,11 +216,10 @@ namespace Antmicro.Renode.Time
             }
         }
 
-        public ulong Ticks { get { return ticks; } }
-        public double TotalSeconds { get { return ticks / (double)TicksPerSecond; } }
-        public double TotalMilliseconds { get { return ticks / (double)TicksPerMillisecond; }}
-
-        public ulong TotalMicroseconds { get { return ticks / TicksPerMicrosecond; } }
+        public ulong Ticks => ticks;
+        public ulong TotalMicroseconds => ticks / TicksPerMicrosecond;
+        public double TotalMilliseconds => ticks / (double)TicksPerMillisecond;
+        public double TotalSeconds => ticks / (double)TicksPerSecond;
 
         public const ulong TicksPerSecond = TicksPerMicrosecond * 1000000;
         public const ulong TicksPerMillisecond = TicksPerMicrosecond * 1000;
