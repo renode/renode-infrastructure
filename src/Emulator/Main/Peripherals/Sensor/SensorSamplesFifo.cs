5372f3dd743cfe8685f69a3095804f8b42b11811
Adam Jeliński <ajelinski@antmicro.com>
2022-08-18 15:21:05 +0200

    [#36075] SensorSamplesFifo: Add `DumpOldestSample` method

--- src/Emulator/Main/Peripherals/Sensor/SensorSamplesFifo.cs
+++ src/Emulator/Main/Peripherals/Sensor/SensorSamplesFifo.cs
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2010-2021 Antmicro
+// Copyright (c) 2010-2022 Antmicro
 //
 // This file is licensed under the MIT License.
 // Full license text is available in 'licenses/MIT.txt'.
@@ -19,6 +19,14 @@ namespace Antmicro.Renode.Peripherals.Sensors
         public SensorSamplesFifo()
         {
             samplesFifo = new Queue<T>();
+        }
+
+        public void DumpOldestSample()
+        {
+            lock(samplesFifo)
+            {
+                samplesFifo.TryDequeue<T>(out _);
+            }
         }
 
         public void FeedSample(T sample)
