0c90f07e5d450c33b515c2260fede88ed4d267f8
Mateusz Karlic <mkarlic@internships.antmicro.com>
2022-09-08 13:14:05 +0200

    [#37179] LEDTester: Add AssertAndHoldState

--- src/Emulator/Main/Testing/LEDTester.cs
+++ src/Emulator/Main/Testing/LEDTester.cs
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2010-2020 Antmicro
+// Copyright (c) 2010-2022 Antmicro
 // Copyright (c) 2011-2015 Realtime Embedded
 //
 // This file is licensed under the MIT License.
@@ -11,6 +11,7 @@ using Antmicro.Renode.Peripherals;
 using System.Threading;
 using Antmicro.Renode.Peripherals.Miscellaneous;
 using Antmicro.Renode.Utilities;
+using System.Runtime.CompilerServices;
 
 namespace Antmicro.Renode.Testing
 {
@@ -31,8 +32,7 @@ namespace Antmicro.Renode.Testing
 
         public LEDTester AssertState(bool state, float timeout = 0)
         {
-            var machine = led.GetMachine();
-            var timeoutEvent = machine.LocalTimeSource.EnqueueTimeoutEvent((ulong)(timeout * 1000));
+            var timeoutEvent = GetTimeoutEvent((ulong)(timeout * 1000));
 
             var ev = new ManualResetEvent(false);
             var method = (Action<ILed, bool>)((s, o) => ev.Set());
@@ -57,6 +57,84 @@ namespace Antmicro.Renode.Testing
             }
 
             throw new InvalidOperationException("LED assertion not met.");
+        }
+
+        public LEDTester AssertAndHoldState(bool initialState, float timeoutAssert, float timeoutHold)
+        {
+            var locker = new Object();
+            int numberOfStateChanges = 0;
+            bool isHolding;
+
+            var ev = new AutoResetEvent(false);
+            var method = (Action<ILed, bool>)
+            ((led, currState) => 
+            {
+                lock(locker)
+                {
+                    ++numberOfStateChanges;
+                    ev.Set();
+                }
+            });
+
+            try
+            {
+                float timeout;
+                // this needs to be treated as atomic block so the state doesn't change during initialization
+                lock(locker)
+                {
+                    led.StateChanged += method;
+                    isHolding = initialState == led.State;
+                    timeout = isHolding ? timeoutHold : timeoutAssert;
+                }
+                var timeoutEvent = GetTimeoutEvent((ulong)(timeout * 1000));
+                
+                do
+                {
+                    var eventSrc = WaitHandle.WaitAny( new [] { timeoutEvent.WaitHandle, ev } );
+
+                    if(isHolding)
+                    {
+                        if(numberOfStateChanges > 0)
+                        {
+                            throw new InvalidOperationException("LED changed state.");
+                        }
+                    }
+                    else
+                    {
+                        lock(locker)
+                        {
+                            if(numberOfStateChanges == 1)
+                            {
+                                isHolding = true;
+                                timeoutEvent = GetTimeoutEvent((ulong)(timeoutHold * 1000));
+                                --numberOfStateChanges;
+                            }
+                            else if(eventSrc == 0)
+                            {
+                                throw new InvalidOperationException("Initial LED assertion not met.");
+                            }
+                            else
+                            {
+                                throw new InvalidOperationException("LED changed state.");
+                            }
+                        }
+                    }
+                }
+                while((!timeoutEvent.IsTriggered && isHolding) || !isHolding);
+            }
+            finally
+            {
+                led.StateChanged -= method;
+                ev.Dispose();
+            }
+
+            return this;
+        }
+
+        [MethodImplAttribute(MethodImplOptions.AggressiveInlining)] 
+        private TimeoutEvent GetTimeoutEvent(ulong timeout)
+        {
+            return led.GetMachine().LocalTimeSource.EnqueueTimeoutEvent(timeout);
         }
 
         private readonly ILed led;
