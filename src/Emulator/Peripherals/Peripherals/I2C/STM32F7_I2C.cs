6b6b1d8ab481b26286ee157d10de480ece229890
Michał Szprejda <mszprejda@antmicro.com>
2022-10-18 13:30:51 +0200

    [#37330] STM32F7_I2C: Call FinishTransmission on slave when stop condition occurs

--- src/Emulator/Peripherals/Peripherals/I2C/STM32F7_I2C.cs
+++ src/Emulator/Peripherals/Peripherals/I2C/STM32F7_I2C.cs
@@ -371,6 +371,7 @@ namespace Antmicro.Renode.Peripherals.I2C
             }
             if(autoEnd.Value)
             {
+                currentSlave.FinishTransmission();
                 stopDetection.Value = true;
                 masterMode = false;
             }
