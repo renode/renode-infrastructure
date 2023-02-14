1c9ff897f03cb99a404bc4914cf2a0f2a3b9db21
Wojciech Meller <wmeller@antmicro.com>
2023-02-14 11:26:27 +0100

    [#41251] OffsetAttribute: Add more arguments
    Add quadWords, doubleWords and words to improve readability

--- src/Emulator/Main/Utilities/Packets/OffsetAttribute.cs
+++ src/Emulator/Main/Utilities/Packets/OffsetAttribute.cs
@@ -1,19 +1,27 @@
 //
-// Copyright (c) 2010-2022 Antmicro
+// Copyright (c) 2010-2023 Antmicro
 //
 //  This file is licensed under the MIT License.
 //  Full license text is available in 'licenses/MIT.txt'.
 //
 using System;
+using System.Linq;
 
 namespace Antmicro.Renode.Utilities.Packets
 {
     [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
     public sealed class OffsetAttribute : Attribute
     {
-        public OffsetAttribute(uint bytes = 0, uint bits = 0)
+        public OffsetAttribute(uint quadWords = 0, uint doubleWords = 0, uint words = 0, uint bytes = 0, uint bits = 0)
         {
-            OffsetInBytes = bytes + (bits >> 3);
+            OffsetInBytes = new[]
+            {
+                (quadWords << 3),
+                (doubleWords << 2),
+                (words << 1),
+                bytes,
+                (bits >> 3)
+            }.Aggregate((a, b) => a + b);
             OffsetInBits = bits & 0x7;
         }
 
