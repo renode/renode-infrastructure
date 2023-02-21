8e9cfeb8119f70bcefa52cdfe5513efc60886c39
Mateusz Karlic <mkarlic@antmicro.com>
2023-02-21 12:02:52 +0100

    [FIX] DynamicModulesSpawner: Fix indentation

--- src/Emulator/Extensions/TAPHelper/DynamicModuleSpawner.cs
+++ src/Emulator/Extensions/TAPHelper/DynamicModuleSpawner.cs
@@ -36,7 +36,7 @@ namespace Antmicro.Renode.TAPHelper
             FileCopier.Copy(emulatorAssemblySourcePath, emulatorAssemblyPath, true);
 
             // Generate binary
-			GenerateTAPHelper(dirName, filName, extensionsAssemblyPath);
+            GenerateTAPHelper(dirName, filName, extensionsAssemblyPath);
 
             return generatedFileName;
         }
@@ -54,7 +54,7 @@ namespace Antmicro.Renode.TAPHelper
             var typeBuilder = moduleBuilder.DefineType("TAPHelper", TypeAttributes.Public|TypeAttributes.Class);
             var mainMethodBuilder = typeBuilder.DefineMethod("Main", MethodAttributes.Public | MethodAttributes.Static, typeof(int), new Type[] { typeof(string[]) });
             
-			//
+            //
             // Main method
             //
             var generator = mainMethodBuilder.GetILGenerator();
@@ -62,16 +62,16 @@ namespace Antmicro.Renode.TAPHelper
             var setReturnFail = generator.DefineLabel();
             var freeMemoryAndFinish = generator.DefineLabel();
 
-			// load the proper assembly
-			generator.Emit(OpCodes.Ldtoken, typeof(Func<IntPtr, bool, int>));
-			generator.Emit(OpCodes.Call, typeof(Type).GetMethod("GetTypeFromHandle"));
-			generator.Emit(OpCodes.Ldstr, extensionsAssemblyPath);
-			generator.Emit(OpCodes.Call, typeof(Assembly).GetMethod("LoadFrom", new [] { typeof(string) }));
-			generator.Emit(OpCodes.Ldstr, "Antmicro.Renode.TAPHelper.TAPTools");
-			generator.Emit(OpCodes.Callvirt, typeof(Assembly).GetMethod("GetType", new [] { typeof(string) }));
-			generator.Emit(OpCodes.Ldstr, "OpenTAP");
-			generator.Emit(OpCodes.Call, typeof(Delegate).GetMethod("CreateDelegate", new [] { typeof(Type), typeof(Type), typeof(string) }));
-			generator.Emit(OpCodes.Castclass, typeof(Func<IntPtr, bool, int>));
+            // load the proper assembly
+            generator.Emit(OpCodes.Ldtoken, typeof(Func<IntPtr, bool, int>));
+            generator.Emit(OpCodes.Call, typeof(Type).GetMethod("GetTypeFromHandle"));
+            generator.Emit(OpCodes.Ldstr, extensionsAssemblyPath);
+            generator.Emit(OpCodes.Call, typeof(Assembly).GetMethod("LoadFrom", new [] { typeof(string) }));
+            generator.Emit(OpCodes.Ldstr, "Antmicro.Renode.TAPHelper.TAPTools");
+            generator.Emit(OpCodes.Callvirt, typeof(Assembly).GetMethod("GetType", new [] { typeof(string) }));
+            generator.Emit(OpCodes.Ldstr, "OpenTAP");
+            generator.Emit(OpCodes.Call, typeof(Delegate).GetMethod("CreateDelegate", new [] { typeof(Type), typeof(Type), typeof(string) }));
+            generator.Emit(OpCodes.Castclass, typeof(Func<IntPtr, bool, int>));
 
             // push device name on stack
             generator.Emit(OpCodes.Ldarg_0);
@@ -88,7 +88,7 @@ namespace Antmicro.Renode.TAPHelper
             generator.Emit(OpCodes.Call, typeof(bool).GetMethod("Parse", new [] { typeof(string) }));
 
             // call OpenTAP method
-			generator.Emit(OpCodes.Callvirt, typeof(Func<IntPtr, bool, int>).GetMethod("Invoke", new [] { typeof(IntPtr), typeof(bool) }));
+            generator.Emit(OpCodes.Callvirt, typeof(Func<IntPtr, bool, int>).GetMethod("Invoke", new [] { typeof(IntPtr), typeof(bool) }));
 
             generator.Emit(OpCodes.Ldc_I4, 0);
             generator.Emit(OpCodes.Blt, setReturnFail);
@@ -114,7 +114,7 @@ namespace Antmicro.Renode.TAPHelper
             generator.MarkLabel(freeMemoryAndFinish);
             generator.Emit(OpCodes.Ldloc, intptrLocale);
             generator.Emit(OpCodes.Call, typeof(Marshal).GetMethod("FreeCoTaskMem"));
-			
+            
             generator.Emit(OpCodes.Ret);
 
             typeBuilder.CreateType();
