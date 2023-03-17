30f3ec1738d93d10e3efb7b7e6e21525f0bb3744
Mateusz Holenko <mholenko@antmicro.com>
2023-03-17 16:41:26 +0100

    [#42830] LEDTester: Add duty cycle detection

--- src/Emulator/Main/Testing/LEDTester.cs
+++ src/Emulator/Main/Testing/LEDTester.cs
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2010-2022 Antmicro
+// Copyright (c) 2010-2023 Antmicro
 // Copyright (c) 2011-2015 Realtime Embedded
 //
 // This file is licensed under the MIT License.
@@ -11,6 +11,7 @@ using Antmicro.Renode.Peripherals;
 using System.Threading;
 using Antmicro.Renode.Peripherals.Miscellaneous;
 using Antmicro.Renode.Utilities;
+using Antmicro.Renode.Time;
 using System.Runtime.CompilerServices;
 
 namespace Antmicro.Renode.Testing
@@ -131,10 +132,87 @@ namespace Antmicro.Renode.Testing
             return this;
         }
 
+        public LEDTester AssertDutyCycle(float testDuration, double expectedDutyCycle, double tolerance = 0.05)
+        {
+            ulong lowTicks = 0;
+            ulong highTicks = 0;
+
+            var method = MakeStateChangeHandler((currState, dt) =>
+            {
+                if(currState)
+                {
+                    // we switch to high, so up to this point it was low
+                    lowTicks += dt.Ticks;
+                }
+                else
+                {
+                    highTicks += dt.Ticks;
+                }
+            });
+
+            try
+            {
+                led.StateChanged += method;
+
+                var timeoutEvent = GetTimeoutEvent((ulong)(testDuration * 1000));
+                timeoutEvent.WaitHandle.WaitOne();
+
+                var highPercentage = (double)highTicks / (highTicks + lowTicks) * 100;
+                if(highPercentage < expectedDutyCycle - (tolerance * 100) || expectedDutyCycle > expectedDutyCycle + (tolerance * 100))
+                {
+                    throw new InvalidOperationException($"Fill assertion not met: expected {expectedDutyCycle} with tolerance {tolerance * 100}%, but got {highPercentage}");
+                }
+            }
+            finally
+            {
+                led.StateChanged -= method;
+            }
+
+            return this;
+        }
+
         [MethodImplAttribute(MethodImplOptions.AggressiveInlining)]
         private TimeoutEvent GetTimeoutEvent(ulong timeout)
         {
             return led.GetMachine().LocalTimeSource.EnqueueTimeoutEvent(timeout);
+        }
+
+        private Action<ILed, bool> MakeStateChangeHandler(Action<bool, TimeInterval> stateChanged)
+        {
+            TimeStamp? previousEventTimestamp = null;
+            return (Action<ILed, bool>)((led, currState) =>
+            {
+                if(!TimeDomainsManager.Instance.TryGetVirtualTimeStamp(out var vts))
+                {
+                    throw new InvalidOperationException("Couldn't obtain virtual time");
+                }
+
+                // first we need to "sync" with the first state change;
+                // it doesn't matter if that's low/high or high/low transition
+                if(previousEventTimestamp == null)
+                {
+                    previousEventTimestamp = vts;
+                    return;
+                }
+
+                TimeInterval dt;
+                // TODO: Below `if` block is a way to avoid TimeInterval underflow and the resulting exception
+                // that occurs when the OnGPIO comes from a different thread(core) than expected.
+                // This should be fixed by making sure we always get the Timestamp from the correct thread.
+                // Until then the results will be radomly less accurate.
+                if(vts.TimeElapsed > previousEventTimestamp.Value.TimeElapsed)
+                {
+                    dt = vts.TimeElapsed - previousEventTimestamp.Value.TimeElapsed;
+                }
+                else
+                {
+                    dt = TimeInterval.Empty;
+                }
+
+                stateChanged.Invoke(currState, dt);
+
+                previousEventTimestamp = vts;
+            });
         }
 
         private readonly ILed led;
