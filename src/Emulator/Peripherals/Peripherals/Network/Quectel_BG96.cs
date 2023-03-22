2fdf08565e914b069ed96983a656db84d4770345
Piotr Wojnarowski <pwojnarowski@antmicro.com>
2023-03-22 22:53:30 +0100

    [#42415] Quectel_BG96: Add additional command stubs

--- src/Emulator/Peripherals/Peripherals/Network/Quectel_BG96.cs
+++ src/Emulator/Peripherals/Peripherals/Network/Quectel_BG96.cs
@@ -16,6 +16,41 @@ namespace Antmicro.Renode.Peripherals.Network
         {
         }
 
+        // QIACT - Activate a PDP Context
+        [AtCommand("AT+QIACT", CommandType.Write)]
+        protected Response Qiact(int contextId)
+        {
+            if(!IsValidContextId(contextId))
+            {
+                return Error;
+            }
+            return Ok; // stub
+        }
+
+        // QICSGP - Configure Parameters of a TCP/IP Context
+        [AtCommand("AT+QICSGP", CommandType.Write)]
+        protected virtual Response Qicsgp(int contextId, ProtocolType contextType = ProtocolType.IpV4,
+            string apn = "", string username = "", string password = "",
+            AuthenticationMethod authenticationType = AuthenticationMethod.None)
+        {
+            if(!IsValidContextId(contextId))
+            {
+                return Error;
+            }
+            return Ok; // stub
+        }
+
+        // QIDEACT - Deactivate a PDP Context
+        [AtCommand("AT+QIDEACT", CommandType.Write)]
+        protected virtual Response Qideact(int contextId)
+        {
+            if(!IsValidContextId(contextId))
+            {
+                return Error;
+            }
+            return Ok; // stub
+        }
+
         protected override string Vendor => "Quectel";
         protected override string ModelName => "BG96";
         protected override string Revision => "Revision: BG96MAR01A01M1G";
@@ -25,5 +60,19 @@ namespace Antmicro.Renode.Peripherals.Network
         private const string DefaultImeiNumber = "866818039921444";
         private const string DefaultSoftwareVersionNumber = "31";
         private const string DefaultSerialNumber = "<serial number>";
+
+        protected enum ProtocolType
+        {
+            IpV4 = 1,
+            IpV4V6,
+        }
+
+        protected enum AuthenticationMethod
+        {
+            None,
+            Pap,
+            Chap,
+            PapOrChap,
+        }
     }
 }
