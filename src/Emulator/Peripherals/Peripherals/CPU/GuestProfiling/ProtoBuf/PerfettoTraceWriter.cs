393bfddc70c4509df4a011224adf54212663dcae
Piotr Wojnarowski <pwojnarowski@antmicro.com>
2023-04-25 13:32:18 +0200

    [#44181] PerfettoTraceWriter: Loosen Stream type in FlushBuffer

--- src/Emulator/Peripherals/Peripherals/CPU/GuestProfiling/ProtoBuf/PerfettoTraceWriter.cs
+++ src/Emulator/Peripherals/Peripherals/CPU/GuestProfiling/ProtoBuf/PerfettoTraceWriter.cs
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2010-2022 Antmicro
+// Copyright (c) 2010-2023 Antmicro
 //
 // This file is licensed under the MIT License.
 // Full license text is available in 'licenses/MIT.txt'.
@@ -118,7 +118,7 @@ namespace Antmicro.Renode.Peripherals.CPU.GuestProfiling.ProtoBuf
             });
         }
 
-        public void FlushBuffer(FileStream fileStream)
+        public void FlushBuffer(Stream fileStream)
         {
             if(lastSyncMarker >= SyncMarkerInterval)
             {
