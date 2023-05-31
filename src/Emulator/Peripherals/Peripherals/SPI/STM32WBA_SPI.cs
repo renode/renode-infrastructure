91f35affffbebdd87e7d79c18ea771d94a48d190
Piotr Wojnarowski <pwojnarowski@antmicro.com>
2023-05-31 13:23:22 +0200

    [#45214] STM32WBA_SPI: Add peripheral

---
+++ src/Emulator/Peripherals/Peripherals/SPI/STM32WBA_SPI.cs
@@ -0,0 +1,19 @@
+//
+// Copyright (c) 2010-2023 Antmicro
+//
+// This file is licensed under the MIT License.
+// Full license text is available in 'licenses/MIT.txt'.
+//
+using Antmicro.Renode.Core;
+
+namespace Antmicro.Renode.Peripherals.SPI
+{
+    public class STM32WBA_SPI : STM32H7_SPI
+    {
+        public STM32WBA_SPI(Machine machine) : base(machine)
+        {
+        }
+
+        protected override bool IsWba { get; } = true;
+    }
+}
