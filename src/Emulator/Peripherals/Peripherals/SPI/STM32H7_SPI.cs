48336a1e470760577349ad7c0d342db3bd65c285
Piotr Wojnarowski <pwojnarowski@antmicro.com>
2023-05-31 17:28:53 +0200

    [#45214] STM32H7_SPI: Don't expose H7-only registers on WBA

--- src/Emulator/Peripherals/Peripherals/SPI/STM32H7_SPI.cs
+++ src/Emulator/Peripherals/Peripherals/SPI/STM32H7_SPI.cs
@@ -129,13 +129,20 @@ namespace Antmicro.Renode.Peripherals.SPI
 
             Registers.Control2.Define(registers)
                 .WithValueField(0, 16, out transmissionSize, name: "TSIZE")
-                .WithTag("TSER", 16, 16);
+                .If(IsWba)
+                    .Then(r => r.WithReservedBits(16, 16))
+                    .Else(r => r.WithTag("TSER", 16, 16));
 
             Registers.Configuration1.Define(registers)
                 .WithValueField(0, 5, out packetSizeBits, name: "DSIZE")
                 .WithTag("FTHLV", 5, 4)
-                .WithTag("UDRCFG", 9, 2)
-                .WithTag("UDRDET", 11, 2)
+                .If(IsWba)
+                    .Then(r => r
+                        .WithTaggedFlag("UDRCFG", 9)
+                        .WithReservedBits(10, 3))
+                    .Else(r => r
+                        .WithTag("UDRCFG", 9, 2)
+                        .WithTag("UDRDET", 11, 2))
                 .WithReservedBits(13, 1)
                 .WithFlag(14, out receiveDMAEnabled, name: "RXDMAEN")
                 // Software expects this value to be as it was set. Transmitting with DMA doesn't require any special logic
@@ -145,12 +152,19 @@ namespace Antmicro.Renode.Peripherals.SPI
                 .WithTaggedFlag("CRCEN", 22)
                 .WithReservedBits(23, 5)
                 .WithTag("MBR", 28, 3)
-                .WithReservedBits(31, 1);
+                .If(IsWba)
+                    .Then(r => r.WithTaggedFlag("BPASS", 31))
+                    .Else(r => r.WithReservedBits(31, 1));
 
             Registers.Configuration2.Define(registers)
                 .WithTag("MSSI", 0, 4)
                 .WithTag("MIDI", 4, 4)
-                .WithReservedBits(8, 7)
+                .WithReservedBits(8, 5)
+                .If(IsWba)
+                    .Then(r => r
+                        .WithTaggedFlag("RDIOM", 13)
+                        .WithTaggedFlag("RDIOP", 14))
+                    .Else(r => r.WithReservedBits(13, 2))
                 .WithTaggedFlag("IOSWP", 15)
                 .WithReservedBits(16, 1)
                 .WithTag("COMM", 17, 2)
@@ -187,7 +201,9 @@ namespace Antmicro.Renode.Peripherals.SPI
                 .WithTaggedFlag("CRCE", 7)
                 .WithTaggedFlag("TIFRE", 8)
                 .WithTaggedFlag("MODF", 9)
-                .WithTaggedFlag("TSERF", 10)
+                .If(IsWba)
+                    .Then(r => r.WithReservedBits(10, 1))
+                    .Else(r => r.WithTaggedFlag("TSERF", 10))
                 .WithTaggedFlag("SUSP", 11)
                 .WithFlag(12, FieldMode.Read, name: "TXC",
                     valueProviderCallback: _ => transmissionSize.Value == 0 ? transmitFifo.Count == 0 : endOfTransfer.Value)
@@ -210,7 +226,9 @@ namespace Antmicro.Renode.Peripherals.SPI
                 .WithTaggedFlag("CRCEC", 7)
                 .WithTaggedFlag("TIFREC", 8)
                 .WithTaggedFlag("MODFC", 9)
-                .WithTaggedFlag("TSERFC", 10)
+                .If(IsWba)
+                    .Then(r => r.WithReservedBits(10, 1))
+                    .Else(r => r.WithTaggedFlag("TSERFC", 10))
                 .WithTaggedFlag("SUSPC", 11)
                 .WithReservedBits(12, 20);
 
@@ -232,29 +250,32 @@ namespace Antmicro.Renode.Peripherals.SPI
                         return value;
                     });
 
-            Registers.I2SConfiguration.Define(registers)
-                .WithFlag(0, name: "I2SMOD", valueProviderCallback: _ => false, writeCallback: (_, value) =>
-                    {
-                        if(value)
+            if(!IsWba)
+            {
+                Registers.I2SConfiguration.Define(registers)
+                    .WithFlag(0, name: "I2SMOD", valueProviderCallback: _ => false, writeCallback: (_, value) =>
                         {
-                            this.Log(LogLevel.Error, "Attempted to enable I2S. This mode is not supported");
-                        }
-                    })
-                .WithTag("I2SCFG[2:0]", 1, 3)
-                .WithTag("I2SSTD[1:0]", 4, 2)
-                .WithReservedBits(6, 1)
-                .WithTaggedFlag("PCMSYNC", 7)
-                .WithTag("DATLEN[1:0]", 8, 2)
-                .WithTaggedFlag("CHLEN", 10)
-                .WithTaggedFlag("CKPOL", 11)
-                .WithTaggedFlag("FIXCH", 12)
-                .WithTaggedFlag("WSINV", 13)
-                .WithTaggedFlag("DATFMT", 14)
-                .WithReservedBits(15, 1)
-                .WithTag("I2SDIV[7:0]", 16, 8)
-                .WithTaggedFlag("ODD", 24)
-                .WithTaggedFlag("MCKOE", 25)
-                .WithReservedBits(26, 6);
+                            if(value)
+                            {
+                                this.Log(LogLevel.Error, "Attempted to enable I2S. This mode is not supported");
+                            }
+                        })
+                    .WithTag("I2SCFG[2:0]", 1, 3)
+                    .WithTag("I2SSTD[1:0]", 4, 2)
+                    .WithReservedBits(6, 1)
+                    .WithTaggedFlag("PCMSYNC", 7)
+                    .WithTag("DATLEN[1:0]", 8, 2)
+                    .WithTaggedFlag("CHLEN", 10)
+                    .WithTaggedFlag("CKPOL", 11)
+                    .WithTaggedFlag("FIXCH", 12)
+                    .WithTaggedFlag("WSINV", 13)
+                    .WithTaggedFlag("DATFMT", 14)
+                    .WithReservedBits(15, 1)
+                    .WithTag("I2SDIV[7:0]", 16, 8)
+                    .WithTaggedFlag("ODD", 24)
+                    .WithTaggedFlag("MCKOE", 25)
+                    .WithReservedBits(26, 6);
+            }
         }
 
         private bool CanWriteToRegister(Registers reg, uint value)
@@ -354,13 +375,14 @@ namespace Antmicro.Renode.Peripherals.SPI
             InterruptEnable = 0x10,             // SPI_IER
             Status = 0x14,                      // SPI_SR
             InterruptStatusFlagsClear = 0x18,   // SPI_IFCR
+            AutonomousModeControl = 0x1C,       // SPI_AUTOCR (WBA only)
             TransmitData = 0x20,                // SPI_TXDR
             ReceiveData = 0x30,                 // SPI_RXDR
             CRCPolynomial = 0x40,               // SPI_CRCPOLY
             TransmitterCRC = 0x44,              // SPI_TXCRC
             ReceiverCRC = 0x48,                 // SPI_RXCRC
             UnderrunData = 0x4C,                // SPI_UDRDR
-            I2SConfiguration = 0x50,            // SPI_I2SCFGR
+            I2SConfiguration = 0x50,            // SPI_I2SCFGR (non-WBA only)
         }
     }
 }
