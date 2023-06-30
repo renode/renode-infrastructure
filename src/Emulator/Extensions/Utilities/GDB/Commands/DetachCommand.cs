3dd778414bfedf3f8e6e4dfeba5252459803cba8
Piotr Wojnarowski <pwojnarowski@antmicro.com>
2023-06-30 16:48:41 +0200

    [#46273] GDB: Add Detach command

---
+++ src/Emulator/Extensions/Utilities/GDB/Commands/DetachCommand.cs
@@ -0,0 +1,26 @@
+﻿//
+// Copyright (c) 2010-2023 Antmicro
+//
+//  This file is licensed under the MIT License.
+//  Full license text is available in 'licenses/MIT.txt'.
+//
+using Antmicro.Renode.Utilities.GDB;
+
+namespace Antmicro.Renode.Extensions.Utilities.GDB.Commands
+{
+    public class DetachCommand : Command, IMultithreadCommand
+    {
+        public DetachCommand(CommandsManager manager) : base(manager)
+        {
+        }
+
+        // This command is only there so that GDB doesn't report that the
+        // "Remote doesn't know how to detach". It doesn't need to do anything,
+        // the actual detaching/cleanup is done when the connection is closed.
+        [Execute("D")]
+        public PacketData Execute()
+        {
+            return PacketData.Success;
+        }
+    }
+}
