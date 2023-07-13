b2bfc0c704d4e720af81094aa483ffd78b70b54b
Nikodem Rabuliński <nrabulinski@antmicro.com>
2023-07-13 15:01:05 +0200

    [#46523] STM32WBA_ADC: Added STM32WBA series ADC peripheral

---
+++ src/Emulator/Peripherals/Peripherals/Analog/STM32WBA_ADC.cs
@@ -0,0 +1,33 @@
+//
+// Copyright (c) 2010-2023 Antmicro
+//
+// This file is licensed under the MIT License.
+// Full license text is available in 'licenses/MIT.txt'.
+//
+
+using Antmicro.Renode.Core;
+using Antmicro.Renode.Peripherals.DMA;
+
+namespace Antmicro.Renode.Peripherals.Analog
+{
+    public class STM32WBA_ADC : STM32_ADC_Common
+    {
+        public STM32WBA_ADC(Machine machine, double referenceVoltage, uint externalEventFrequency, int dmaChannel = 0, IDMA dmaPeripheral = null)
+            : base(
+                machine,
+                referenceVoltage,
+                externalEventFrequency,
+                dmaChannel,
+                dmaPeripheral,
+                // Base class configuration
+                watchdogCount: 3,
+                hasCalibration: true,
+                channelCount: 14,
+                hasPrescaler: true,
+                hasVbatPin: false,
+                hasChannelSequence: true,
+                hasPowerRegister: true
+            )
+        {}
+    }
+}
