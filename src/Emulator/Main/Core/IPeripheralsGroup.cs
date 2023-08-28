f8ea53573a2fbfb8585a098e1f4d1650da902701
Mateusz Holenko <mholenko@antmicro.com>
2023-08-28 14:03:43 +0200

    [FIX] IPeripheralsGroup: Fix formatting

--- src/Emulator/Main/Core/IPeripheralsGroup.cs
+++ src/Emulator/Main/Core/IPeripheralsGroup.cs
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2010-2018 Antmicro
+// Copyright (c) 2010-2023 Antmicro
 // Copyright (c) 2011-2015 Realtime Embedded
 //
 // This file is licensed under the MIT License.
@@ -12,7 +12,7 @@ namespace Antmicro.Renode.Core
 {
     public interface IPeripheralsGroup
     {
-        string Name { get; } 
+        string Name { get; }
         Machine Machine { get; }
         IEnumerable<IPeripheral> Peripherals { get; }
 
