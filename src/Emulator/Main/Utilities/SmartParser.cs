e8323d401ab990b039b5dff7d7e4c99df60a40ab
Mateusz Holenko <mholenko@antmicro.com>
2023-09-13 14:55:57 +0200

    [FIX] SmartParser: Fix formatting

--- src/Emulator/Main/Utilities/SmartParser.cs
+++ src/Emulator/Main/Utilities/SmartParser.cs
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2010-2018 Antmicro
+// Copyright (c) 2010-2023 Antmicro
 //
 // This file is licensed under the MIT License.
 // Full license text is available in 'licenses/MIT.txt'.
@@ -113,7 +113,7 @@ namespace Antmicro.Renode.Utilities
                 return false;
             }
         }
-        
+
         private static bool TryGetConvertMethodDelegate(Type type, int fromBase, out Delegate result)
         {
             var parameters = new [] { typeof(string), typeof(int) };
