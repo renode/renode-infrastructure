a8d7118fba751153738f6e0f6bbbb9edd8d695ea
Jan Malek <jmalek@antmicro.com>
2023-10-03 12:20:12 +0200

    [#49891] NANDFlash: Refactor implementation

--- src/Emulator/Peripherals/Peripherals/SPI/NANDFlash.cs
+++ src/Emulator/Peripherals/Peripherals/SPI/NANDFlash.cs
@@ -1,15 +1,16 @@
 //
+// Copyright (c) 2010-2023 Antmicro
+//
 // This file is licensed under the MIT License.
 // Full license text is available in 'licenses/MIT.txt'.
 //
-
 using System;
 using System.Collections.Generic;
 using Antmicro.Renode.Core.Structure.Registers;
-using Antmicro.Renode.Peripherals.SPI;
 using Antmicro.Renode.Logging;
 using Antmicro.Renode.Peripherals.Bus.Wrappers;
 using Antmicro.Renode.Peripherals.Memory;
+using Antmicro.Renode.Utilities;
 
 namespace Antmicro.Renode.Peripherals.SPI
 {
@@ -25,8 +26,6 @@ namespace Antmicro.Renode.Peripherals.SPI
 
             RegistersCollection = new ByteRegisterCollection(this);
 
-            DefineRegisters();
-
             // Use separate data & spare areas so we can easily program
             // the data memory from a bin/hex file
             this.dataMemory = dataMemory;
@@ -41,7 +40,117 @@ namespace Antmicro.Renode.Peripherals.SPI
             this.lunsPerChip = lunsPerChip;
 
             this.pageCache = new byte[pageSize + spareSize];
+
+            DefineRegisters();
+            Reset();
         }
+
+        public void Reset()
+        {
+            RegistersCollection.Reset();
+            state = State.Idle;
+
+            receiveStack.Clear();
+            sendQueue.Clear();
+        }
+
+        public byte Transmit(byte data)
+        {
+            this.Log(LogLevel.Noisy, "Received byte: 0x{0:X} in state {1}", data, state);
+            byte result = 0;
+
+            switch(state)
+            {
+            case State.Idle:
+                HandleCommand((Command)data);
+                break;
+            case State.Unsupported:
+                break;
+            case State.GetFeatureAddress:
+            {
+                var r = RegistersCollection.Read(data);
+                sendQueue.Enqueue(r);
+                this.Log(LogLevel.Noisy, "Reading register {0} = {1}", data, r);
+                state = State.TransmitQueue;
+                break;
+            }
+            case State.GetReadColumn:
+                receiveStack.Push(data);
+                result = sendQueue.Dequeue();
+                if(sendQueue.Count == 0)
+                {
+                    column = RxDequeueAsUInt();
+                    this.Log(LogLevel.Noisy, "Column set to {0}", column);
+                    state = State.TransmitCache;
+                    sendQueue.Enqueue(0);
+                }
+                break;
+            case State.TransmitCache:
+                sendQueue.Enqueue(pageCache[column++]);
+                result = sendQueue.Dequeue();
+                break;
+            case State.SetFeatureAddress:
+                receiveStack.Push(data);
+                state = State.SetFeatureData;
+                break;
+            case State.SetFeatureData:
+                if(receiveStack.Count == 0)
+                {
+                    this.Log(LogLevel.Error, "Trailing bytes on SetFeature command");
+                }
+                else
+                {
+                    var r = (long)receiveStack.Pop();
+                    RegistersCollection.Write(r, data);
+                    this.Log(LogLevel.Noisy, "Setting register {0} = {1}", (long)r, data);
+                }
+                break;
+            case State.TransmitQueue:
+                receiveStack.Push(data);
+                if(sendQueue.Count == 0)
+                {
+                    this.Log(LogLevel.Warning, "Nothing left in send queue");
+                }
+                else
+                {
+                    result = sendQueue.Dequeue();
+                }
+                break;
+            default:
+                this.Log(LogLevel.Error, "Received byte 0x{0:X} in an unexpected state: {1}. Ignoring it...", data, state);
+                break;
+            }
+
+            this.Log(LogLevel.Noisy, "Returning byte: 0x{0:X}", result);
+            return result;
+        }
+
+        public void FinishTransmission()
+        {
+            this.Log(LogLevel.Noisy, "Transmission finished");
+
+            if(lastCommand == Command.PageRead)
+            {
+                if(receiveStack.Count != 3)
+                {
+                    this.Log(LogLevel.Error, "Incorrect byte count for PageRead {0}", receiveStack.Count);
+                }
+                else
+                {
+                    uint page = RxDequeueAsUInt();
+                    this.Log(LogLevel.Noisy, "Loading page {0} from cache", page);
+                    uint offset = page * pageSize;
+                    dataMemory.ReadBytes(offset, (int)pageSize, pageCache, 0);
+                    // TODO: Copy spareMemory to pageCache
+                }
+            }
+
+            receiveStack.Clear();
+            sendQueue.Clear();
+            state = State.Idle;
+        }
+
+        public ByteRegisterCollection RegistersCollection { get; }
 
         private void DefineRegisters()
         {
@@ -59,15 +168,6 @@ namespace Antmicro.Renode.Peripherals.SPI
             ;
         }
 
-        public void Reset()
-        {
-            RegistersCollection.Reset();
-            state = State.Idle;
-
-            receiveStack.Clear();
-            sendQueue.Clear();
-        }
-
         private uint RxDequeueAsUInt()
         {
             uint output = 0;
@@ -79,48 +179,18 @@ namespace Antmicro.Renode.Peripherals.SPI
             return output;
         }
 
-        public void FinishTransmission()
+        private void HandleCommand(Command command)
         {
-            this.Log(LogLevel.Noisy, "Transmission finished");
+            lastCommand = command;
+            byte[] bytesToLoad = null;
 
-            switch (lastCmd)
-            {
-            case Command.PageRead:
-                if (receiveStack.Count != 3)
-                {
-                    this.Log(LogLevel.Error, "Incorrect byte count for PageRead {0}", receiveStack.Count);
-                }
-                else
-                {
-                    uint page = RxDequeueAsUInt();
-                    this.Log(LogLevel.Info, "Loading page {0} from cache", page);
-                    uint offset = page * pageSize;
-                    dataMemory.ReadBytes(offset, (int)pageSize, pageCache, 0);
-                    // TODO: Copy spareMemory to pageCache
-                }
-                break;
-            default:
-                break;
-            }
-            receiveStack.Clear();
-            sendQueue.Clear();
-            state = State.Idle;
-        }
-
-        private void HandleCommandByte(byte data)
-        {
-            var cmd = (Command)data;
-            lastCmd = cmd;
-
-            switch (cmd)
+            switch(command)
             {
             case Command.Reset:
                 Reset();
                 break;
             case Command.ReadId:
-                sendQueue.Enqueue((byte)0);
-                sendQueue.Enqueue((byte)0xef);
-                sendQueue.Enqueue((byte)0xbc);
+                bytesToLoad = ReadIdBytes;
                 state = State.TransmitQueue;
                 break;
             case Command.GetFeature:
@@ -130,90 +200,48 @@ namespace Antmicro.Renode.Peripherals.SPI
                 state = State.SetFeatureAddress;
                 break;
             case Command.PageRead:
-                sendQueue.Enqueue((byte)0);
-                sendQueue.Enqueue((byte)0);
-                sendQueue.Enqueue((byte)0);
+                bytesToLoad = PageReadBytes;
                 state = State.TransmitQueue;
                 break;
             case Command.ReadFromCache:
             case Command.FastReadFromCache:
-                sendQueue.Enqueue((byte)0);
-                sendQueue.Enqueue((byte)0);
+                bytesToLoad = ReadFromCacheBytes;
                 state = State.GetReadColumn;
                 break;
             default:
-                this.Log(LogLevel.Error, "Unsupported command 0x{0:X}", data);
+                this.Log(LogLevel.Error, "Unsupported command 0x{0:X}", (byte)command);
                 state = State.Unsupported;
                 break;
             }
-        }
 
-        public byte Transmit(byte b)
-        {
-            this.Log(LogLevel.Noisy, "Received byte: 0x{0:X} in state {1}", b, state);
-            byte result = 0;
-
-            switch(state)
+            if(bytesToLoad != null)
             {
-            case State.Idle:
-                HandleCommandByte(b);
-                break;
-            case State.Unsupported:
-                break;
-            case State.GetFeatureAddress:
-                {
-                var r = RegistersCollection.Read((long)b);
-                sendQueue.Enqueue(r);
-                this.Log(LogLevel.Info, "Reading register {0} = {1}", (long)b, r);
-                state = State.TransmitQueue;
-                break;
-                }
-            case State.GetReadColumn:
-                receiveStack.Push(b);
-                result = sendQueue.Dequeue();
-                if (sendQueue.Count == 0)
-                {
-                    column = RxDequeueAsUInt();
-                    this.Log(LogLevel.Info, "Column set to {0}", column);
-                    state = State.TransmitCache;
-                    sendQueue.Enqueue(0);
-                }
-                break;
-            case State.TransmitCache:
-                sendQueue.Enqueue(pageCache[column++]);
-                result = sendQueue.Dequeue();
-                break;
-            case State.SetFeatureAddress:
-                receiveStack.Push(b);
-                state = State.SetFeatureData;
-                break;
-            case State.SetFeatureData:
-                if (receiveStack.Count == 0)
-                {
-                    this.Log(LogLevel.Error, "Trailing bytes on SetFeature command");
-                }
-                else
-                {
-                    var r = (long)receiveStack.Pop();
-                    RegistersCollection.Write(r, b);
-                    this.Log(LogLevel.Info, "Setting register {0} = {1}", (long)r, b);
-                }
-                break;
-            case State.TransmitQueue:
-                receiveStack.Push(b);
-                if (sendQueue.Count == 0)
-                    this.Log(LogLevel.Warning, "Nothing left in send queue");
-                else
-                    result = sendQueue.Dequeue();
-                break;
-            default:
-                this.Log(LogLevel.Error, "Received byte 0x{0:X} in an unexpected state: {1}. Ignoring it...", b, state);
-                break;
+                sendQueue.EnqueueRange(bytesToLoad);
             }
-
-            this.Log(LogLevel.Noisy, "Returning byte: 0x{0:X}", result);
-            return result;
         }
+
+        private readonly Queue<byte> sendQueue;
+        private readonly Stack<byte> receiveStack;
+        private readonly MappedMemory dataMemory;
+        private readonly MappedMemory spareMemory;
+
+        private readonly byte[] pageCache;
+        private readonly uint pageSize;
+        private readonly uint spareSize;
+        private readonly uint pagesPerBlock;
+        private readonly uint blocksPerLun;
+        private readonly uint lunsPerChip;
+
+        private State state;
+        private Command lastCommand;
+
+        private uint column;
+
+        private static readonly byte[] ReadIdBytes = new byte[] { 0x0, 0xef, 0xbc };
+        private static readonly byte[] PageReadBytes = new byte[] { 0x0, 0x0, 0x0 };
+        private static readonly byte[] ReadFromCacheBytes = new byte[] { 0x0, 0x0 };
+
+        private const byte EmptySegment = 0xff;
 
         private enum Command
         {
@@ -246,25 +274,5 @@ namespace Antmicro.Renode.Peripherals.SPI
             Status = 0xc0,
             DieSelect = 0xd0
         }
-
-        private State state;
-        private readonly Stack<byte> receiveStack;
-        private readonly Queue<byte> sendQueue;
-        private Command lastCmd;
-
-        private const byte EmptySegment = 0xff;
-        private readonly MappedMemory dataMemory;
-        private readonly MappedMemory spareMemory;
-
-        private uint pageSize;
-        private uint spareSize;
-        private uint pagesPerBlock;
-        private uint blocksPerLun;
-        private uint lunsPerChip;
-
-        private uint column = 0;
-        private byte[] pageCache;
-
-        public ByteRegisterCollection RegistersCollection { get; }
     }
 }
