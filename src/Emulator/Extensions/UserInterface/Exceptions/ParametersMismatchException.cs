ea648a19792652e5192d56694cb6afea8ecdf559
Kamil Galik <kgalik@internships.antmicro.com>
2023-10-20 10:32:31 +0200

    [#49548] ParametersMismatchException: Add additional data

--- src/Emulator/Extensions/UserInterface/Exceptions/ParametersMismatchException.cs
+++ src/Emulator/Extensions/UserInterface/Exceptions/ParametersMismatchException.cs
@@ -1,21 +1,32 @@
 //
-// Copyright (c) 2010-2018 Antmicro
+// Copyright (c) 2010-2023 Antmicro
 // Copyright (c) 2011-2015 Realtime Embedded
 //
 // This file is licensed under the MIT License.
 // Full license text is available in 'licenses/MIT.txt'.
 //
+using System;
 using Antmicro.Renode.Exceptions;
 
 namespace Antmicro.Renode.UserInterface.Exceptions
 {
     public class ParametersMismatchException : RecoverableException
     {
-        public ParametersMismatchException() : base("Parameters did not match the signature")
+        public ParametersMismatchException(Type type, string command, string name) : base("Parameters did not match the signature")
         {
+            Name = name;
+            Type = type;
+            Command = command;
         }
-        public ParametersMismatchException(string message) : base(message)
+        public ParametersMismatchException(Type type, string command, string name, string message) : base(message)
         {
+            Name = name;
+            Type = type;
+            Command = command;
         }
+
+        public string Name { get; }
+        public Type Type { get; }
+        public string Command { get; }
     }
 }
