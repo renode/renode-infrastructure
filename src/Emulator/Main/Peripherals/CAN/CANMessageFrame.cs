96b1fea446ebb5668d8c3dbd47403f83a12d0e77
Marek Slowinski <mslowinski@antmicro.com>
2023-10-25 13:18:02 +0200

    [#48977] CAN: Extend CAN frame

--- src/Emulator/Main/Peripherals/CAN/CANMessageFrame.cs
+++ src/Emulator/Main/Peripherals/CAN/CANMessageFrame.cs
@@ -1,5 +1,5 @@
 ﻿﻿﻿//
-// Copyright (c) 2010-2018 Antmicro
+// Copyright (c) 2010-2023 Antmicro
 //
 //  This file is licensed under the MIT License.
 //  Full license text is available in 'licenses/MIT.txt'.
@@ -12,18 +12,26 @@ namespace Antmicro.Renode.Core.CAN
 {
     public class CANMessageFrame
     {
-        public CANMessageFrame(uint id, byte[] data)
+        public CANMessageFrame(uint id, byte[] data, bool extendedFormat = false, bool remoteFrame = false, bool fdFormat = false, bool bitRateSwitch = false)
         {
             Id = id;
             Data = data;
+            ExtendedFormat = extendedFormat;
+            RemoteFrame = remoteFrame;
+            FDFormat = fdFormat;
+            BitRateSwitch = bitRateSwitch;
         }
 
         public override string ToString()
         {
-            return $"[Message: Data=[{Data.Select(x => "0x{0:X}".FormatWith(x)).Stringify()}], Id={Id}, DataLength={Data.Length}]";
+            return $"[Message: Data=[{Data.Select(x => "0x{0:X}".FormatWith(x)).Stringify()}], Remote: {RemoteFrame}, Extended:{ExtendedFormat}, BitRateSwitch:{BitRateSwitch}, Id={Id}, DataLength={Data.Length}]";
         }
 
         public uint Id { get; }
         public byte[] Data { get; }
+        public bool ExtendedFormat { get; }
+        public bool RemoteFrame { get; }
+        public bool FDFormat { get; }
+        public bool BitRateSwitch { get; }
     }
 }
