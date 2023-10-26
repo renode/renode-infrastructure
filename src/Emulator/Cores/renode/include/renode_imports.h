509a073e8a42f7af945f2c5800c007908cd3ef31
Piotr Wojnarowski <pwojnarowski@antmicro.com>
2023-10-26 15:10:10 +0200

    [#50442] renode_imports: Store return value in variable
    With an eye towards putting something between the callback call and the return.

--- src/Emulator/Cores/renode/include/renode_imports.h
+++ src/Emulator/Cores/renode/include/renode_imports.h
@@ -16,13 +16,30 @@
 #define renode_func_header(TYPE, IMPORTED_NAME, LOCAL_NAME) \
 EXTERN_C void renode_glue(renode_glue(renode_glue(renode_external_attach__, renode_glue(RENODE_EXT_TYPE_, TYPE)), __), IMPORTED_NAME) (TYPE param)
 
+#define RETURN_TYPE(TYPE) renode_glue(TYPE, _return$)
+
+#define HAS_RETVAL_ 0
+#define HAS_RETVAL_return 1
+#define HAS_RETVAL2(KEYWORD) renode_direct_glue(HAS_RETVAL_, KEYWORD)
+#define HAS_RETVAL(TYPE) HAS_RETVAL2(renode_glue(TYPE, _keyword$))
+
+#define IF_THEN_ELSE(COND, THEN, ELSE) renode_direct_glue(IF_THEN_ELSE_, COND)(THEN, ELSE)
+#define IF_THEN_ELSE_0(THEN, ELSE) ELSE
+#define IF_THEN_ELSE_1(THEN, ELSE) THEN
+
 #define EXTERNAL(TYPE, NAME) EXTERNAL_AS(TYPE, $##NAME, NAME)
 
 #define EXTERNAL_AS(TYPE, IMPORTED_NAME, LOCAL_NAME) \
     TYPE renode_glue(LOCAL_NAME, _callback$);\
     renode_glue(TYPE, _return$) LOCAL_NAME (renode_glue(TYPE, _args$)) \
     {\
-       renode_glue(TYPE, _keyword$) (* renode_glue(LOCAL_NAME, _callback$)) (renode_glue(TYPE, _vars$)); \
+       /* If this function returns a value, generate code of the form \
+        * uint32_t retval = (*callback)(); \
+        * Otherwise, just call it. */ \
+       IF_THEN_ELSE(HAS_RETVAL(TYPE), RETURN_TYPE(TYPE) retval = , ) \
+       (* renode_glue(LOCAL_NAME, _callback$)) (renode_glue(TYPE, _vars$)); \
+       /* If this function returns a value, return it. */ \
+       IF_THEN_ELSE(HAS_RETVAL(TYPE), return retval;, ) \
     }\
     renode_func_header(TYPE, IMPORTED_NAME, LOCAL_NAME)\
     {\
