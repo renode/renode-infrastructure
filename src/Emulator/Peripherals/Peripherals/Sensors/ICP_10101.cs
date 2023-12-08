1e15d299302c36a8eb959e46561f2952dd6c366e
Mateusz Karlic <mkarlic@antmicro.com>
2023-12-08 14:29:28 +0100

    [#51713] ICP_10101: Add Pressure and Temperature logic

--- src/Emulator/Peripherals/Peripherals/Sensors/ICP_10101.cs
+++ src/Emulator/Peripherals/Peripherals/Sensors/ICP_10101.cs
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2010-2023 Antmicro
+// Copyright (c) 2010-2024 Antmicro
 //
 // This file is licensed under the MIT License.
 // Full license text is available in 'licenses/MIT.txt'.
@@ -9,20 +9,20 @@ using System.Collections.Generic;
 using System.Linq;
 using Antmicro.Renode.Exceptions;
 using Antmicro.Renode.Logging;
-using Antmicro.Renode.Peripherals;
 using Antmicro.Renode.Peripherals.I2C;
 using Antmicro.Renode.Peripherals.Sensor;
 using Antmicro.Renode.Utilities;
 
 namespace Antmicro.Renode.Peripherals.Sensors
 {
-    public class ICP_10101 : II2CPeripheral, ISensor
+    public class ICP_10101 : II2CPeripheral, ISensor, ITemperatureSensor
     {
         public ICP_10101()
         {
             crcEngine = new CRCEngine(0x31, 8, false, false, 0xFF, 0x00);
             writeHandlers = new Dictionary<Command, Action<byte[], int>>();
             readHandlers = new Dictionary<Command, Func<int, IEnumerable<byte>>>();
+            Pressure = MinPressure;
             DefineCommandHandlers();
         }
 
@@ -43,7 +43,8 @@ namespace Antmicro.Renode.Peripherals.Sensors
                 this.Log(LogLevel.Warning, "Malformed command.");
                 return;
             }
-            command = (Command)BitHelper.ToUInt16(data, 0, reverse: true);
+            this.Log(LogLevel.Debug, "Received command: {0}", Misc.PrettyPrintCollectionHex(data));
+            command = (Command)BitHelper.ToUInt16(data, 0, reverse: false);
             HandleWrite(data, 2);
         }
 
@@ -52,14 +53,80 @@ namespace Antmicro.Renode.Peripherals.Sensors
             if(!command.HasValue)
             {
                 this.Log(LogLevel.Error, "Attempted read, but Command not specified.");
+                return Enumerable.Empty<byte>().ToArray();
             }
-            return DataWithCrc(HandleRead(count)).Take(count).ToArray();
+            // Concat to handle cases, where HandleRead could return 0 bytes
+            var rets = InsertCrc(HandleRead(count)).Concat(new byte[]{ 0, 0 }).Take(count).ToArray();
+            this.Log(LogLevel.Debug, "Reading data with CRC: {0}, current command: {1}, requested: {2} bytes", Misc.PrettyPrintCollectionHex(rets), GetCommandString(), count);
+            return rets;
         }
 
         public void FinishTransmission()
         {
-            command = null;
+            // Don't reset state machine (current command) here - instead the software driver should use Soft Reset command
         }
+
+        public void SetCalibrationValue(uint index, ushort value)
+        {
+            if(index >= CalibrationValues.Length)
+            {
+                throw new RecoverableException($"Index has to be in range 0 - {CalibrationValues.Length - 1}");
+            }
+            CalibrationValues[index] = value;
+        }
+
+        // Sensor calibration values, used when reading the pressure
+        // This calibration values have been determined using statistical solution search with the following goals in mind:
+        // * decrease probability that the raw pressure value returned from the sensor is negative
+        // * make sure the raw pressure value fits in 3 bytes of return value
+        // * minimize error margin between set pressure and reported raw pressure (post-conversion) as much as possible, taking into account temperature changes
+        // in real HW these are definitely different, but for our use case they are sufficient
+        public ushort[] CalibrationValues { get; private set; } = new ushort[4] { 620, 17540, 9475, 2945 };
+        // Another good vectors:
+        // { 15320, 15780, 32302, 3497 };
+        // { 30261, -20852 /*44684*/, -24209 /*41327*/, 3107 };
+
+        // Pressure is specified in Pascals
+        public uint Pressure
+        {
+            get => pressure;
+            set
+            {
+                if(value < MinPressure || value > MaxPressure)
+                {
+                    this.Log(LogLevel.Warning, "Pressure value: {0} is out of range and it will be clamped. Supported range: {1} - {2}", value, MinPressure, MaxPressure);
+                    pressure = value < MinPressure ? MinPressure : MaxPressure;
+                }
+                else
+                {
+                    pressure = value;
+                }
+            }
+        }
+
+        // Temperature is specified in degrees Celsius
+        public decimal Temperature
+        {
+            get => temperature;
+            set
+            {
+                if(value < MinTemperature || value > MaxTemperature)
+                {
+                    this.Log(LogLevel.Warning, "Temperature value: {0} is out of range and it will be clamped. Supported range: {1} - {2}", value, MinTemperature, MaxTemperature);
+                    temperature = value < MinTemperature ? MinTemperature : MaxTemperature;
+                }
+                else
+                {
+                    temperature = value;
+                }
+            }
+        }
+
+        public const decimal MinTemperature = -45;
+        public const decimal MaxTemperature = 85;
+
+        public const uint MinPressure = 30000;
+        public const uint MaxPressure = 110000;
 
         private void DefineCommandHandlers()
         {
@@ -127,21 +194,36 @@ namespace Antmicro.Renode.Peripherals.Sensors
             );
 
             RegisterCommand(Command.Id,
-                readHandler: _ => BitHelper.GetBytesFromValue(0b001000, 2, reverse: true)
+                readHandler: _ => BitHelper.GetBytesFromValue(0b001000, 2, reverse: false) // It's already in the correct byte order
             );
 
             RegisterCommand(Command.CalibrationParametersPointer,
                 writeHandler: (data, offset) =>
                 {
-                    // TODO
+                    if(data.Length <= offset + 2)
+                    {
+                        this.Log(LogLevel.Error, "Read from OTP received not enough bytes, got: {0}, total of: {0} are expected", data.Length, offset + 3);
+                        return;
+                    }
+                    var addr = data[offset] << 16 | data[offset + 1] << 8 | data[offset + 2];
+                    if(addr != CalibrationAddress)
+                    {
+                        this.Log(LogLevel.Error, "Read from OTP from offset 0x{0:X}, different than 0x{1:X} is unsupported!", addr, CalibrationAddress);
+                        return;
+                    }
+                    // Just reset the current calibration value number, as there is no data 
+                    // other than calibration values to be read from the sensor memory
+                    // and the docs don't mention another usage for this command at this moment (rev 1.2)
+                    calibrationValueIndex = 0;
                 }
             );
 
             RegisterCommand(Command.CalibrationParameters,
                 readHandler: _ =>
                 {
-                    // TODO
-                    return Enumerable.Empty<byte>();
+                    ushort rets = CalibrationValues[calibrationValueIndex];
+                    calibrationValueIndex = (calibrationValueIndex + 1) % CalibrationValues.Length;
+                    return BitHelper.GetBytesFromValue(rets, 2, reverse: false);
                 }
             );
         }
@@ -153,18 +235,20 @@ namespace Antmicro.Renode.Peripherals.Sensors
                 throw new RecoverableException("Attempted to register command twice");
             }
 
+            this.Log(LogLevel.Noisy, "Registered command 0x{0:X}.", command);
+
             writeHandlers.Add(command, writeHandler ?? ((data, offset) =>
             {
                 if(offset != data.Length)
                 {
-                    this.Log(LogLevel.Warning, $"Attempted write while handling read-only command '{command}'.");
+                    this.Log(LogLevel.Warning, "Attempted write while handling read-only command '{0}'.", command);
                 }
             }));
             readHandlers.Add(command, readHandler ?? (count =>
             {
                 if(count != 0)
                 {
-                    this.Log(LogLevel.Warning, $"Attempted read while handling write-only command '{command}'.");
+                    this.Log(LogLevel.Warning, "Attempted read while handling write-only command '{0}'.", command);
                 }
                 return Enumerable.Empty<byte>();
             }));
@@ -172,72 +256,112 @@ namespace Antmicro.Renode.Peripherals.Sensors
 
         private IEnumerable<byte> GetTemperatureBytes(OperationMode mode)
         {
-            return BitHelper.GetBytesFromValue(GetTemperature(mode), 2, reverse: true);
+            return BitHelper.GetBytesFromValue(GetTemperature(mode), 2, reverse: false);
         }
 
-        private ushort GetTemperature(OperationMode mode)
+        private ushort GetTemperature(OperationMode _)
         {
-            return 0; // TODO
-        }
-
-        private IEnumerable<byte> GetPressureBytes(OperationMode mode)
-        {
-            return BitHelper.GetBytesFromValue(GetPressure(mode), 4, reverse: true);
+            return (ushort)((Temperature + 45) * (1 << 16) / 175);
         }
 
         private uint GetPressure(OperationMode mode)
         {
-            return 0; // TODO
-        }
-
-        private ushort? GetCalibrationParameter(ulong pointer)
-        {
-            if(pointer < CalibrationParameterStart || CalibrationParameterStart + NumberOfCalibrationParameters <= pointer)
+            GetCoefficients(mode, out long A, out long B, out long C);
+            var decimalPressure = (B / (Pressure - A)) - C;
+            // Detect and report that the sensor might be miscalibrated
+            if(decimalPressure < 0 || decimalPressure > 0xFFFFFF)
             {
-                return null;
+                this.Log(LogLevel.Warning, "Raw pressure value is invalid: {0}. The sensor might be miscalibrated.", decimalPressure);
             }
-            // TODO
-            return 0x0;
+            uint rawPressure = (uint)decimalPressure;
+
+            this.Log(LogLevel.Noisy, "p_raw={0}, p_long={1}", rawPressure, decimalPressure);
+            return rawPressure;
         }
 
-        private IEnumerable<byte> DataWithCrc(IEnumerable<byte> data)
+        private IEnumerable<byte> GetPressureBytes(OperationMode mode)
         {
-            var i = 0;
+            // Shift value left, since LLSB is discarded (has no meaning in pressure calculation)
+            return BitHelper.GetBytesFromValue((uint)(GetPressure(mode) << 8), 4, reverse: false);
+        }
+
+        private void GetCoefficients(OperationMode mode, out long A, out long B, out long C)
+        {
+            long t = GetTemperature(mode) - 32768;
+            long p_LUT0 = LUT_lower + (CalibrationValues[0] * t * t) / InverseQuadraticFactor;
+            long p_LUT1 = OffsetFactor * CalibrationValues[3] + (CalibrationValues[1] * t * t) / InverseQuadraticFactor;
+            long p_LUT2 = LUT_upper + (CalibrationValues[2] * t * t) / InverseQuadraticFactor;
+            
+            C = (p_LUT0 * p_LUT1 * (p_Pa[0] - p_Pa[1]) +
+                 p_LUT1 * p_LUT2 * (p_Pa[1] - p_Pa[2]) +
+                 p_LUT2 * p_LUT0 * (p_Pa[2] - p_Pa[0])) /
+                (p_LUT2 * (p_Pa[0] - p_Pa[1]) +
+                 p_LUT0 * (p_Pa[1] - p_Pa[2]) +
+                 p_LUT1 * (p_Pa[2] - p_Pa[0]));
+
+            A = (p_Pa[0] * p_LUT0 - p_Pa[1] * p_LUT1 - (p_Pa[1] - p_Pa[0]) * C) / (p_LUT0 - p_LUT1);
+
+            B = (p_Pa[0] - A) * (p_LUT0 + C);
+
+            this.Log(LogLevel.Noisy, "A={0} B={1} C={2}, p_LUT0={3}, p_LUT1={4}, p_LUT2={5}", A, B, C, p_LUT0, p_LUT1, p_LUT2);
+        }
+
+        private IEnumerable<byte> InsertCrc(IEnumerable<byte> data)
+        {
+            var wordsNumber = 0;
             var word = new byte[2];
             foreach(var b in data)
             {
                 yield return b;
-                word[i % 2] = b;
-                if(i % 2 == 1)
+                word[wordsNumber % 2] = b;
+                if(wordsNumber % 2 == 1)
                 {
                     yield return (byte)crcEngine.Calculate(word);
                 }
-                i += 1;
+                ++wordsNumber;
+            }
+            if(wordsNumber % 2 == 1)
+            {
+                throw new InvalidOperationException("Data length has to be multiple of 2 bytes");
             }
         }
 
+        private string GetCommandString() => command != null ? $"0x{command:X} ({command})" : "None";
+
         private void HandleWriteDefault(byte[] data, int offset)
         {
-            this.Log(LogLevel.Warning, "Unhandled write, command (0x{0:X4}).", command);
+            this.Log(LogLevel.Warning, "Unhandled write, command: {0}.", GetCommandString());
         }
 
         private IEnumerable<byte> HandleReadDefault(int count)
         {
-            this.Log(LogLevel.Warning, "Unhandled read, command (0x{0:X4}).", command);
+            this.Log(LogLevel.Warning, "Unhandled read, command: {0}.", GetCommandString());
             return Enumerable.Empty<byte>();
         }
 
-        private Action<byte[], int> HandleWrite => writeHandlers.TryGetValue(command.Value, out var value) ? value : HandleWriteDefault;
-        private Func<int, IEnumerable<byte>> HandleRead => readHandlers.TryGetValue(command.Value, out var value) ? value : HandleReadDefault;
+        private Action<byte[], int> HandleWrite => writeHandlers.TryGetValue(command.Value, out var handler) ? handler : HandleWriteDefault;
+        private Func<int, IEnumerable<byte>> HandleRead => readHandlers.TryGetValue(command.Value, out var handler) ? handler : HandleReadDefault;
 
         private Command? command;
+
+        private uint pressure;
+        private decimal temperature;
 
         private readonly CRCEngine crcEngine;
         private readonly Dictionary<Command, Action<byte[], int>> writeHandlers;
         private readonly Dictionary<Command, Func<int, IEnumerable<byte>>> readHandlers;
 
-        private const int NumberOfCalibrationParameters = 4;
-        private const int CalibrationParameterStart = 0x00669C;
+        // Which calibration value is read in incremental read-from OTP
+        private int calibrationValueIndex = 0;
+
+        // Configuration constants, taken directly from the datasheet (https://invensense.tdk.com/wp-content/uploads/2021/06/DS-000408-ICP-10101-v1.2.pdf)
+        private readonly int[] p_Pa = new int [] { 45000, 80000, 105000 };
+        private const int LUT_lower = 3670016; //3.5 * (1 << 20);
+        private const int LUT_upper = 12058624; //11.5 * (1 << 20);
+        private const int InverseQuadraticFactor = 16777216;
+        private const int OffsetFactor = 2048;
+
+        private const int CalibrationAddress = 0x00669C;
 
         public enum OperationMode
         {
@@ -247,7 +371,7 @@ namespace Antmicro.Renode.Peripherals.Sensors
             UltraLowNoise,
         }
 
-        public enum Command
+        public enum Command : ushort
         {
             LowPowerTemperaturePressure = 0x609C,
             LowPowerPressureTemperature = 0x401A,
