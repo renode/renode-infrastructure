f5c4be33629a5c098c5a76404bd28e9e7827f4bd
Piotr Wojnarowski <pwojnarowski@antmicro.com>
2024-01-11 13:35:19 +0100

    [#52787] DevicesConfig: Prefer registration points other than null

--- src/Emulator/Extensions/Config/Devices/DevicesConfig.cs
+++ src/Emulator/Extensions/Config/Devices/DevicesConfig.cs
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2010-2018 Antmicro
+// Copyright (c) 2010-2024 Antmicro
 // Copyright (c) 2011-2015 Realtime Embedded
 //
 // This file is licensed under the MIT License.
@@ -591,7 +591,10 @@ namespace Antmicro.Renode.Config.Devices
                 var parent = parents.Single(x => x.Key == parentName).Value;
                 var connections = device.Connections[parentName];
                 var ifaces = parent.GetType().GetInterfaces().Where(x => IsSpecializationOfRawGeneric(typeof(IPeripheralRegister<,>), x)).ToList();
-                var ifaceCandidates = ifaces.Where(x => x.GetGenericArguments()[0].IsAssignableFrom(device.Peripheral.GetType())).ToList();
+                var ifaceCandidates = ifaces
+                    .Where(x => x.GetGenericArguments()[0].IsAssignableFrom(device.Peripheral.GetType()))
+                    .OrderBy(x => typeof(NullRegistrationPoint).IsAssignableFrom(x.GetGenericArguments()[1])) // Use the null registration point last
+                    .ToList();
                 foreach(var connection in connections)
                 {
                     IRegistrationPoint regPoint = null;
