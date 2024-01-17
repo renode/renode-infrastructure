7a496943d4412ac4fa9c8c0ef455b4afe6be6a36
ant <ant@tworobiec.wro-concordia.antmicro>
2024-01-17 14:31:31 +0100

    [#45405] ArrayMemory: Add offset checks

--- src/Emulator/Peripherals/Peripherals/Memory/ArrayMemory.cs
+++ src/Emulator/Peripherals/Peripherals/Memory/ArrayMemory.cs
@@ -24,11 +24,20 @@ namespace Antmicro.Renode.Peripherals.Memory
 
         public ArrayMemory(int size)
         {
+            if(size <= 0)
+            {
+                throw new ConstructionException($"Memory size should be positive, but tried to configure it to: {size}");
+            }
             array = new byte[size];
         }
 
         public virtual ulong ReadQuadWord(long offset)
         {
+            if(!IsCorrectOffset(offset, sizeof(ulong)))
+            {
+                LogOffsetError(offset);
+                return 0;
+            }
             var intOffset = (int)offset;
             var result = BitConverter.ToUInt64(array, intOffset);
             return result;
@@ -36,12 +45,22 @@ namespace Antmicro.Renode.Peripherals.Memory
 
         public virtual void WriteQuadWord(long offset, ulong value)
         {
+            if(!IsCorrectOffset(offset, sizeof(ulong)))
+            {
+                LogOffsetError(offset);
+                return;
+            }
             var bytes = BitConverter.GetBytes(value);
             bytes.CopyTo(array, offset);
         }
 
         public uint ReadDoubleWord(long offset)
         {
+            if(!IsCorrectOffset(offset, sizeof(uint)))
+            {
+                LogOffsetError(offset);
+                return 0;
+            }
             var intOffset = (int)offset;
             var result = BitConverter.ToUInt32(array, intOffset);
             return result;
@@ -49,9 +68,13 @@ namespace Antmicro.Renode.Peripherals.Memory
 
         public virtual void WriteDoubleWord(long offset, uint value)
         {
+            if(!IsCorrectOffset(offset, sizeof(uint)))
+            {
+                LogOffsetError(offset);
+                return;
+            }
             var bytes = BitConverter.GetBytes(value);
             bytes.CopyTo(array, offset);
-
         }
 
         public void Reset()
@@ -61,6 +84,11 @@ namespace Antmicro.Renode.Peripherals.Memory
 
         public ushort ReadWord(long offset)
         {
+            if(!IsCorrectOffset(offset, sizeof(ushort)))
+            {
+                LogOffsetError(offset);
+                return 0;
+            }
             var intOffset = (int)offset;
             var result = BitConverter.ToUInt16(array, intOffset);
             return result;
@@ -68,25 +96,45 @@ namespace Antmicro.Renode.Peripherals.Memory
 
         public virtual void WriteWord(long offset, ushort value)
         {
+            if(!IsCorrectOffset(offset, sizeof(ushort)))
+            {
+                LogOffsetError(offset);
+                return;
+            }
             var bytes = BitConverter.GetBytes(value);
             bytes.CopyTo(array, offset);
         }
 
         public byte ReadByte(long offset)
         {
+            if(!IsCorrectOffset(offset, sizeof(byte)))
+            {
+                LogOffsetError(offset);
+                return 0;
+            }
             var intOffset = (int)offset;
             var result = array[intOffset];
             return result;
         }
 
         public virtual void WriteByte(long offset, byte value)
-        { 
+        {
+            if(!IsCorrectOffset(offset, sizeof(byte)))
+            {
+                LogOffsetError(offset);
+                return;
+            }
             var intOffset = (int)offset;
             array[intOffset] = value;
         }
 
         public byte[] ReadBytes(long offset, int count, ICPU context = null)
         {
+            if(!IsCorrectOffset(offset, count))
+            {
+                LogOffsetError(offset);
+                return new byte[count];
+            }
             var result = new byte[count];
             Array.Copy(array, offset, result, 0, count);
             return result;
@@ -94,6 +142,11 @@ namespace Antmicro.Renode.Peripherals.Memory
 
         public void WriteBytes(long offset, byte[] bytes, int startingIndex, int count, ICPU context = null)
         {
+            if(!IsCorrectOffset(offset, count))
+            {
+                LogOffsetError(offset);
+                return;
+            }
             Array.Copy(bytes, startingIndex, array, offset, count);
         }
 
@@ -111,5 +164,15 @@ namespace Antmicro.Renode.Peripherals.Memory
         }
 
         protected readonly byte[] array;
+
+        private bool IsCorrectOffset(long offset, int size)
+        {
+            return offset >= 0 && offset <= array.Length - size;
+        }
+
+        private void LogOffsetError(long offset)
+        {
+            this.Log(LogLevel.Error, "Tried to read byte at offset 0x{0:X} outside the range of the peripheral 0x0 - 0x{1:X}", offset, array.Length - 1);
+        }
     }
 }
