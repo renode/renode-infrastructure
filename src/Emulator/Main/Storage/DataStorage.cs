e178618228fc880f143deba971327677a478e54c
Piotr Wojnarowski <pwojnarowski@antmicro.com>
2024-01-23 16:36:58 +0100

    [#53760] DataStorage: Allow specifying padding byte

--- src/Emulator/Main/Storage/DataStorage.cs
+++ src/Emulator/Main/Storage/DataStorage.cs
@@ -1,3 +1,9 @@
+//
+// Copyright (c) 2010-2024 Antmicro
+//
+// This file is licensed under the MIT License.
+// Full license text is available in 'licenses/MIT.txt'.
+//
 
 using System.IO;
 using Antmicro.Renode.Exceptions;
@@ -7,7 +13,7 @@ namespace Antmicro.Renode.Storage
 {
     public static class DataStorage
     {
-        public static Stream Create(string imageFile, long? size = null, bool persistent = false)
+        public static Stream Create(string imageFile, long? size = null, bool persistent = false, byte paddingByte = 0)
         {
             if(string.IsNullOrEmpty(imageFile))
             {
@@ -21,7 +27,7 @@ namespace Antmicro.Renode.Storage
                 imageFile = tempFileName;
             }
 
-            return new SerializableStreamView(new FileStream(imageFile, FileMode.OpenOrCreate), size);
+            return new SerializableStreamView(new FileStream(imageFile, FileMode.OpenOrCreate), size, paddingByte: paddingByte);
         }
 
         public static Stream Create(long size, byte paddingByte = 0)
