76f741906ec34db64e5064bdf1947949437ae93e
Adam Jeliński <ajelinski@antmicro.com>
2024-01-25 11:35:35 +0100

    [#49573] Add `CPUState` class

---
+++ src/Emulator/Main/Peripherals/CPU/CPUState.cs
@@ -0,0 +1,17 @@
+//
+// Copyright (c) 2010-2024 Antmicro
+//
+// This file is licensed under the MIT License.
+// Full license text is available in 'licenses/MIT.txt'.
+//
+
+namespace Antmicro.Renode.Peripherals.CPU
+{
+    public enum CPUState
+    {
+        Aborted,
+        InReset,
+        Running,
+    }
+}
+
