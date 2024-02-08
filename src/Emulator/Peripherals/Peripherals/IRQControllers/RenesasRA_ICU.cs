04ebb9590908da5560aca825e5134f95e78f2a57
Jan Olencki <jolencki@antmicro.com>
2024-02-08 11:00:41 +0100

    [#53476] RenesasRA_ICU: Add RA2 support

--- src/Emulator/Peripherals/Peripherals/IRQControllers/RenesasRA_ICU.cs
+++ src/Emulator/Peripherals/Peripherals/IRQControllers/RenesasRA_ICU.cs
@@ -20,7 +20,7 @@ namespace Antmicro.Renode.Peripherals.IRQControllers
     [AllowedTranslations(AllowedTranslation.ByteToDoubleWord | AllowedTranslation.WordToDoubleWord)]
     public class RenesasRA_ICU : BasicDoubleWordPeripheral, IIRQController, IKnownSize
     {
-        public RenesasRA_ICU(IMachine machine, IGPIOReceiver nvic,
+        public RenesasRA_ICU(IMachine machine, IGPIOReceiver nvic, EventToInterruptLinkType eventToInterruptLink = EventToInterruptLinkType.RA8,
             uint numberOfExternalInterrupts = DefaultNumberOfExternalInterrupts,
             uint highestEventNumber = DefaultHighestEventNumber,
             uint numberOfNVICOutputs = DefaultNumberOfNVICOutputs) : base(machine)
@@ -38,6 +38,7 @@ namespace Antmicro.Renode.Peripherals.IRQControllers
             }
 
             this.nvic = nvic;
+            eventLinkType = eventToInterruptLink;
 
             interruptsForEvent = Enumerable.Range(0, (int)numberOfEvents).Select(_ => new HashSet<int>()).ToArray();
             latestEventState = new bool[numberOfEvents];
@@ -85,7 +86,14 @@ namespace Antmicro.Renode.Peripherals.IRQControllers
 
         private int GetEventForEventLink(int irqIndex, ulong eventLink)
         {
-            return (int)eventLink;
+            // The IELS register in platforms other than RA2 directly indicate an event.
+            if(eventLinkType != EventToInterruptLinkType.RA2)
+            {
+                return (int)eventLink;
+            }
+            // For RA2 also a group of interrupt takes account.
+            var irqGroup = irqIndex % 8;
+            return eventLinkRA2[eventLink, irqGroup];
         }
 
         private bool IsEventTriggered(int eventIndex, bool previousState, bool state)
@@ -268,7 +276,7 @@ namespace Antmicro.Renode.Peripherals.IRQControllers
                 ;
             });
 
-            var eventLinkRegisterLength = 9;
+            var eventLinkRegisterLength = eventLinkType == EventToInterruptLinkType.RA2 ? 5 : 9;
             Registers.ICUEventLinkSetting0.DefineMany(this, (uint)interruptEventLink.Length, (register, registerIndex) =>
             {
                 register
@@ -294,6 +302,7 @@ namespace Antmicro.Renode.Peripherals.IRQControllers
         private readonly IValueRegisterField[] interruptEventLink;
         private readonly IFlagRegisterField[] interruptPending;
 
+        private readonly EventToInterruptLinkType eventLinkType;
         private readonly ISet<int>[] interruptsForEvent;
         private readonly bool[] latestEventState;
         private readonly IGPIOReceiver nvic;
@@ -303,6 +312,179 @@ namespace Antmicro.Renode.Peripherals.IRQControllers
         private const uint DefaultHighestEventNumber = 0x1DA;
         private const uint DefaultNumberOfDMACEvents = 8;
         private const uint DefaultNumberOfNVICOutputs = 96;
+
+        private readonly int[,] eventLinkRA2 =
+            {
+                //  GROUP0          GROUP1          GROUP2          GROUP3          GROUP4          GROUP5          GROUP6          GROUP7          
+                {
+                //  IELS = 0x00
+                //  NO_EVENT,       NO_EVENT,       NO_EVENT,       NO_EVENT,       NO_EVENT,       NO_EVENT,       NO_EVENT,       NO_EVENT
+                    0x00,           0x00,           0x00,           0x00,           0x00,           0x00,           0x00,           0x00
+                },
+                {
+                //  IELS = 0x01
+                //  PORT_IRQ0,      PORT_IRQ1,      PORT_IRQ2,      PORT_IRQ3,      PORT_IRQ0,      PORT_IRQ1,      PORT_IRQ2,      PORT_IRQ3
+                    0x01,           0x02,           0x03,           0x04,           0x01,           0x02,           0x03,           0x04
+                },
+                {
+                //  IELS = 0x02
+                //  DTC_COMPLETE,   LVD_LVD2,       FCU_FRDYI,      SYSTEM_SNZREQ,  DTC_COMPLETE,   LVD_LVD2,       FCU_FRDYI,      SYSTEM_SNZREQ
+                    0x09,           0x0E,           0x0C,           0x10,           0x09,           0x0E,           0x0C,           0x10
+                },
+                {
+                //  IELS = 0x03
+                //  ICU_SNZCANCEL,  AGT1_AGTCMAI,   AGT1_AGTCMBI,   IWDT_NMIUNDF,   ICU_SNZCANCEL,  AGT1_AGTCMAI,   AGT1_AGTCMBI,   IWDT_NMIUNDF
+                    0x0B,           0x15,           0x16,           0x17,           0x0B,           0x15,           0x16,           0x17
+                },
+                {
+                //  IELS = 0x04
+                //  LVD_LVD1,       RTC_ALM,        RTC_PRD,        RTC_CUP,        LVD_LVD1,       RTC_ALM,        RTC_PRD,        RTC_CUP
+                    0x0D,           0x19,           0x1A,           0x1B,           0x0D,           0x19,           0x1A,           0x1B
+                },
+                {
+                //  IELS = 0x05
+                //  AGT1_AGTI,      ADC120_GBADI,   ADC120_CMPAI,   ADC120_CMPBI,   AGT1_AGTI,      ADC120_GBADI,   ADC120_CMPAI,   ADC120_CMPBI
+                    0x14,           0x1D,           0x1E,           0x1F,           0x14,           0x1D,           0x1E,           0x1F
+                },
+                {
+                //  IELS = 0x06
+                //  WDT_NMIUNDF,    ADC120_WCMPUM,  IIC0_TEI,       IIC0_EEI,       WDT_NMIUNDF,    ADC120_WCMPUM,  IIC0_TEI,       IIC0_EEI
+                    0x18,           0x21,           0x29,           0x2A,           0x18,           0x21,           0x29,           0x2A
+                },
+                {
+                //  IELS = 0x07
+                //  ADC120_ADI,     ACMP_LP1,       CTSU_CTSURD,    CTSU_CTSUFN,    ADC120_ADI,     ACMP_LP1,       CTSU_CTSURD,    CTSU_CTSUFN
+                    0x1C,           0x24,           0x31,           0x32,           0x1C,           0x24,           0x31,           0x32
+                },
+                {
+                //  IELS = 0x08
+                //  ADC120_WCMPM,   IIC0_TXI,       CAC_MENDI,      CAC_OVFI,       ADC120_WCMPM,   IIC0_TXI,       CAC_MENDI,      CAC_OVFI
+                    0x20,           0x28,           0x36,           0x37,           0x20,           0x28,           0x36,           0x37
+                },
+                {
+                //  IELS = 0x09
+                //  ACMP_LP0,       CTSU_CTSUWR,    CAN0_TXF,       CAN0_RXM,       ACMP_LP0,       CTSU_CTSUWR,    CAN0_TXF,       CAN0_RXM
+                    0x23,           0x30,           0x3A,           0x3B,           0x23,           0x30,           0x3A,           0x3B
+                },
+                {
+                //  IELS = 0x0A
+                //  IIC0_RXI,       DOC_DOPCI,      ELC_SWEVT0,     ELC_SWEVT1,     IIC0_RXI,       DOC_DOPCI,      ELC_SWEVT0,     ELC_SWEVT1
+                    0x27,           0x34,           0x3F,           0x40,           0x27,           0x34,           0x3F,           0x40
+                },
+                {
+                //  IELS = 0x0B
+                //  IIC0_WUI,       CAC_FERRI,      POEG_GROUP0,    POEG_GROUP1,    IIC0_WUI,       CAC_FERRI,      POEG_GROUP0,    POEG_GROUP1
+                    0x2B,           0x35,           0x41,           0x42,           0x2B,           0x35,           0x41,           0x42
+                },
+                {
+                //  IELS = 0x0C
+                //  CAN0_ERS,       CAN0_RXF,       GPT0_CMPC,      GPT0_CMPD,      CAN0_ERS,       CAN0_RXF,       GPT0_CMPC,      GPT0_CMPD
+                    0x38,           0x39,           0x48,           0x49,           0x38,           0x39,           0x48,           0x49
+                },
+                {
+                //  IELS = 0x0D
+                //  CAN0_TXM,       GPT0_CCMPB,     GPT2_CMPC,      GPT2_CMPD,      CAN0_TXM,       GPT0_CCMPB,     GPT2_CMPC,      GPT2_CMPD
+                    0x3C,           0x47,           0x54,           0x55,           0x3C,           0x47,           0x54,           0x55
+                },
+                {
+                //  IELS = 0x0E
+                //  GPT0_CCMPA,     GPT0_UDF,       GPT2_OVF,       GPT2_UDF,       GPT0_CCMPA,     GPT0_UDF,       GPT2_OVF,       GPT2_UDF
+                    0x46,           0x4B,           0x56,           0x57,           0x46,           0x4B,           0x56,           0x57
+                },
+                {
+                //  IELS = 0x0F
+                //  GPT0_OVF,       GPT2_CCMPB,     SCI0_TEI,       SCI0_ERI,       GPT0_OVF,       GPT2_CCMPB,     SCI0_TEI,       SCI0_ERI
+                    0x4A,           0x53,           0x73,           0x74,           0x4A,           0x53,           0x73,           0x74
+                },
+                {
+                //  IELS = 0x10
+                //  GPT2_CCMPA,     SCI0_TXI,       SPI0_SPII,      SPI0_SPEI,      GPT2_CCMPA,     SCI0_TXI,       SPI0_SPII,      SPI0_SPEI
+                    0x52,           0x72,           0x83,           0x84,           0x52,           0x72,           0x83,           0x84
+                },
+                {
+                //  IELS = 0x11
+                //  GPT_UVWEDGE,    SPI0_SPTI,      SPI0_SPTEND,    AGT0_AGTI,      GPT_UVWEDGE,    SPI0_SPTI,      SPI0_SPTEND,    PORT_IRQ7
+                    0x70,           0x82,           0x85,           0x11,           0x70,           0x82,           0x85,           0x08
+                },
+                {
+                //  IELS = 0x12
+                //  SCI0_RXI,       AES_RDREQ,      TRNG_RDREQ,     GPT1_CMPD,      SCI0_RXI,       AES_RDREQ,      TRNG_RDREQ,     GPT3_CMPD
+                    0x71,           0x8C,           0x8D,           0x4F,           0x71,           0x8C,           0x8D,           0x5B
+                },
+                {
+                //  IELS = 0x13
+                //  SCI0_AM,        AGT0_AGTCMBI,   IOPORT_GROUP2,  GPT4_CMPD,      SCI0_AM,        PORT_IRQ5,      PORT_IRQ6,      GPT4_UDF
+                    0x75,           0x13,           0x3E,           0x61,           0x75,           0x06,           0x07,           0x63
+                },
+                {
+                //  IELS = 0x14
+                //  SPI0_SPRI,      IIC1_TXI,       GPT1_CMPC,      GPT5_UDF,       SPI0_SPRI,      IIC1_EEI,       MOSC_STOP,      GPT5_CMPD
+                    0x81,           0x2D,           0x4E,           0x69,           0x81,           0x2F,           0x0F,           0x67
+                },
+                {
+                //  IELS = 0x15
+                //  AES_WRREQ,      IOPORT_GROUP1,  GPT4_CMPC,      GPT6_CMPD,      AES_WRREQ,      GPT1_UDF,       GPT3_CMPC,      GPT6_UDF
+                    0x8B,           0x3D,           0x60,           0x6D,           0x8B,           0x51,           0x5A,           0x6F
+                },
+                {
+                //  IELS = 0x16
+                //  AGT0_AGTCMAI,   GPT1_CCMPB,     GPT5_OVF,       GPT7_UDF,       PORT_IRQ4,      GPT3_CCMPB,     GPT4_OVF,       GPT7_CMPD
+                    0x12,           0x4D,           0x68,           0x9D,           0x05,           0x59,           0x62,           0x9B
+                },
+                {
+                //  IELS = 0x17
+                //  IIC1_RXI,       GPT3_UDF,       GPT6_CMPC,      GPT8_CMPD,      IIC1_TEI,       GPT5_CCMPB,     GPT5_CMPC,      GPT8_UDF
+                    0x2C,           0x5D,           0x6C,           0xA1,           0x2E,           0x65,           0x66,           0xA3
+                },
+                {
+                //  IELS = 0x18
+                //  KEY_INTKR,      GPT4_CCMPB,     GPT7_OVF,       GPT9_UDF,       GPT1_OVF,       GPT7_CCMPB,     GPT6_OVF,       GPT9_CMPD
+                    0x33,           0x5F,           0x9C,           0xA9,           0x50,           0x99,           0x6E,           0xA7
+                },
+                {
+                //  IELS = 0x19
+                //  GPT1_CCMPA,     GPT6_CCMPB,     GPT8_CMPC,      SCI1_ERI,       GPT3_CCMPA,     GPT9_CCMPB,     GPT7_CMPC,      SCI2_ERI
+                    0x4C,           0x6B,           0xA0,           0x7A,           0x58,           0xA5,           0x9A,           0x91
+                },
+                {
+                //  IELS = 0x1A
+                //  GPT3_OVF,       GPT8_CCMPB,     GPT9_OVF,       SCI3_ERI,       GPT5_CCMPA,     SCI1_AM,        GPT8_OVF,       SCI9_ERI
+                    0x5C,           0x9F,           0xA8,           0x96,           0x64,           0x7B,           0xA2,           0x7F
+                },
+                {
+                //  IELS = 0x1B
+                //  GPT4_CCMPA,     SCI1_TXI,       SCI1_TEI,       SCI9_AM,        GPT7_CCMPA,     SCI2_TXI,       GPT9_CMPC,      SPI1_SPEI
+                    0x5E,           0x78,           0x79,           0x80,           0x98,           0x8F,           0xA6,           0x89
+                },
+                {
+                //  IELS = 0x1C
+                //  GPT6_CCMPA,     SCI2_AM,        SCI3_TEI,       NO_EVENT,       GPT9_CCMPA,     SCI9_TXI,       SCI2_TEI,       NO_EVENT
+                    0x6A,           0x92,           0x95,           0x00,           0xA4,           0x7D,           0x90,           0x00
+                },
+                {
+                //  IELS = 0x1D
+                //  GPT8_CCMPA,     SCI3_TXI,       SPI1_SPII,      NO_EVENT,       SCI2_RXI,       SPI1_SPTI,      SCI3_AM,        NO_EVENT
+                    0x9E,           0x94,           0x88,           0x00,           0x8E,           0x87,           0x97,           0x00
+                },
+                {
+                //  IELS = 0x1E
+                //  SCI1_RXI,       NO_EVENT,       NO_EVENT,       NO_EVENT,       SCI9_RXI,       NO_EVENT,       SCI9_TEI,       NO_EVENT
+                    0x77,           0x00,           0x00,           0x00,           0x7C,           0x00,           0x7E,           0x00
+                },
+                {
+                //  IELS = 0x1F
+                //  SCI3_RXI,       NO_EVENT,       NO_EVENT,       NO_EVENT,       SPI1_SPRI,      NO_EVENT,       SPI1_SPTEND,    NO_EVENT
+                    0x93,           0x00,           0x00,           0x00,           0x86,           0x00,           0x8A,           0x00
+                }
+            };
+
+        public enum EventToInterruptLinkType
+        {
+            RA8,
+            RA6 = RA8,
+            RA4 = RA8,
+            RA2
+        }
 
         private enum InterruptTrigger
         {
