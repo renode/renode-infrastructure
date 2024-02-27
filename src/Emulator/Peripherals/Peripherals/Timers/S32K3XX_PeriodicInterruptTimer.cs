6f8589086b87bd34bcdfef2f6297a76670d31a29
Marcin Witkowski <mwitkowski@antmicro.com>
2024-02-27 14:39:59 +0100

    [#54666] S32K3XX_PeriodicInterruptTimer: Add basic implementation

--- src/Emulator/Peripherals/Peripherals/Timers/S32K3XX_PeriodicInterruptTimer.cs
+++ src/Emulator/Peripherals/Peripherals/Timers/S32K3XX_PeriodicInterruptTimer.cs
@@ -5,29 +5,54 @@
 // Full license text is available in 'licenses/MIT.txt'.
 //
 using System;
+using System.Linq;
 using System.Collections.Generic;
 using Antmicro.Renode.Core;
 using Antmicro.Renode.Core.Structure.Registers;
-using Antmicro.Renode.Peripherals;
+using Antmicro.Renode.Time;
 
 namespace Antmicro.Renode.Peripherals.Timers
 {
     public class S32K3XX_PeriodicInterruptTimer : BasicDoubleWordPeripheral, IKnownSize
     {
-        public S32K3XX_PeriodicInterruptTimer(IMachine machine, bool hasRealTimeInterrupt = false, bool hasLifetimeTimer = false, bool supportsTimersChaining = false) : base(machine)
+        public S32K3XX_PeriodicInterruptTimer(IMachine machine, long oscillatorFrequency, bool hasRealTimeInterrupt = false, bool hasLifetimeTimer = false, bool supportsTimersChaining = false) : base(machine)
         {
-            DefineRegisters(hasRealTimeInterrupt, hasLifetimeTimer, supportsTimersChaining);
+            clockChannels = new SortedList<Registers, ClockChannel>();
+
+            IRQ = new GPIO();
+
+            DefineRegisters(oscillatorFrequency, hasRealTimeInterrupt, hasLifetimeTimer, supportsTimersChaining);
+            Reset();
+        }
+
+        public override void Reset()
+        {
+            base.Reset();
+            IRQ.Unset();
+
+            foreach(var clockChannel in clockChannels.Values)
+            {
+                clockChannel.Reset();
+            }
         }
 
         public long Size => 0x4000;
-        public GPIO IRQ { get; } = new GPIO();
+        public GPIO IRQ { get; }
 
-        private void DefineRegisters(bool hasRealTimeInterrupt, bool hasLifetimeTimer, bool supportsTimersChaining)
+        private void UpdateInterrupts()
         {
-            var moduleControl = Registers.ModuleControl.Define(this, hasRealTimeInterrupt ? 0x6U : 0x2U)
+            var interrupt = clockChannels.Values.Any(clockChannel => clockChannel.InterruptEnable && clockChannel.InterruptFlag);
+            IRQ.Set(interrupt);
+        }
+
+        private void DefineRegisters(long oscillatorFrequency, bool hasRealTimeInterrupt, bool hasLifetimeTimer, bool supportsTimersChaining)
+        {
+            var moduleControl = Registers.ModuleControl.Define(this)
                 .WithReservedBits(3, 29)
                 .WithTaggedFlag("ModuleDisableForPIT", 1)
-                .WithTaggedFlag("Freeze", 0);
+                .WithTaggedFlag("Freeze", 0)
+            ;
+
             if(hasRealTimeInterrupt)
             {
                 moduleControl.WithTaggedFlag("ModuleDisableForRTI", 2);
@@ -39,68 +64,200 @@ namespace Antmicro.Renode.Peripherals.Timers
 
             if(hasLifetimeTimer)
             {
-                Registers.UpperLifetimer.Define(this)
-                    .WithTag("LifetimerValue", 0, 32);
+                // NOTE: Technically value of those two registers is only valid when PIT has configured lifetimer,
+                // that is Channel 1 has enabled chain mode, and both Channel 0 and Channel 1 start values are set to
+                // 0xFFFF_FFFF, but documentation doesn't specify what we should read from lifetimer registers when that's
+                // not the case
+
                 Registers.LowerLifetimer.Define(this)
-                    .WithTag("LifetimerValue", 0, 32);
+                    .WithValueField(0, 32, out var lowerLifetimerValue, FieldMode.Read, name: "LifetimerValue");
+                ;
+
+                Registers.UpperLifetimer.Define(this)
+                    .WithValueField(0, 32, FieldMode.Read, name: "LifetimerValue",
+                        valueProviderCallback: _ =>
+                        {
+                            var channel0 = clockChannels[Registers.Control0];
+                            var channel1 = clockChannels[Registers.Control1];
+                            var lifetimerValue = ulong.MaxValue - ((channel0.Value << 32) | channel1.Value);
+
+                            lowerLifetimerValue.Value = lifetimerValue;
+                            return lifetimerValue >> 32;
+                        })
+                ;
             }
 
-            var channels = new Dictionary<Registers, bool>
-            {
-                {Registers.LoadValue0, false},
-                {Registers.LoadValue1, supportsTimersChaining},
-                {Registers.LoadValue2, supportsTimersChaining},
-                {Registers.LoadValue3, supportsTimersChaining},
-            };
             if(hasRealTimeInterrupt)
             {
-                channels.Add(Registers.LoadValueRTI, false);
+                DefineChannelRegisters(oscillatorFrequency, null, Registers.ControlRTI, Registers.FlagRTI, Registers.LoadValueRTI, Registers.CurrentValueRTI);
             }
-            foreach(var channel in channels)
-            {
-                DefineChannelRegisters(channel.Key, channel.Value);
-            }
+
+            DefineChannelRegisters(oscillatorFrequency, null, Registers.Control0, Registers.Flag0, Registers.LoadValue0, Registers.CurrentValue0);
+            DefineChannelRegisters(oscillatorFrequency, Registers.Control0, Registers.Control1, Registers.Flag1, Registers.LoadValue1, Registers.CurrentValue1);
+            DefineChannelRegisters(oscillatorFrequency, Registers.Control1, Registers.Control2, Registers.Flag2, Registers.LoadValue2, Registers.CurrentValue2);
+            DefineChannelRegisters(oscillatorFrequency, Registers.Control2, Registers.Control3, Registers.Flag3, Registers.LoadValue3, Registers.CurrentValue3);
 
             if(hasRealTimeInterrupt)
             {
                 Registers.LoadValueSyncStatusRTI.Define(this)
                     .WithReservedBits(1, 31)
-                    .WithTaggedFlag("SyncStatus", 0);
+                    .WithTaggedFlag("SyncStatus", 0)
+                ;
             }
         }
 
-        private void DefineChannelRegisters(Registers loadValueOffset, bool supportsChaining)
+        private void DefineChannelRegisters(long oscillatorFrequency, Registers? chainedTo, Registers control, Registers flag, Registers load, Registers currentValue)
         {
-            // All channels have the same register map.
-            var offsetToTimer0 = loadValueOffset - Registers.LoadValue0;
-            var currentValueOffset = Registers.CurrentValue0 + offsetToTimer0;
-            var controlOffset = Registers.Control0 + offsetToTimer0;
-            var flagOffset = Registers.Flag0 + offsetToTimer0;
+            var clockChannel = new ClockChannel(machine.ClockSource, this, oscillatorFrequency, Enum.GetName(typeof(Registers), control));
+            clockChannel.OnInterrupt += UpdateInterrupts;
 
-            loadValueOffset.Define(this)
-                .WithTag("StartValue", 0, 32);
-            currentValueOffset.Define(this)
-                .WithTag("CurrentValue", 0, 32);
-
-            var controlRegister = controlOffset.Define(this)
+            var controlRegister = control.Define(this)
                 .WithReservedBits(3, 29)
-                .WithTaggedFlag("InterruptEnable", 1)
-                .WithTaggedFlag("TimerEnable", 0);
-            if(supportsChaining)
+                .WithFlag(1, name: "InterruptEnable",
+                    valueProviderCallback: _ => clockChannel.InterruptEnable,
+                    changeCallback: (_, value) =>
+                    {
+                        clockChannel.InterruptEnable = value;
+                        UpdateInterrupts();
+                    })
+                .WithFlag(0, name: "TimerEnable",
+                    valueProviderCallback: _ => clockChannel.Enabled,
+                    changeCallback: (_, value) => clockChannel.Enabled = value)
+            ;
+
+            if(chainedTo != null)
             {
-                controlRegister.WithTaggedFlag("ChainMode", 2);
+                controlRegister.WithFlag(2, name: "ChainMode",
+                    valueProviderCallback: _ => clockChannel.ChainMode,
+                    writeCallback: (_, value) => clockChannel.ChainMode = value)
+                ;
+
+                var chainedTimer = clockChannels[chainedTo.Value];
+                chainedTimer.OnInterrupt += () =>
+                {
+                    if(clockChannel.ChainMode)
+                    {
+                        clockChannel.Step();
+                    }
+                };
             }
             else
             {
                 controlRegister.WithReservedBits(2, 1);
             }
 
-            flagOffset.Define(this)
+            flag.Define(this)
                 .WithReservedBits(1, 31)
-                .WithTaggedFlag("InterruptFlag", 0);
+                .WithFlag(0, name: "InterruptFlag",
+                    valueProviderCallback: _ => clockChannel.InterruptFlag,
+                    writeCallback: (_, value) => { if(value) clockChannel.InterruptFlag = false; } )
+                .WithWriteCallback((_, __) => UpdateInterrupts())
+            ;
+
+            load.Define(this)
+                .WithValueField(0, 32, name: "StartValue",
+                    valueProviderCallback: _ => clockChannel.StartValue,
+                    writeCallback: (_, value) => clockChannel.StartValue = value)
+            ;
+
+            currentValue.Define(this)
+                .WithValueField(0, 32, FieldMode.Read, name: "CurrentValue",
+                    valueProviderCallback: _ => clockChannel.Value)
+            ;
+
+            clockChannels.Add(control, clockChannel);
         }
 
-        public enum Registers
+        private readonly IDictionary<Registers, ClockChannel> clockChannels;
+
+        private class ClockChannel
+        {
+            public ClockChannel(IClockSource clockSource, IPeripheral parent, long frequency, string name)
+            {
+                underlyingTimer = new LimitTimer(clockSource, frequency, parent, name);
+                underlyingTimer.LimitReached += () =>
+                {
+                    InterruptFlag = true;
+                    OnInterrupt?.Invoke();
+                };
+            }
+
+            public void Step()
+            {
+                if(Value == 0)
+                {
+                    Value = StartValue;
+                }
+                else if(--Value == 0)
+                {
+                    InterruptFlag = true;
+                    OnInterrupt?.Invoke();
+                }
+            }
+
+            public void Reset()
+            {
+                Enabled = false;
+                InterruptEnable = false;
+                InterruptFlag = false;
+                Value = ulong.MaxValue;
+                StartValue = Value;
+                ChainMode = false;
+            }
+
+            public event Action OnInterrupt;
+
+            public bool Enabled
+            {
+                get => timerEnabled;
+                set
+                {
+                    timerEnabled = value;
+                    underlyingTimer.Enabled = value && !ChainMode;
+                }
+            }
+
+            public bool InterruptEnable
+            {
+                get => underlyingTimer.EventEnabled;
+                set => underlyingTimer.EventEnabled = value;
+            }
+
+            public bool InterruptFlag { get; set; }
+
+            public ulong Value
+            {
+                get => underlyingTimer.Value;
+                private set => underlyingTimer.Value = value;
+            }
+
+            public ulong StartValue
+            {
+                get => underlyingTimer.Limit;
+                set
+                {
+                    underlyingTimer.Limit = value;
+                    underlyingTimer.Value = value;
+                }
+            }
+
+            public bool ChainMode
+            {
+                get => chainMode;
+                set
+                {
+                    chainMode = value;
+                    Enabled = timerEnabled;
+                }
+            }
+
+            private readonly LimitTimer underlyingTimer;
+
+            private bool chainMode;
+            private bool timerEnabled;
+        }
+
+        private enum Registers
         {
             ModuleControl = 0x0, // MCR
             UpperLifetimer = 0xE0, // LTMR64H
