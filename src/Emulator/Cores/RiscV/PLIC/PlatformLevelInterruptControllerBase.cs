73f27c0bf62c8a578d5475a2c4665f07a756345d
Andrzej Drabarek <adrabarek@antmicro.com>
2024-03-04 16:04:03 +0100

    [DEV] Update copyright, fix formatting

--- src/Emulator/Cores/RiscV/PLIC/PlatformLevelInterruptControllerBase.cs
+++ src/Emulator/Cores/RiscV/PLIC/PlatformLevelInterruptControllerBase.cs
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2010-2023 Antmicro
+// Copyright (c) 2010-2024 Antmicro
 //
 // This file is licensed under the MIT License.
 // Full license text is available in 'licenses/MIT.txt'.
@@ -81,8 +81,10 @@ namespace Antmicro.Renode.Peripherals.IRQControllers.PLIC
                 var irq = irqSources[number];
                 irq.State = value;
 
-                if(value) {
-                    foreach(var irqContext in irqContexts) {
+                if(value)
+                {
+                    foreach(var irqContext in irqContexts)
+                    {
                         irqContext.MarkSourceAsPending(irq);
                     }
                 }
