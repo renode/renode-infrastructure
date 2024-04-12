177526abe93e81e95b3caf9938edc3ad47e761be
Adam Jeliński <ajelinski@antmicro.com>
2024-04-12 07:31:51 +0200

    [#53750] BusRegistered: Add ToString

--- src/Emulator/Main/Peripherals/Bus/BusRegistered.cs
+++ src/Emulator/Main/Peripherals/Bus/BusRegistered.cs
@@ -1,12 +1,10 @@
 //
-// Copyright (c) 2010-2018 Antmicro
+// Copyright (c) 2010-2024 Antmicro
 // Copyright (c) 2011-2015 Realtime Embedded
 //
 // This file is licensed under the MIT License.
 // Full license text is available in 'licenses/MIT.txt'.
 //
-
-using Antmicro.Renode.Peripherals;
 
 namespace Antmicro.Renode.Peripherals.Bus
 {
@@ -17,6 +15,11 @@ namespace Antmicro.Renode.Peripherals.Bus
 			Peripheral = what;
             RegistrationPoint = where;
 		}
+
+        public override string ToString()
+        {
+            return $"{Peripheral.GetName()} registered at {RegistrationPoint}";
+        }
 
 		public T Peripheral { get; private set; }
         public BusRangeRegistration RegistrationPoint { get; private set; }
