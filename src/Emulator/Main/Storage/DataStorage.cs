cf6178d6197e1801f6e2dbc7d51f0104d6b99f2c
Franciszek Pindel <fpindel@antmicro.com>
2024-04-22 13:03:59 +0200

    [#58237] DataStorage: Rename methods

--- src/Emulator/Main/Storage/DataStorage.cs
+++ src/Emulator/Main/Storage/DataStorage.cs
@@ -8,12 +8,19 @@
 using System.IO;
 using Antmicro.Renode.Exceptions;
 using Antmicro.Renode.Utilities;
+using Antmicro.Renode.Logging;
 
 namespace Antmicro.Renode.Storage
 {
     public static class DataStorage
     {
         public static Stream Create(string imageFile, long? size = null, bool persistent = false, byte paddingByte = 0)
+        {
+            Logger.Log(LogLevel.Warning, "Create method is obsolete, please use CreateFromFile method");
+            return CreateFromFile(imageFile, size, persistent, paddingByte);
+        }
+
+        public static Stream CreateFromFile(string imageFile, long? size = null, bool persistent = false, byte paddingByte = 0)
         {
             if(string.IsNullOrEmpty(imageFile))
             {
@@ -32,7 +39,19 @@ namespace Antmicro.Renode.Storage
 
         public static Stream Create(long size, byte paddingByte = 0)
         {
+            Logger.Log(LogLevel.Warning, "Create method is obsolete, please use CreateInTemporaryFile method");
+            return CreateInTemporaryFile(size, paddingByte);
+        }
+
+        public static Stream CreateInTemporaryFile(long size, byte paddingByte = 0)
+        {
             return new SerializableStreamView(new FileStream(TemporaryFilesManager.Instance.GetTemporaryFile(), FileMode.OpenOrCreate), size, paddingByte);
+        }
+
+        public static Stream CreateInMemory(int size, byte paddingByte = 0)
+        {
+            var mem = new byte[size];
+            return new SerializableStreamView(new MemoryStream(mem), size, paddingByte);
         }
     }
 }
