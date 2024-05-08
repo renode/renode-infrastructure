2f6f1baeb1559adb7cf29e79633144d879bb1b98
Adam Jeliński <ajelinski@antmicro.com>
2024-05-08 17:27:13 +0200

    [#58660] IBusRegistered: Convert: Preserve CPU

--- src/Emulator/Main/Peripherals/Bus/IBusRegistered.cs
+++ src/Emulator/Main/Peripherals/Bus/IBusRegistered.cs
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2010-2018 Antmicro
+// Copyright (c) 2010-2024 Antmicro
 // Copyright (c) 2011-2015 Realtime Embedded
 //
 // This file is licensed under the MIT License.
@@ -20,7 +20,7 @@ namespace Antmicro.Renode.Peripherals.Bus
         public static IBusRegistered<TTo> Convert<TFrom, TTo>(this IBusRegistered<TFrom> conversionSource) where TTo : TFrom where TFrom : IBusPeripheral
         {
             return new BusRegistered<TTo>((TTo)conversionSource.Peripheral, new BusRangeRegistration(conversionSource.RegistrationPoint.Range,
-                                          conversionSource.RegistrationPoint.Offset));
+                                          conversionSource.RegistrationPoint.Offset, conversionSource.RegistrationPoint.CPU));
         }
 
         public static IEnumerable<IBusRegistered<TTo>> Convert<TFrom, TTo>(this IEnumerable<IBusRegistered<TFrom>> sourceCollection) where TTo : TFrom where TFrom : IBusPeripheral
