5b71a1f4365630dcc77ad1aab95ca863ae488b15
Jakub Jatczak <jjatczak@antmicro.com>
2024-05-20 14:57:50 +0200

    [#58508] DmaEngine: Refactor IssueCopy

--- src/Emulator/Main/Peripherals/DMA/DmaEngine.cs
+++ src/Emulator/Main/Peripherals/DMA/DmaEngine.cs
@@ -37,114 +37,119 @@ namespace Antmicro.Renode.Peripherals.DMA
             }
 
             var buffer = new byte[request.Size];
-            IBusRegistered<IBusPeripheral> whatIsAt;
+            var sourceAddress = request.Source.Address ?? 0;
+            var whatIsAtSource = sysbus.WhatIsAt(sourceAddress, context);
+            var isSourceContinuousMemory = (whatIsAtSource == null || whatIsAtSource.Peripheral is MappedMemory) // Not a peripheral
+                                                        && readLengthInBytes == request.SourceIncrementStep; // Consistent memory region
             if(!request.Source.Address.HasValue)
             {
+                // request array based copy
                 Array.Copy(request.Source.Array, request.Source.StartIndex.Value, buffer, 0, request.Size);
             }
-            else
+            else if(isSourceContinuousMemory)
             {
-                var sourceAddress = request.Source.Address.Value;
-                whatIsAt = sysbus.WhatIsAt(sourceAddress, context);
-                //allow ReadBytes if the read memory is without gaps
-                if((whatIsAt == null || whatIsAt.Peripheral is MappedMemory) && readLengthInBytes == request.SourceIncrementStep)
+                if(request.IncrementReadAddress)
                 {
+                        // Transfer Units |  1  |  2  |  3  |  4  |
+                        // Source         |  A  |  B  |  C  |  D  |
+                        // Copied         |  A  |  B  |  C  |  D  |
+                    sysbus.ReadBytes(sourceAddress, request.Size, buffer, 0, context: context);
+                    response.ReadAddress += (ulong)request.Size;
+                }
+                else
+                {
+                    // When reading from the memory with IncrementReadAddress unset, effectively, only the last unit will be used
+                    // Transfer Units |  1  |  2  |  3  |  4  |
+                    // Source         |  A  |  B  |  C  |  D  |
+                    // Copied         |  D  |     |     |     |
+                    sysbus.ReadBytes(sourceAddress, readLengthInBytes, buffer, 0, context: context);
+                }
+            }
+            else if(whatIsAtSource != null)
+            {
+                var transferred = 0;
+                var offset = 0UL;
+                while(transferred < request.Size)
+                {
+                    var readAddress = sourceAddress + offset;
+                    switch(request.ReadTransferType)
+                    {
+                    case TransferType.Byte:
+                        buffer[transferred] = sysbus.ReadByte(readAddress, context);
+                        break;
+                    case TransferType.Word:
+                        BitConverter.GetBytes(sysbus.ReadWord(readAddress, context)).CopyTo(buffer, transferred);
+                        break;
+                    case TransferType.DoubleWord:
+                        BitConverter.GetBytes(sysbus.ReadDoubleWord(readAddress, context)).CopyTo(buffer, transferred);
+                        break;
+                    case TransferType.QuadWord:
+                        BitConverter.GetBytes(sysbus.ReadQuadWord(readAddress, context)).CopyTo(buffer, transferred);
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException($"Requested read transfer size: {request.ReadTransferType} is not supported by DmaEngine");
+                    }
+                    transferred += readLengthInBytes;
                     if(request.IncrementReadAddress)
                     {
-                        sysbus.ReadBytes(sourceAddress, request.Size, buffer, 0, context: context);
-                        response.ReadAddress += (ulong)request.Size;
-                    }
-                    else
-                    {
-                        sysbus.ReadBytes(sourceAddress, readLengthInBytes, buffer, 0, context: context);
-                    }
-                }
-                else if(whatIsAt != null)
-                {
-                    var transferred = 0;
-                    var offset = 0UL;
-                    while(transferred < request.Size)
-                    {
-                        var readAddress = sourceAddress + offset;
-                        switch(request.ReadTransferType)
-                        {
-                        case TransferType.Byte:
-                            buffer[transferred] = sysbus.ReadByte(readAddress, context);
-                            break;
-                        case TransferType.Word:
-                            BitConverter.GetBytes(sysbus.ReadWord(readAddress, context)).CopyTo(buffer, transferred);
-                            break;
-                        case TransferType.DoubleWord:
-                            BitConverter.GetBytes(sysbus.ReadDoubleWord(readAddress, context)).CopyTo(buffer, transferred);
-                            break;
-                        case TransferType.QuadWord:
-                            BitConverter.GetBytes(sysbus.ReadQuadWord(readAddress, context)).CopyTo(buffer, transferred);
-                            break;
-                        default:
-                            throw new ArgumentOutOfRangeException($"Requested read transfer size: {request.ReadTransferType} is not supported by DmaEngine");
-                        }
-                        transferred += readLengthInBytes;
-                        if(request.IncrementReadAddress)
-                        {
-                            offset += request.SourceIncrementStep;
-                            response.ReadAddress += request.SourceIncrementStep;
-                        }
+                        offset += request.SourceIncrementStep;
+                        response.ReadAddress += request.SourceIncrementStep;
                     }
                 }
             }
 
+            var destinationAddress = request.Destination.Address ?? 0;
+            var whatIsAtDestination = sysbus.WhatIsAt(destinationAddress);
+            var isDestinationContinuousMemory = (whatIsAtDestination == null || whatIsAtDestination.Peripheral is MappedMemory) // Not a peripheral
+                                                        && readLengthInBytes == request.DestinationIncrementStep;  // Consistent memory region
             if(!request.Destination.Address.HasValue)
             {
+                // request array based copy
                 Array.Copy(buffer, 0, request.Destination.Array, request.Destination.StartIndex.Value, request.Size);
             }
-            else
+            else if(isDestinationContinuousMemory)
             {
-                var destinationAddress = request.Destination.Address.Value;
-                whatIsAt = sysbus.WhatIsAt(destinationAddress);
-                if((whatIsAt == null || whatIsAt.Peripheral is MappedMemory) && writeLengthInBytes == request.DestinationIncrementStep)
+                if(request.IncrementWriteAddress)
                 {
-                    if(request.IncrementWriteAddress)
-                    {
-                        sysbus.WriteBytes(buffer, destinationAddress, context: context);
-                        response.WriteAddress += (ulong)request.Size;
-                    }
-                    else
-                    {
-                        // if the place to write is memory and we're not incrementing address, effectively only the last element is written
-                        var skipCount = (request.Size == writeLengthInBytes) ? 0 : request.Size - writeLengthInBytes;
-                        DebugHelper.Assert((skipCount + request.Size) <= buffer.Length);
-                        sysbus.WriteBytes(buffer.Skip(skipCount).ToArray(), destinationAddress, context: context);
-                    }
+                    sysbus.WriteBytes(buffer, destinationAddress, context: context);
+                    response.WriteAddress += (ulong)request.Size;
                 }
                 else
                 {
-                    var transferred = 0;
-                    var offset = 0UL;
-                    while(transferred < request.Size)
+                    // if the place to write is memory and we're not incrementing address, effectively only the last element is written
+                    var skipCount = (request.Size == writeLengthInBytes) ? 0 : request.Size - writeLengthInBytes;
+                    DebugHelper.Assert((skipCount + request.Size) <= buffer.Length);
+                    sysbus.WriteBytes(buffer.Skip(skipCount).ToArray(), destinationAddress, context: context);
+                }
+            }
+            else if(whatIsAtDestination != null)
+            {
+                var transferred = 0;
+                var offset = 0UL;
+                while(transferred < request.Size)
+                {
+                    switch(request.WriteTransferType)
                     {
-                        switch(request.WriteTransferType)
-                        {
-                        case TransferType.Byte:
-                            sysbus.WriteByte(destinationAddress + offset, buffer[transferred], context);
-                            break;
-                        case TransferType.Word:
-                            sysbus.WriteWord(destinationAddress + offset, BitConverter.ToUInt16(buffer, transferred), context);
-                            break;
-                        case TransferType.DoubleWord:
-                            sysbus.WriteDoubleWord(destinationAddress + offset, BitConverter.ToUInt32(buffer, transferred), context);
-                            break;
-                        case TransferType.QuadWord:
-                            sysbus.WriteQuadWord(destinationAddress + offset, BitConverter.ToUInt64(buffer, transferred), context);
-                            break;
-                        default:
-                            throw new ArgumentOutOfRangeException($"Requested write transfer size: {request.WriteTransferType} is not supported by DmaEngine");
-                        }
-                        transferred += writeLengthInBytes;
-                        if(request.IncrementWriteAddress)
-                        {
-                            offset += request.DestinationIncrementStep;
-                            response.WriteAddress += request.DestinationIncrementStep;
-                        }
+                    case TransferType.Byte:
+                        sysbus.WriteByte(destinationAddress + offset, buffer[transferred], context);
+                        break;
+                    case TransferType.Word:
+                        sysbus.WriteWord(destinationAddress + offset, BitConverter.ToUInt16(buffer, transferred), context);
+                        break;
+                    case TransferType.DoubleWord:
+                        sysbus.WriteDoubleWord(destinationAddress + offset, BitConverter.ToUInt32(buffer, transferred), context);
+                        break;
+                    case TransferType.QuadWord:
+                        sysbus.WriteQuadWord(destinationAddress + offset, BitConverter.ToUInt64(buffer, transferred), context);
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException($"Requested write transfer size: {request.WriteTransferType} is not supported by DmaEngine");
+                    }
+                    transferred += writeLengthInBytes;
+                    if(request.IncrementWriteAddress)
+                    {
+                        offset += request.DestinationIncrementStep;
+                        response.WriteAddress += request.DestinationIncrementStep;
                     }
                 }
             }
