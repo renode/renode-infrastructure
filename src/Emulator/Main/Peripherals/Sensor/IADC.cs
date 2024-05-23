66df5620efdd436fade686430f2f26d9d7f9eb6b
Wojciech Meller <wmeller@antmicro.com>
2024-05-23 02:21:38 +0200

    [#59551] Add IADC

---
+++ src/Emulator/Main/Peripherals/Sensor/IADC.cs
@@ -0,0 +1,18 @@
+﻿//
+// Copyright (c) 2010-2024 Antmicro
+//
+//  This file is licensed under the MIT License.
+//  Full license text is available in 'licenses/MIT.txt'.
+//
+
+namespace Antmicro.Renode.Peripherals.Sensor
+{
+    public interface IADC : ISensor
+    {
+        // ADC value should be treated as RESD VoltageSample value
+        void SetADCValue(int channel, uint value);
+        uint GetADCValue(int channel);
+
+        int ADCChannelCount { get; }
+    }
+}
