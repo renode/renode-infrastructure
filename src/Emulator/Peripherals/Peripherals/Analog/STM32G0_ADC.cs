96b757088586872d44d580639850e08768185ab3
Mateusz Holenko <mholenko@antmicro.com>
2024-06-11 21:08:23 +0200

    [#60603] Add STM32G0 ADC model

---
+++ src/Emulator/Peripherals/Peripherals/Analog/STM32G0_ADC.cs
@@ -0,0 +1,33 @@
+//
+// Copyright (c) 2010-2024 Antmicro
+//
+// This file is licensed under the MIT License.
+// Full license text is available in 'licenses/MIT.txt'.
+//
+
+using Antmicro.Renode.Core;
+using Antmicro.Renode.Peripherals.DMA;
+
+namespace Antmicro.Renode.Peripherals.Analog
+{
+    public class STM32G0_ADC : STM32_ADC_Common
+    {
+        public STM32G0_ADC(IMachine machine, double referenceVoltage, uint externalEventFrequency, int dmaChannel = 0, IDMA dmaPeripheral = null)
+            : base(
+                machine,
+                referenceVoltage,
+                externalEventFrequency,
+                dmaChannel,
+                dmaPeripheral,
+                // Base class configuration
+                watchdogCount: 1,
+                hasCalibration: false,
+                channelCount: 19,
+                hasPrescaler: false,
+                hasVbatPin: true,
+                hasChannelSequence: true,
+                hasPowerRegister: false
+            )
+        {}
+    }
+}
