6004136e3a8c7fbec9d91d7a4ca97c8657e1cdec
Mateusz Holenko <mholenko@antmicro.com>
2024-06-20 13:13:49 +0200

    [DEV] STM32_UART: Clear interrupts on reset

--- src/Emulator/Peripherals/Peripherals/UART/STM32_UART.cs
+++ src/Emulator/Peripherals/Peripherals/UART/STM32_UART.cs
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2010-2023 Antmicro
+// Copyright (c) 2010-2024 Antmicro
 //
 // This file is licensed under the MIT License.
 // Full license text is available in 'licenses/MIT.txt'.
@@ -40,6 +40,7 @@ namespace Antmicro.Renode.Peripherals.UART
         {
             base.Reset();
             receiveFifo.Clear();
+            IRQ.Set(false);
         }
 
         public uint BaudRate
