c2b42377d66b9712412f42d61f259f5692e5cc67
Sebastian Kwaśniak <skwasniak@internships.antmicro.com>
2024-07-11 16:25:30 +0200

    [#62335] KB1200_UART: input support

--- src/Emulator/Peripherals/Peripherals/UART/KB1200_UART.cs
+++ src/Emulator/Peripherals/Peripherals/UART/KB1200_UART.cs
@@ -8,6 +8,9 @@ using Antmicro.Renode.Peripherals.Bus;
 using Antmicro.Renode.Core;
 using System.Collections.Generic;
 using Antmicro.Renode.Core.Structure.Registers;
+using Antmicro.Renode.Logging;
+using Antmicro.Renode.Utilities;
+using System;
 
 namespace Antmicro.Renode.Peripherals.UART
 {
@@ -18,45 +21,60 @@ namespace Antmicro.Renode.Peripherals.UART
             var registersMap = new Dictionary<long, DoubleWordRegister>();
 
             registersMap.Add((long)Registers.Configuration, new DoubleWordRegister(this)
-                .WithTaggedFlag(name: "SERIE_RX_ENABLE", 0)
-                .WithTaggedFlag(name: "SERIE_TX_ENABLE", 1)
+                .WithTaggedFlag("SERIE_RX_ENABLE", 0)
+                .WithFlag(1, out serieTxEnable, name: "SERIE_TX_ENABLE")
                 .WithTag("Parity", 2, 2)
                 .WithReservedBits(6, 10)
                 .WithTag("Baud Rate", 16, 16)
             );
             
             registersMap.Add((long)Registers.InterruptEnable, new DoubleWordRegister(this)
-                .WithTag("SERIE", 0, 3)
-                .WithReservedBits(4, 28)
+                .WithFlag(0, out serieIrqRxEnable, name: "SERIE_IRQ_RX_ENABLE")
+                .WithFlag(1, out serieIrqTxEnable, name: "SERIE_IRQ_TX_ENABLE")
+                .WithTaggedFlag("SERIE_IRQ_RX_ERROR", 2)
+                .WithReservedBits(3, 29)
+                .WithWriteCallback((_, __) => UpdateInterrupts())
             );
             
             registersMap.Add((long)Registers.PendingFlag, new DoubleWordRegister(this)
-                .WithTag("SERPF", 0, 3)
+                .WithFlag(0, out serpfRxCntFull, 
+                    FieldMode.Read | FieldMode.WriteOneToClear, 
+                    name: "SERPF_RX_CNT_FULL")
+                // In Renode, the TX happens instantly
+                .WithFlag(1, FieldMode.Read, valueProviderCallback: _ => true, name: "SEPRF_TX_EMPTY")
+                .WithTaggedFlag("SERPF_RX_ERROR", 2)
                 .WithReservedBits(4, 28)
+                .WithWriteCallback((_, __) => UpdateInterrupts())
             );
             
             registersMap.Add((long)Registers.Status, new DoubleWordRegister(this)
                 .WithTaggedFlag("TX_FULL", 0)
                 .WithTaggedFlag("TX_OVERRUN", 1)
                 .WithTaggedFlag("TX_BUSY", 2)
-                .WithReservedBits(3, 12)
-                .WithTaggedFlag("RX_EMPTY", 16)
-                .WithTaggedFlag("RX_OVERRUN", 17)
-                .WithTaggedFlag("RX_BUSY", 18)
-                .WithTaggedFlag("RX_TIMEOUT", 19)
-                .WithReservedBits(20, 4)
-                .WithTaggedFlag("PARITY_ERROR", 24)
-                .WithTaggedFlag("FRAME_ERROR", 25)
-                .WithReservedBits(26, 6)
+                .WithReservedBits(3, 1)
+                .WithFlag(4, FieldMode.Read, name: "RX_EMPTY", 
+                    valueProviderCallback: _ => receiveQueue.Count == 0)
+                .WithTaggedFlag("RX_OVERRUN", 5)
+                .WithTaggedFlag("RX_BUSY", 6)
+                .WithTaggedFlag("RX_TIMEOUT", 7)
+                .WithTaggedFlag("PARITY_ERROR", 8)
+                .WithTaggedFlag("FRAME_ERROR", 9)
+                .WithReservedBits(10, 22)
             );
             
             registersMap.Add((long)Registers.RxDataBuffer, new DoubleWordRegister(this)
-                .WithTag("SERTBUF", 0, 8)
+                .WithValueField(0, 8, 
+                    FieldMode.Read, 
+                    name: "SERTBUF", 
+                    valueProviderCallback: _ => HandleReceiveData())
                 .WithReservedBits(8, 24)
             );
 
             registersMap.Add((long)Registers.TxDataBuffer, new DoubleWordRegister(this)
-                .WithValueField(0, 8, FieldMode.Write, name: "SERRBUF", writeCallback: (_, v) => TransmitCharacter((byte)v))
+                .WithValueField(0, 8, 
+                    FieldMode.Write, 
+                    name: "SERRBUF", 
+                    writeCallback: (_, v) => HandleTransmitData((uint)v))
                 .WithReservedBits(8, 24)
             );
             
@@ -65,18 +83,36 @@ namespace Antmicro.Renode.Peripherals.UART
                 .WithReservedBits(3, 29)
             );
 
-            TxInterrupt = new GPIO();
-            RxInterrupt = new GPIO();
-
             registers = new DoubleWordRegisterCollection(this, registersMap);
+        }
+
+        public override void WriteChar(byte value)
+        {
+            if(!IsReceiveEnabled)
+            {
+                this.Log(LogLevel.Warning, "Char was received, but the receiver (or the whole USART) is not enabled. Ignoring.");
+                return;
+            }
+            // We assume FIFO size is 1
+            serpfRxCntFull.Value = true;
+            receiveQueue.Enqueue(value);
+            UpdateInterrupts();
+        }
+
+        public void UpdateInterrupts() 
+        {
+            bool irqPending = (serieIrqRxEnable.Value && serpfRxCntFull.Value) 
+                            || (serieIrqTxEnable.Value);
+            this.Log(LogLevel.Noisy, "Setting IRQ: {0}", irqPending);
+            IRQ.Set(irqPending);
         }
 
         public override void Reset()
         {
             base.Reset();
             registers.Reset();
-            TxInterrupt.Set(false);
-            RxInterrupt.Set(false);
+            receiveQueue.Clear();
+            IRQ.Set(false);
         }
 
         public uint ReadDoubleWord(long offset)
@@ -89,8 +125,7 @@ namespace Antmicro.Renode.Peripherals.UART
             registers.Write(offset, val);
         }
 
-        public GPIO TxInterrupt { get; }
-        public GPIO RxInterrupt { get; }
+        public GPIO IRQ { get; } = new GPIO();
 
         public override Bits StopBits { get; }
 
@@ -109,6 +144,32 @@ namespace Antmicro.Renode.Peripherals.UART
         {
             // intentionally left blank
         }
+
+        private uint HandleReceiveData()
+        {
+            if(receiveQueue.TryDequeue(out var result))
+            {
+                UpdateInterrupts();
+            }
+            return result;
+        }
+
+        private void HandleTransmitData(uint value)
+        {
+            if(!serieTxEnable.Value)
+            {
+                this.Log(LogLevel.Warning, "Char was to be sent, but the transmitter (or the whole USART) is not enabled. Ignoring.");
+                return;
+            }
+            TransmitCharacter((byte)value);
+        }
+
+        private readonly IFlagRegisterField serieIrqRxEnable;
+        private readonly IFlagRegisterField serieTxEnable;
+        private readonly IFlagRegisterField serieIrqTxEnable;
+        private readonly IFlagRegisterField serpfRxCntFull;
+
+        private readonly Queue<byte> receiveQueue = new Queue<byte>();
 
         private readonly DoubleWordRegisterCollection registers;
 
