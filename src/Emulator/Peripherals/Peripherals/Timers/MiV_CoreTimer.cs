559c43809a7f988d41c9e48ca676263c93aec451
Jan Olencki <jolencki@antmicro.com>
2024-07-29 16:16:22 +0200

    [#63119] MiV_CoreTimer: Remove redundant using

--- src/Emulator/Peripherals/Peripherals/Timers/MiV_CoreTimer.cs
+++ src/Emulator/Peripherals/Peripherals/Timers/MiV_CoreTimer.cs
@@ -4,7 +4,6 @@
 // This file is licensed under the MIT License.
 // Full license text is available in 'licenses/MIT.txt'.
 //
-using System;
 using System.Collections.Generic;
 using Antmicro.Renode.Core;
 using Antmicro.Renode.Core.Structure.Registers;
