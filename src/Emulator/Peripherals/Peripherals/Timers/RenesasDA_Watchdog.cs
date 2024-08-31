7367f9c320a2d5bd431491eeb7d4bca57251866d
Adam Jeliński <ajelinski@antmicro.com>
2024-08-31 11:45:42 +0200

    [#63672] RenesasDA_Watchdog: Add freeze support

--- src/Emulator/Peripherals/Peripherals/Timers/RenesasDA_Watchdog.cs
+++ src/Emulator/Peripherals/Peripherals/Timers/RenesasDA_Watchdog.cs
@@ -67,24 +67,77 @@ namespace Antmicro.Renode.Peripherals.Timers
                 .WithFlag(3, FieldMode.Read, name: "WRITE_BUSY",
                     valueProviderCallback: _ => false // Not implemented. For now it's never busy.
                 )
-                .WithTaggedFlag("WDOG_FREEZE_EN", 2)
+                // Controls whether watchdog can be frozen by `RenesasDA14_GeneralPurposeRegisters`
+                // but it can only happen with `NMI_RST` unset; see `Frozen`.
+                .WithFlag(2, out watchdogFreezeEnabled, name: "WDOG_FREEZE_EN")
                 .WithReservedBits(1, 1)
-                .WithFlag(0, out nonMaskableInterruptReset, name: "NMI_RST");
+                .WithFlag(0, out nonMaskableInterruptReset, name: "NMI_RST")
+                .WithChangeCallback((oldValue, newValue) =>
+                {
+                    // Unsetting `WDOG_FREEZE_EN` or setting `NMI_RST` clears freeze.
+                    UpdateEnabled();
+                });
         }
 
         public override void Reset()
         {
             IRQ.Unset();
-            ticker.Reset();
+            ticker.Reset();  // The ticker is enabled by default so it's also enabled after reset.
             base.Reset();
+
+            frozen = false;
+            resetRequested = false;
         }
 
-        // Start/Stop is performed on registers that don't belong to this peripheral.
-        // 'Enabled' should be used by the peripheral that handles the start/stop.
-        public bool Enabled
+        // Frozen keeps the value even if it effectively doesn't freeze the ticker due to `NMI_RST` or
+        // `WDOG_FREEZE_EN` so changing them might lead to immediate freeze if frozen is set. Datasheet
+        // isn't clear on what happens in such cases but this behavior seems reasonable.
+        public bool Frozen
         {
             get => ticker.Enabled;
-            set => ticker.Enabled = value;
+            set
+            {
+                if(value != frozen)
+                {
+                    this.NoisyLog("Attempting to {0} freeze", value ? "set" : "reset");
+                    frozen = value;
+                    UpdateEnabled();
+                }
+            }
+        }
+
+        // Only this function, except for Reset, should enable and disable ticker.
+        // Use it every time conditions for ticker change.
+        private void UpdateEnabled()
+        {
+            if(resetRequested)
+            {
+                ticker.Enabled = false;
+                return;
+            }
+
+            var newEnabled = true;
+            if(frozen)
+            {
+                if(nonMaskableInterruptReset.Value)
+                {
+                    this.DebugLog("Ignoring freeze because {0} is set", nameof(nonMaskableInterruptReset));
+                }
+                else if(!watchdogFreezeEnabled.Value)
+                {
+                    this.DebugLog("Ignoring freeze because {0} isn't set", nameof(watchdogFreezeEnabled));
+                }
+                else
+                {
+                    newEnabled = false;
+                }
+            }
+
+            if(ticker.Enabled != newEnabled)
+            {
+                this.NoisyLog("Freeze {0}", newEnabled ? "unset" : "set");
+                ticker.Enabled = newEnabled;
+            }
         }
 
         public long Size => 0x10;
@@ -127,15 +180,20 @@ namespace Antmicro.Renode.Peripherals.Timers
             {
                 // Limit for reset
                 this.Log(LogLevel.Warning, "Reseting machine");
-                Enabled = false;
+                resetRequested = true;
+                UpdateEnabled();
                 machine.RequestReset();
             }
         }
+
+        private bool frozen;
+        private bool resetRequested;
 
         private readonly LimitTimer ticker;
         private readonly IDoubleWordPeripheral nvic;
 
         private IValueRegisterField writeLockFilter;
+        private IFlagRegisterField watchdogFreezeEnabled;
         private IFlagRegisterField nonMaskableInterruptReset;
 
         // In hardware it counts down from 0x1fff to negative 0x10. Mock the negative range by starting from +0x10.
