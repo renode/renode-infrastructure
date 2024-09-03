1f1e7a0756fe824e1f05deb4a1e1649292262d73
Jan Olencki <jolencki@antmicro.com>
2024-09-03 11:56:25 +0200

    [#51951] MPFS_DDRMock: add a few missing registers

--- src/Emulator/Peripherals/Peripherals/Miscellaneous/MPFS_DDRMock.cs
+++ src/Emulator/Peripherals/Peripherals/Miscellaneous/MPFS_DDRMock.cs
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2010-2023 Antmicro
+// Copyright (c) 2010-2024 Antmicro
 // Copyright (c) 2020-2023 Microchip
 //
 // This file is licensed under the MIT License.
@@ -22,34 +22,43 @@ namespace Antmicro.Renode.Peripherals.Miscellaneous
             var value = 0x0u;
             switch(offset)
             {
-                case 0x0B4: //In HW, this would return 0x00, returning this signature instead.
-                    value = 0x52454E44;
+                case 0x08: /* DDRPHY_STARTUP */
+                    value = 0x200;
                     break;
-                case 0x864:
-                    value = 0x44332211;
+                case 0x010: /* PLL_DI_0_1 */
+                    value = 0x200;
                     break;
-                case 0x868:
-                    value = 0xDDCCBBAA;
-                    break;
-                case 0x81C:
-                    value = 0x0;
-                    break;
-                case 0x820:
-                    value = 0x0;
-                    break;
-                case 0x824:
+                case 0x02C: /* SSCG_REG_2 */
                     value = 0xFFFFFFFF;
                     break;
-                case 0x834:
+                case 0x0B4: /* RPC_RESET_MAIN_PLL */
+                    value = 0x52454E44; //In HW, this would return 0x00, returning this signature instead.
+                    break;
+                case 0x864: /* ADDCMD_STATUS0 */
+                    value = 0x44332211;
+                    break;
+                case 0x868: /* ADDCMD_STATUS_1 */
+                    value = 0xDDCCBBAA;
+                    break;
+                case 0x81C: /* GT_ERR_COMB */
+                    value = 0x0;
+                    break;
+                case 0x820: /* GT_CLK_SEL */
+                    value = 0x0;
+                    break;
+                case 0x824: /* GT_TXDLY */
+                    value = 0xFFFFFFFF;
+                    break;
+                case 0x834: /* DQ_DQS_ERR_DONE */
                     value = 0x8;
                     break;
-                case 0x84C:
+                case 0x84C: /* DQDQS_STATUS1 */
                     value = 0xFF;
                     break;
-                case 0x850:
+                case 0x850: /* DQDQS_STATUS2 */
                     value = 0xFF;
                     break;
-                case 0xC08:
+                case 0xC08: /* PLL_CNTL */
                     value = 0xFF;
                     break;
                 default:
