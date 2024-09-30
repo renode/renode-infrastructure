a029a4f4f68dab522e79e0f9dd290442e9f326d9
Mateusz Holenko <mholenko@antmicro.com>
2024-09-30 10:54:31 +0200

    [#66197] ICPUWithRegisters: Access CPU registers by name

--- src/Emulator/Main/Peripherals/CPU/ICPUWithRegisters.cs
+++ src/Emulator/Main/Peripherals/CPU/ICPUWithRegisters.cs
@@ -34,16 +34,52 @@ namespace Antmicro.Renode.Peripherals.CPU
             return cpu.GetRegister(register).RawValue;
         }
 
+        public static ulong GetRegisterUlong(this ICPUWithRegisters cpu, string register)
+        {
+            return cpu.GetRegister(register).RawValue;
+        }
+
         public static void SetRegisterUlong(this ICPUWithRegisters cpu, int register, ulong value)
         {
             cpu.SetRegister(register, value);
+        }
+
+        public static void SetRegisterUlong(this ICPUWithRegisters cpu, string register, ulong value)
+        {
+            cpu.SetRegister(register, value);
+        }
+
+        public static void SetRegister(this ICPUWithRegisters cpu, string register, RegisterValue value)
+        {
+            // for dynamically added registers (e.g., V ones for RISC-V) it's possible for `Alias` to be null;
+            // this needs to be investigated, but for now let's just filter such situations out
+            var reg = cpu.GetRegisters().FirstOrDefault(x => x.Aliases != null && x.Aliases.Contains(register));
+            // `reg` is a struct so it'll never be `null`
+            if(reg.Aliases == null)
+            {
+                throw new RecoverableException($"Wrong register name: {register}");
+            }
+            cpu.SetRegister(reg.Index, value);
+        }
+
+        public static RegisterValue GetRegister(this ICPUWithRegisters cpu, string register)
+        {
+            // for dynamically added registers (e.g., V ones for RISC-V) it's possible for `Alias` to be null;
+            // this needs to be investigated, but for now let's just filter such situations out
+            var reg = cpu.GetRegisters().FirstOrDefault(x => x.Aliases != null && x.Aliases.Contains(register));
+            // `reg` is a struct, so it'll never be `null`
+            if(reg.Aliases == null)
+            {
+                throw new RecoverableException($"Wrong register name: {register}");
+            }
+            return cpu.GetRegister(reg.Index);
         }
 
         public static string[,] GetRegistersValues(this ICPUWithRegisters cpu)
         {
             var result = new List<Tuple<string, int, ulong>>();
 
-            foreach(var reg in cpu.GetRegisters().Where(r => r.Aliases != null))
+            foreach(var reg in cpu.GetRegisters()/*.Where(r => r.Aliases != null)*/)
             {
                 result.Add(Tuple.Create(reg.ToString(), reg.Index, cpu.GetRegister(reg.Index).RawValue));
             }
