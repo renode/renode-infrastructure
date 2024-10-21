ca2586872238883e732a3239685b44e4ca1b0cb5
Mateusz Holenko <mholenko@antmicro.com>
2024-10-21 17:03:14 +0200

    [#67422] FilePath: Fix formatting

--- src/Emulator/Main/Utilities/FilePath.cs
+++ src/Emulator/Main/Utilities/FilePath.cs
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2010-2022 Antmicro
+// Copyright (c) 2010-2024 Antmicro
 //
 // This file is licensed under the MIT License.
 // Full license text is available in 'licenses/MIT.txt'.
@@ -47,7 +47,7 @@ namespace Antmicro.Renode.Utilities
                     }
                 }
             }
-            catch (UnauthorizedAccessException e)
+            catch(UnauthorizedAccessException e)
             {
                 throw new RecoverableException($"Error while accessing {path}: {e.Message}");
             }
