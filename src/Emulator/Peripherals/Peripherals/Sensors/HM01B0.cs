7d1a26cd9eb6718e81570a8c27b930c3ebe97b58
Mateusz Holenko <mholenko@antmicro.com>
2024-10-23 10:48:24 +0200

    [#67442] HM01B0: Fix formatting

--- src/Emulator/Peripherals/Peripherals/Sensors/HM01B0.cs
+++ src/Emulator/Peripherals/Peripherals/Sensors/HM01B0.cs
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2010-2021 Antmicro
+// Copyright (c) 2010-2024 Antmicro
 //
 // This file is licensed under the MIT License.
 // Full license text is available in 'licenses/MIT.txt'.
@@ -67,7 +67,7 @@ namespace Antmicro.Renode.Peripherals.Sensors
                 frames.Add(imageData);
             }
         }
-        
+
         public void DropFrames()
         {
             currentFrameIndex = 0;
