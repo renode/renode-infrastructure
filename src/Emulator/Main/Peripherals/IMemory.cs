a033ad25cbe033d4e3878a8da25c9b8bd3df31a3
Adam Jeliński <ajelinski@antmicro.com>
2024-11-05 16:21:57 +0100

    [#67444] IMemory: Add common memory interfaces

--- src/Emulator/Main/Peripherals/IMemory.cs
+++ src/Emulator/Main/Peripherals/IMemory.cs
@@ -1,15 +1,15 @@
 //
-// Copyright (c) 2010-2018 Antmicro
+// Copyright (c) 2010-2024 Antmicro
 // Copyright (c) 2011-2015 Realtime Embedded
 //
 // This file is licensed under the MIT License.
 // Full license text is available in 'licenses/MIT.txt'.
 //
-using System;
+using Antmicro.Renode.Peripherals.Bus;
 
 namespace Antmicro.Renode.Peripherals
 {
-    public interface IMemory
+    public interface IMemory : IPeripheral, IKnownSize, IBytePeripheral, IWordPeripheral, IDoubleWordPeripheral, IMultibyteWritePeripheral, IQuadWordPeripheral
     {
     }
 }
