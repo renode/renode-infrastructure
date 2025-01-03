1faaeb2c5aea1fc0bec957d4b39d6b882cd36200
Michał Konstańczak-Szprejda <mszprejda@antmicro.com>
2025-01-03 12:43:23 +0100

    [#69335] Peripherals: Add IContextState interface

---
+++ src/Emulator/Main/Peripherals/IContextState.cs
@@ -0,0 +1,13 @@
+//
+// Copyright (c) 2010-2025 Antmicro
+//
+// This file is licensed under the MIT License.
+// Full license text is available in 'licenses/MIT.txt'.
+//
+
+namespace Antmicro.Renode.Peripherals
+{
+    public interface IContextState
+    {
+    }
+}
