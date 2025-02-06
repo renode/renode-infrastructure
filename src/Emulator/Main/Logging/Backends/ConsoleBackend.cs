788b7b56263eed978e45bc8f0d7bfd896ea3bf05
Wojciech Meller <wmeller@antmicro.com>
2025-02-06 01:44:31 +0100

    [#71557] ConsoleBackend: Refactor to inherit from FormattedTextBackend

--- src/Emulator/Main/Logging/Backends/ConsoleBackend.cs
+++ src/Emulator/Main/Logging/Backends/ConsoleBackend.cs
@@ -7,13 +7,10 @@
 //
 using System;
 using System.IO;
-using System.Linq;
-using Antmicro.Renode.Utilities;
-using Antmicro.Renode.Debugging;
 
 namespace Antmicro.Renode.Logging
 {
-    public class ConsoleBackend : TextBackend
+    public class ConsoleBackend : FormattedTextBackend
     {
         static ConsoleBackend()
         {
@@ -25,41 +22,6 @@ namespace Antmicro.Renode.Logging
         public override void Dispose()
         {
 
-        }
-
-        public override void Log(LogEntry entry)
-        {
-            if(!ShouldBeLogged(entry))
-            {
-                return;
-            }
-
-            var type = entry.Type;
-            var changeColor = !PlainMode && output == Console.Out && type.Color.HasValue && !isRedirected;
-            var message = FormatLogEntry(entry);
-
-            lock(syncObject)
-            {
-                if(changeColor)
-                {
-                    Console.ForegroundColor = type.Color.Value;
-                }
-                string line;
-                if(LogThreadId && entry.ThreadId != null)
-                {
-                    line = string.Format("{0:HH:mm:ss.ffff} [{1}] ({3}) {2}", CustomDateTime.Now, type, message, entry.ThreadId);
-                }
-                else
-                {
-                    line = string.Format("{0:HH:mm:ss.ffff} [{1}] {2}", CustomDateTime.Now, type, message);
-                }
-
-                WriteNewLine(line);
-                if(changeColor)
-                {
-                    Console.ResetColor();
-                }
-            }
         }
 
         public override void Flush()
@@ -95,23 +57,34 @@ namespace Antmicro.Renode.Logging
         // do not generate color output
         // do not set window title
         // minimize the use of VT100 codes
-        public bool PlainMode { get; set; }
-
-        public bool LogThreadId { get; set; }
-
-        private ConsoleBackend()
+        public override bool PlainMode
         {
-            syncObject = new object();
-            isRedirected = Console.IsOutputRedirected;
+            get => plainMode || output != Console.Out || isRedirected;
+            set => plainMode = value;
         }
 
-        private void WriteNewLine(string line)
+        protected override void SetColor(ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+        }
+
+        protected override void ResetColor()
+        {
+            Console.ResetColor();
+        }
+
+        protected override void WriteLine(string line)
         {
             output.WriteLine(line);
         }
 
+        private ConsoleBackend()
+        {
+            isRedirected = Console.IsOutputRedirected;
+        }
+
         private TextWriter output = Console.Out;
-        private object syncObject;
+        private bool plainMode;
         private readonly bool isRedirected;
     }
 }
