5f3e58c9518c91fab3f140502ea8feb556d8c0c7
Dawid Pietrykowski <dpietrykowski@antmicro.com>
2025-02-20 09:38:44 +0100

    [#73334] Use MiV CLINT prescaler as a timer divisor

--- src/Emulator/Cores/RiscV/MiV_CoreLevelInterruptor.cs
+++ src/Emulator/Cores/RiscV/MiV_CoreLevelInterruptor.cs
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2010-2022 Antmicro
+// Copyright (c) 2010-2025 Antmicro
 //
 // This file is licensed under the MIT License.
 // Full license text is available in 'licenses/MIT.txt'.
@@ -11,8 +11,8 @@ namespace Antmicro.Renode.Peripherals.IRQControllers
 {
     public class MiV_CoreLevelInterruptor : CoreLevelInterruptor, IKnownSize
     {
-        public MiV_CoreLevelInterruptor(IMachine machine, long frequency, uint prescaler, int numberOfTargets = 1)
-            : base(machine, frequency, numberOfTargets)
+        public MiV_CoreLevelInterruptor(IMachine machine, long frequency, uint prescaler = 1, int numberOfTargets = 1)
+            : base(machine, frequency, numberOfTargets, prescaler)
         {
             // we are extending the existing register map defined by the base class
             AddRegister((long)Registers.Prescaler,
