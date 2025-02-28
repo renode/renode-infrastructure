1e91b950f1bbe3eb48112ba260033402af0d72c7
Piotr Wojnarowski <pwojnarowski@antmicro.com>
2025-02-28 17:13:55 +0100

    [#73600] arm64: Only enable GIC CPU i/f for GICv3+

--- src/Emulator/Cores/Arm64/ARMv8A.cs
+++ src/Emulator/Cores/Arm64/ARMv8A.cs
@@ -36,6 +36,7 @@ namespace Antmicro.Renode.Peripherals.CPU
             {
                 throw new ConstructionException($"Failed to attach CPU to Generic Interrupt Controller: {e.Message}", e);
             }
+            TlibSetGicCpuRegisterInterfaceVersion(gic.ArchitectureVersionAtLeast3 ? GICCPUInterfaceVersion.Version30Or40 : GICCPUInterfaceVersion.None);
             Reset();
             HasSingleSecurityState = TlibHasEl3() == 0;
         }
@@ -472,6 +473,9 @@ namespace Antmicro.Renode.Peripherals.CPU
         }
 
 #pragma warning disable 649
+        [Import]
+        private Action<GICCPUInterfaceVersion> TlibSetGicCpuRegisterInterfaceVersion;
+
         [Import]
         private Func<string, uint, uint> TlibCheckSystemRegisterAccess;
 
