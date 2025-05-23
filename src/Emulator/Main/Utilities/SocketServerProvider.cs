933d564039851917db300ec11019d9332fe45331
Piotr Zierhoffer <pzierhoffer@antmicro.com>
2025-05-23 17:00:43 +0200

    [#77995] telnet: Fix handling of IAC escape sequences

--- src/Emulator/Main/Utilities/SocketServerProvider.cs
+++ src/Emulator/Main/Utilities/SocketServerProvider.cs
@@ -103,6 +103,7 @@ namespace Antmicro.Renode.Utilities
         {
             try
             {
+                var iacEscapePosition = -1;
                 // This thread will poll for bytes constantly for `MaxReadThreadPoolingTimeMs` to assert we have the lowest possible latency while transmiting packet.
                 var watch = new Stopwatch();
                 while(!cancellationToken.IsCancellationRequested)
@@ -112,7 +113,14 @@ namespace Antmicro.Renode.Utilities
                     {
                         while(queue.TryDequeue(out var dequeued))
                         {
-                            stream.Write(dequeued, 0, dequeued.Length);
+                            if(!emitConfigBytes && (iacEscapePosition = Array.FindIndex(dequeued, x => x == IACEscape)) == -1)
+                            {
+                                stream.Write(dequeued, 0, dequeued.Length);
+                            }
+                            else
+                            {
+                                stream.Write(dequeued.Take(iacEscapePosition):, 0, dequeued.Length);
+                            }
                         }
                     }
                     watch.Reset();
@@ -152,12 +160,54 @@ namespace Antmicro.Renode.Utilities
                         break;
                     }
 
-                    DataBlockReceived?.Invoke(buffer.Take(count).ToArray());
+                    if(telnetMode && buffer[0] == IACEscape)
+                    {
+                        if(iacEscapeSpotted)
+                        {
+                            // Previous Read ended in IAC
+                            // IAC followed by IAC effectively sends 255 as input
+                            bytesToIgnore = 0;
+                            iacEscapeSpotted = false;
+                        }
+                        else
+                        {
+                            // Ignore IAC commands, 3 bytes each (unless followed by another IAC)
+                            // TODO: look for iac in the middle of the sequence?
+                            // In practice it doesn't seem to happen at all
+                            // We explicitly do NOT handle subcommand negotiation, a possible todo for the future
+                            bytesToIgnore = IACCommandBytes;
+
+                            if(count == 1)
+                            {
+                                // We see the IAC code and must ensure the next Read does not yield another one
+                                iacEscapeSpotted = true;
+                            }
+                            else if(buffer[1] == IACEscape)
+                            {
+                                // We receive a longer data batch starting with two IAC bytes: just skip one byte, leave the other one as is
+                                bytesToIgnore = 1;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        // Clear IAC escape mode - we encountered a different character in the package
+                        iacEscapeSpotted = false;
+                    }
+                    var skip = 0;
+                    if(bytesToIgnore > 0)
+                    {
+                        skip = bytesToIgnore > count ? count : bytesToIgnore;
+                        bytesToIgnore -= skip;
+                        count -= skip;
+                    }
+
+                    DataBlockReceived?.Invoke(buffer.Skip(skip).Take(count).ToArray());
 
                     var dataReceived = DataReceived;
                     if(dataReceived != null)
                     {
-                        foreach(var b in buffer.Take(count))
+                        foreach(var b in buffer.Skip(skip).Take(count))
                         {
                             dataReceived((int)b);
                         }
@@ -203,12 +253,6 @@ namespace Antmicro.Renode.Utilities
                             255, 252,  34, // IAC WONT  LINEMODE
                         };
                         stream.Write(initBytes, 0, initBytes.Length);
-                        // we expect 9 bytes as a result of sending
-                        // config bytes
-                        for (int i = 0; i < 9; i++)
-                        {
-                            stream.ReadByte();
-                        }
                     }
                 }
                 catch(OperationCanceledException)
@@ -277,7 +321,11 @@ namespace Antmicro.Renode.Utilities
         private Thread writerThread;
         private Socket server;
         private Socket socket;
+        private int bytesToIgnore;
+        private bool iacEscapeSpotted;
 
+        private const int IACEscape = 255;
+        private const int IACCommandBytes = 3;
         private const int MaxReadThreadPoolingTimeMs = 60;
     }
 }
