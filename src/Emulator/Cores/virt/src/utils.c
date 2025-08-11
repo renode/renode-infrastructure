6b257de61429b6836193aa5c8dc42f6974cc6feb
Bartlomiej Stefanski <bstefanski@internships.antmicro.com>
2025-08-11 08:54:05 +0200

    [FIX] virt: Do not use magic values for buffer length

--- src/Emulator/Cores/virt/src/utils.c
+++ src/Emulator/Cores/virt/src/utils.c
@@ -15,13 +15,15 @@
 #include <sys/ioctl.h>
 #include <errno.h>
 
+#define VSNPRINTF_BUFFER_SIZE 1024
+
 
 void kvm_abortf(const char *fmt, ...)
 {
-    char result[1024];
+    char result[VSNPRINTF_BUFFER_SIZE];
     va_list ap;
     va_start(ap, fmt);
-    vsnprintf(result, 1024, fmt, ap);
+    vsnprintf(result, VSNPRINTF_BUFFER_SIZE, fmt, ap);
     kvm_abort(result);
     va_end(ap);
 }
@@ -32,20 +34,20 @@ void kvm_runtime_abortf(const char *fmt, ...)
     get_regs(&regs);
     uint64_t pc = regs.rip;
 
-    char result[1024];
+    char result[VSNPRINTF_BUFFER_SIZE];
     va_list ap;
     va_start(ap, fmt);
-    vsnprintf(result, 1024, fmt, ap);
+    vsnprintf(result, VSNPRINTF_BUFFER_SIZE, fmt, ap);
     kvm_runtime_abort(result, pc);
     va_end(ap);
 }
 
 void kvm_logf(LogLevel level, const char *fmt, ...)
 {
-    char result[1024];
+    char result[VSNPRINTF_BUFFER_SIZE];
     va_list ap;
     va_start(ap, fmt);
-    vsnprintf(result, 1024, fmt, ap);
+    vsnprintf(result, VSNPRINTF_BUFFER_SIZE, fmt, ap);
     kvm_log(level, result);
     va_end(ap);
 }
