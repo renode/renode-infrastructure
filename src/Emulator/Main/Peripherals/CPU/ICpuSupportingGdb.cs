220d2ebd7ce557500cd71566407deae0cf58c83b
Pawel Reich <preich@internships.antmicro.com>
2025-09-09 12:53:11 +0200

    [#61290] ICpuSupportingGdb: Add ShouldEnterDebugMode property

--- src/Emulator/Main/Peripherals/CPU/ICpuSupportingGdb.cs
+++ src/Emulator/Main/Peripherals/CPU/ICpuSupportingGdb.cs
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2010-2024 Antmicro
+// Copyright (c) 2010-2025 Antmicro
 //
 // This file is licensed under the MIT License.
 // Full license text is available in 'licenses/MIT.txt'.
@@ -17,5 +17,7 @@ namespace Antmicro.Renode.Peripherals.CPU
         List<GDBFeatureDescriptor> GDBFeatures { get; }
 
         bool DebuggerConnected { get; set; }
+
+        bool ShouldEnterDebugMode { get; set; }
     }
 }
