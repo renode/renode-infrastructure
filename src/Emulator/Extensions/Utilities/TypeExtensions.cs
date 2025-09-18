df25fb765e8e0e72ff0f2a8ea2dc35a3c4ebfa25
Piotr Wojnarowski <pwojnarowski@antmicro.com>
2025-09-18 14:21:29 +0200

    [#83539] TypeExtensions: Handle T: IEnumerable<T>

--- src/Emulator/Extensions/Utilities/TypeExtensions.cs
+++ src/Emulator/Extensions/Utilities/TypeExtensions.cs
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2010-2024 Antmicro
+// Copyright (c) 2010-2025 Antmicro
 // Copyright (c) 2011-2015 Realtime Embedded
 //
 // This file is licensed under the MIT License.
@@ -80,7 +80,7 @@ namespace Antmicro.Renode.Utilities
         private static bool InnerIsTypeConvertible(Type type)
         {
             var underlyingType = GetEnumerableType(type);
-            if(underlyingType != null)
+            if(underlyingType != null && underlyingType != type)
             {
                 return IsTypeConvertible(underlyingType);
             }
