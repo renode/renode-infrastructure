afdd7780a226a09f740e70c8f727637c4f3f8d77
Piotr Wojnarowski <pwojnarowski@antmicro.com>
2025-09-30 14:02:23 +0200

    [#81726] Packet: Allow specifying width in wider units

--- src/Emulator/Main/Utilities/Packets/WidthAttribute.cs
+++ src/Emulator/Main/Utilities/Packets/WidthAttribute.cs
@@ -5,15 +5,23 @@
 // Full license text is available in 'licenses/MIT.txt'.
 //
 using System;
+using System.Linq;
 
 namespace Antmicro.Renode.Utilities.Packets
 {
     [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Struct | AttributeTargets.Class, Inherited = true, AllowMultiple = false)]
     public sealed class WidthAttribute : Attribute
     {
-        public WidthAttribute(uint value)
+        public WidthAttribute(uint bits = 0, uint bytes = 0, uint words = 0, uint doubleWords = 0, uint quadWords = 0)
         {
-            Value = value;
+            Value = new[]
+            {
+                (quadWords << 6),
+                (doubleWords << 5),
+                (words << 4),
+                (bytes << 3),
+                bits,
+            }.Aggregate((a, b) => a + b);
         }
 
         public uint Value { get; }
