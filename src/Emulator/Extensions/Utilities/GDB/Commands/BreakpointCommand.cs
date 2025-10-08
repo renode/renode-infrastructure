19765888d8ca0e64e23d9fa63977bda07e892e28
Pawel Reich <preich@internships.antmicro.com>
2025-10-08 12:09:05 +0200

    [#61284] WatchpointDescriptor: Move to separate file

--- src/Emulator/Extensions/Utilities/GDB/Commands/BreakpointCommand.cs
+++ src/Emulator/Extensions/Utilities/GDB/Commands/BreakpointCommand.cs
@@ -189,43 +189,5 @@ namespace Antmicro.Renode.Utilities.GDB.Commands
         }
 
         private readonly Dictionary<WatchpointDescriptor, int> watchpoints;
-
-        private class WatchpointDescriptor
-        {
-            public WatchpointDescriptor(ulong address, SysbusAccessWidth width, Access access, BusHookDelegate hook)
-            {
-                Address = address;
-                Width = width;
-                Access = access;
-                Hook = hook;
-            }
-
-            public override bool Equals(object obj)
-            {
-                var objAsBreakpointDescriptor = obj as WatchpointDescriptor;
-                if(objAsBreakpointDescriptor == null)
-                {
-                    return false;
-                }
-
-                return objAsBreakpointDescriptor.Address == Address
-                        && objAsBreakpointDescriptor.Width == Width
-                        && objAsBreakpointDescriptor.Access == Access
-                        && objAsBreakpointDescriptor.Hook == Hook;
-            }
-
-            public override int GetHashCode()
-            {
-                return 17 * (int)Address
-                    + 23 * (int)Width
-                    + 17 * (int)Access
-                    + 17 * Hook.GetHashCode();
-            }
-
-            public readonly ulong Address;
-            public readonly SysbusAccessWidth Width;
-            public readonly Access Access;
-            public readonly BusHookDelegate Hook;
-        }
     }
 }
