806f446c7dadd00c6aac163a1e5f0fe42a64bd7a
Bartlomiej Stefanski <bstefanski@internships.antmicro.com>
2025-11-28 14:54:58 +0100

    [#87726] OMAP_GPTimer: Initialize state in base class constructor

--- src/Emulator/Peripherals/Peripherals/Timers/OMAP_GPTimer.cs
+++ src/Emulator/Peripherals/Peripherals/Timers/OMAP_GPTimer.cs
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2010-2018 Antmicro
+// Copyright (c) 2010-2026 Antmicro
 // Copyright (c) 2011-2015 Realtime Embedded
 //
 // This file is licensed under the MIT License.
@@ -17,9 +17,8 @@ namespace Antmicro.Renode.Peripherals.Timers
 {
     public class OMAP_GPTimer : LimitTimer, IDoubleWordPeripheral
     {
-        public OMAP_GPTimer(IMachine machine) : base(machine.ClockSource, (38400000), direction: Direction.Ascending, limit: (0xFFFFFFFF), enabled: true)
+        public OMAP_GPTimer(IMachine machine) : base(machine.ClockSource, (38400000), direction: Direction.Ascending, limit: (0xFFFFFFFF), enabled: true, autoUpdate: true)
         { // TODO: hack - 10 times slower, because of Stopwatch limitation
-            AutoUpdate = true;
             IRQ = new GPIO();
         }
 
