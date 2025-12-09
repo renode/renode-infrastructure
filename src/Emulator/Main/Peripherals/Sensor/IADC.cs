9fb53b0f5e532776c2c59c44f6bb6df027ddaaae
Wojciech Meller <wmeller@antmicro.com>
2025-12-09 16:19:27 +0100

    [#88008] IADC: Add extensions

--- src/Emulator/Main/Peripherals/Sensor/IADC.cs
+++ src/Emulator/Main/Peripherals/Sensor/IADC.cs
@@ -4,9 +4,33 @@
 // This file is licensed under the MIT License.
 // Full license text is available in 'licenses/MIT.txt'.
 //
+using Antmicro.Renode.Exceptions;
 
 namespace Antmicro.Renode.Peripherals.Sensor
 {
+    public static class IADCExtensions
+    {
+        public static void SetVoltage(this IADC @this, decimal value, int channel)
+        {
+            @this.AssertChannel(channel);
+            @this.SetADCValue(channel, (uint)(value * 1e6m));
+        }
+
+        public static decimal GetVoltage(this IADC @this, int channel)
+        {
+            @this.AssertChannel(channel);
+            return (decimal)@this.GetADCValue(channel) / 1e6m;
+        }
+
+        public static void AssertChannel(this IADC @this, int channel)
+        {
+            if(channel < 0 || channel >= @this.ADCChannelCount)
+            {
+                throw new RecoverableException($"'{nameof(channel)}' is not in [0, {@this.ADCChannelCount - 1}] range");
+            }
+        }
+    }
+
     public interface IADC : ISensor
     {
         // ADC value should be treated as RESD VoltageSample value
