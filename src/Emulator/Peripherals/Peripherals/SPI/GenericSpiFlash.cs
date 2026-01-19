7256890a235e0cb26c80ad7e6b564cffceb5a2b9
Dawid Olszewski <dolszewski@antmicro.com>
2026-01-19 01:57:32 +0100

    [#89640] ISFDPPeripheral: Add interface for devices supporting SFDP

--- src/Emulator/Peripherals/Peripherals/SPI/GenericSpiFlash.cs
+++ src/Emulator/Peripherals/Peripherals/SPI/GenericSpiFlash.cs
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2010-2025 Antmicro
+// Copyright (c) 2010-2026 Antmicro
 //
 // This file is licensed under the MIT License.
 // Full license text is available in 'licenses/MIT.txt'.
@@ -18,7 +18,7 @@ using Range = Antmicro.Renode.Core.Range;
 
 namespace Antmicro.Renode.Peripherals.SPI
 {
-    public partial class GenericSpiFlash : ISPIPeripheral, IGPIOReceiver
+    public partial class GenericSpiFlash : ISPIPeripheral, IGPIOReceiver, ISFDPPeripheral
     {
         public GenericSpiFlash(MappedMemory underlyingMemory, byte manufacturerId, byte memoryType, byte? capacityCode = null,
             bool writeStatusCanSetWriteEnable = true, byte extendedDeviceId = DefaultExtendedDeviceID,
