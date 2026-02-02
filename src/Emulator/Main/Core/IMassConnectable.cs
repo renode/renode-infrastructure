1600a80a439dc86bcde4365b33b6a7703a6fe991
Michał Konstańczak-Szprejda <mszprejda@antmicro.com>
2026-02-02 12:48:15 +0100

    [#90880] Core: Introduce IMassConnectable interface

---
+++ src/Emulator/Main/Core/IMassConnectable.cs
@@ -0,0 +1,14 @@
+//
+// Copyright (c) 2010-2026 Antmicro
+//
+// This file is licensed under the MIT License.
+// Full license text is available in 'licenses/MIT.txt'.
+//
+namespace Antmicro.Renode.Core
+{
+    public interface IMassConnectable<T> : IConnectable<T>
+    {
+        void DetachAll();
+    }
+}
+
