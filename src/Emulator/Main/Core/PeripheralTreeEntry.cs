f50841ce29a5dfccb8cbff2054a26a50a2ed972b
Michał Konstańczak-Szprejda <mszprejda@antmicro.com>
2026-03-12 16:29:26 +0100

    [#90902] PeripheralTreeEntry: Add ShouldBePrinted property

--- src/Emulator/Main/Core/PeripheralTreeEntry.cs
+++ src/Emulator/Main/Core/PeripheralTreeEntry.cs
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2010-2018 Antmicro
+// Copyright (c) 2010-2026 Antmicro
 // Copyright (c) 2011-2015 Realtime Embedded
 //
 // This file is licensed under the MIT License.
@@ -56,6 +56,8 @@ namespace Antmicro.Renode.Core
         public int Level { get; private set; }
 
         public IRegistrationPoint RegistrationPoint { get; private set; }
+
+        public bool ShouldBePrinted { get; set; }
 
         [PreSerialization]
         private void SaveType()
