864d05aae17729d36843634366a9a2afdde79302
Bartlomiej Stefanski <bstefanski@internships.antmicro.com>
2026-03-13 10:02:54 +0100

    [#93213] X86_64: Fully define CPU registers and flags for GDB

--- src/Emulator/Cores/X86/X86_64.cs
+++ src/Emulator/Cores/X86/X86_64.cs
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2010-2025 Antmicro
+// Copyright (c) 2010-2026 Antmicro
 //
 // This file is licensed under the MIT License.
 // Full license text is available in 'licenses/MIT.txt'.
@@ -33,43 +33,54 @@ namespace Antmicro.Renode.Peripherals.CPU
                 var features = new List<GDBFeatureDescriptor>();
 
                 var coreFeature = new GDBFeatureDescriptor("org.gnu.gdb.i386.core");
-                coreFeature.Registers.Add(new GDBRegisterDescriptor(0, 64, "rax", "int64"));
-                coreFeature.Registers.Add(new GDBRegisterDescriptor(1, 64, "rcx", "int64"));
-                coreFeature.Registers.Add(new GDBRegisterDescriptor(2, 64, "rdx", "int64"));
-                coreFeature.Registers.Add(new GDBRegisterDescriptor(3, 64, "rbx", "int64"));
-                coreFeature.Registers.Add(new GDBRegisterDescriptor(4, 64, "rsp", "data_ptr"));
-                coreFeature.Registers.Add(new GDBRegisterDescriptor(5, 64, "rbp", "data_ptr"));
-                coreFeature.Registers.Add(new GDBRegisterDescriptor(6, 64, "rsi", "int64"));
-                coreFeature.Registers.Add(new GDBRegisterDescriptor(7, 64, "rdi", "int64"));
+
+                coreFeature.Registers.Add(new GDBRegisterDescriptor((uint)X86_64Registers.RAX, 64, "rax", "uint64"));
+                coreFeature.Registers.Add(new GDBRegisterDescriptor((uint)X86_64Registers.RCX, 64, "rcx", "uint64"));
+                coreFeature.Registers.Add(new GDBRegisterDescriptor((uint)X86_64Registers.RDX, 64, "rdx", "uint64"));
+                coreFeature.Registers.Add(new GDBRegisterDescriptor((uint)X86_64Registers.RBX, 64, "rbx", "uint64"));
+                coreFeature.Registers.Add(new GDBRegisterDescriptor((uint)X86_64Registers.RSP, 64, "rsp", "data_ptr"));
+                coreFeature.Registers.Add(new GDBRegisterDescriptor((uint)X86_64Registers.RBP, 64, "rbp", "data_ptr"));
+                coreFeature.Registers.Add(new GDBRegisterDescriptor((uint)X86_64Registers.RSI, 64, "rsi", "uint64"));
+                coreFeature.Registers.Add(new GDBRegisterDescriptor((uint)X86_64Registers.RDI, 64, "rdi", "uint64"));
+
                 for(var index = 8u; index <= 15; index++)
                 {
-                    coreFeature.Registers.Add(new GDBRegisterDescriptor(index, 64, $"r{index}", "int64"));
+                    coreFeature.Registers.Add(new GDBRegisterDescriptor((uint)X86_64Registers.R8 + index - 8, 64, $"r{index}", "int64"));
                 }
 
-                coreFeature.Registers.Add(new GDBRegisterDescriptor(16, 64, "rip", "code_ptr"));
+                coreFeature.Registers.Add(new GDBRegisterDescriptor((uint)X86_64Registers.RIP, 64, "rip", "code_ptr"));
 
-                coreFeature.Registers.Add(new GDBRegisterDescriptor(17, 32, "eflags", "int32"));
+                coreFeature.Registers.Add(new GDBRegisterDescriptor((uint)X86_64Registers.EFLAGS, 64, "eflags", "x86_64_eflags"));
 
-                coreFeature.Registers.Add(new GDBRegisterDescriptor(18, 32, "cs", "int32"));
-                coreFeature.Registers.Add(new GDBRegisterDescriptor(19, 32, "ss", "int32"));
-                coreFeature.Registers.Add(new GDBRegisterDescriptor(20, 32, "ds", "int32"));
-                coreFeature.Registers.Add(new GDBRegisterDescriptor(21, 32, "es", "int32"));
-                coreFeature.Registers.Add(new GDBRegisterDescriptor(22, 32, "fs", "int32"));
-                coreFeature.Registers.Add(new GDBRegisterDescriptor(23, 32, "gs", "int32"));
+                coreFeature.Registers.Add(new GDBRegisterDescriptor((uint)X86_64Registers.CS, 64, "cs", "uint64"));
+                coreFeature.Registers.Add(new GDBRegisterDescriptor((uint)X86_64Registers.SS, 64, "ss", "uint64"));
+                coreFeature.Registers.Add(new GDBRegisterDescriptor((uint)X86_64Registers.DS, 64, "ds", "uint64"));
+                coreFeature.Registers.Add(new GDBRegisterDescriptor((uint)X86_64Registers.ES, 64, "es", "uint64"));
+                coreFeature.Registers.Add(new GDBRegisterDescriptor((uint)X86_64Registers.FS, 64, "fs", "uint64"));
+                coreFeature.Registers.Add(new GDBRegisterDescriptor((uint)X86_64Registers.GS, 64, "gs", "uint64"));
 
                 for(var index = 0u; index <= 7; index++)
                 {
-                    coreFeature.Registers.Add(new GDBRegisterDescriptor(24 + index, 80, $"st{index}", "i387_ext"));
+                    coreFeature.Registers.Add(new GDBRegisterDescriptor((uint)X86_64Registers.ST0 + index, 80, $"st{index}", "i387_ext"));
                 }
 
-                coreFeature.Registers.Add(new GDBRegisterDescriptor(32, 32, "fctrl", "int", "float"));
-                coreFeature.Registers.Add(new GDBRegisterDescriptor(33, 32, "fstat", "int", "float"));
-                coreFeature.Registers.Add(new GDBRegisterDescriptor(34, 32, "ftag", "int", "float"));
-                coreFeature.Registers.Add(new GDBRegisterDescriptor(35, 32, "fiseg", "int", "float"));
-                coreFeature.Registers.Add(new GDBRegisterDescriptor(36, 32, "fioff", "int", "float"));
-                coreFeature.Registers.Add(new GDBRegisterDescriptor(37, 32, "foseg", "int", "float"));
-                coreFeature.Registers.Add(new GDBRegisterDescriptor(38, 32, "fooff", "int", "float"));
-                coreFeature.Registers.Add(new GDBRegisterDescriptor(39, 32, "fop", "int", "float"));
+                coreFeature.Registers.Add(new GDBRegisterDescriptor((uint)X86_64Registers.CR0, 64, $"cr0", "x86_64_cr0"));
+                coreFeature.Registers.Add(new GDBRegisterDescriptor((uint)X86_64Registers.CR1, 64, $"cr1", "uint64"));
+                coreFeature.Registers.Add(new GDBRegisterDescriptor((uint)X86_64Registers.CR2, 64, $"cr2", "uint64"));
+                coreFeature.Registers.Add(new GDBRegisterDescriptor((uint)X86_64Registers.CR3, 64, $"cr3", "x86_64_cr3"));
+                coreFeature.Registers.Add(new GDBRegisterDescriptor((uint)X86_64Registers.CR4, 64, $"cr4", "x86_64_cr4"));
+
+                var regnum = (uint)X86_64Registers.CR4 + 1;
+                foreach(var float_reg in new[] { "fctrl", "fstat", "ftag", "fiseg", "fioff", "foseg", "fooff", "fop" })
+                {
+                    coreFeature.Registers.Add(new GDBRegisterDescriptor(regnum++, 32, float_reg, "int", "float"));
+                }
+
+                coreFeature.Types.Add(GDBCustomType.Flags("x86_64_eflags", 8, X86_64GDBFields.EflagsFlags));
+                coreFeature.Types.Add(GDBCustomType.Flags("x86_64_cr0", 8, X86_64GDBFields.Cr0Flags));
+                coreFeature.Types.Add(GDBCustomType.Flags("x86_64_cr3", 8, X86_64GDBFields.Cr3Flags));
+                coreFeature.Types.Add(GDBCustomType.Flags("x86_64_cr4", 8, X86_64GDBFields.Cr4Flags));
+                coreFeature.Types.Add(GDBCustomType.Flags("x86_64_efer", 8, X86_64GDBFields.EferFlags));
 
                 features.Add(coreFeature);
 
