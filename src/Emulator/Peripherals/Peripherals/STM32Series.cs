c3255674d0a42feff22c552c6d1cd8a079094646
Jérémy Lefaure <jlefaure@antmicro.com>
2026-04-14 13:03:24 +0200

    [DEV] STM32Series: Add links to technical documentation pages

--- src/Emulator/Peripherals/Peripherals/STM32Series.cs
+++ src/Emulator/Peripherals/Peripherals/STM32Series.cs
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2010-2024 Antmicro
+// Copyright (c) 2010-2026 Antmicro
 //
 // This file is licensed under the MIT License.
 // Full license text is available in 'licenses/MIT.txt'.
@@ -9,8 +9,19 @@ namespace Antmicro.Renode.Peripherals
 {
     public enum STM32Series
     {
+        /* For F0 series technical documentation and reference manuals, see
+         * https://www.st.com/en/microcontrollers-microprocessors/stm32f0-series/documentation.html
+         */
         F0,
+
+        /* For F4 series technical documentation and reference manuals, see
+         * https://www.st.com/en/microcontrollers-microprocessors/stm32f4-series/documentation.html
+         * */
         F4,
+
+        /* For WBA series technical documentation and reference manuals, see
+         * https://www.st.com/en/microcontrollers-microprocessors/stm32wba-series/documentation.html
+         */
         WBA,
     }
 }
