2a7aca4227b280942e72f1484bed95facbad5d8f
Marcin Witkowski <mwitkowski@antmicro.com>
2026-05-06 11:53:14 +0200

    [#96256] STM32G0DMA: Fix Reset method

--- src/Emulator/Peripherals/Peripherals/DMA/STM32G0DMA.cs
+++ src/Emulator/Peripherals/Peripherals/DMA/STM32G0DMA.cs
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2010-2025 Antmicro
+// Copyright (c) 2010-2026 Antmicro
 //
 // This file is licensed under the MIT License.
 // Full license text is available in 'licenses/MIT.txt'.
@@ -110,6 +110,11 @@ namespace Antmicro.Renode.Peripherals.DMA
         public void Reset()
         {
             registers.Reset();
+            foreach(var channel in channels)
+            {
+                channel.Reset();
+            }
+            Update();
         }
 
         public uint ReadDoubleWord(long offset)
