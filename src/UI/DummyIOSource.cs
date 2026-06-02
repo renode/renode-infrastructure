35040054fac40b3028cbb01d6a7856a65145f112
Piotr Wojnarowski <pwojnarowski@antmicro.com>
2026-06-02 12:07:44 +0200

    [#96128] DummyIOSource: Don't behave as an infinite stream of null bytes

--- src/UI/DummyIOSource.cs
+++ src/UI/DummyIOSource.cs
@@ -1,10 +1,12 @@
 //
-// Copyright (c) 2010-2021 Antmicro
+// Copyright (c) 2010-2026 Antmicro
 // Copyright (c) 2011-2015 Realtime Embedded
 //
 // This file is licensed under the MIT License.
 // Full license text is available in 'licenses/MIT.txt'.
 //
+using System.Threading;
+
 using AntShell.Terminal;
 
 namespace Antmicro.Renode.UI
@@ -13,10 +15,12 @@ namespace Antmicro.Renode.UI
     {
         public void CancelRead()
         {
+            readCancelled.Set();
         }
 
         public void Dispose()
         {
+            CancelRead();
         }
 
         public void Flush()
@@ -25,17 +29,20 @@ namespace Antmicro.Renode.UI
 
         public bool TryPeek(out int value)
         {
-            value = 0;
-            return true;
+            value = -1;
+            return false;
         }
 
         public int Read()
         {
-            return 0;
+            readCancelled.WaitOne();
+            return -1;
         }
 
         public void Write(byte b)
         {
         }
+
+        private readonly ManualResetEvent readCancelled = new ManualResetEvent(false);
     }
 }
