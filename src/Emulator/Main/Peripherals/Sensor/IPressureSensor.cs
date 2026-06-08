fc64f7b15a827288fc943b63a67b541c211cfa71
Pawel Reich <preich@internships.antmicro.com>
2026-06-08 13:47:30 +0200

    [#97563] Add `IPressureSensor`

---
+++ src/Emulator/Main/Peripherals/Sensor/IPressureSensor.cs
@@ -0,0 +1,15 @@
+//
+// Copyright (c) 2010-2026 Antmicro
+//
+// This file is licensed under the MIT License.
+// Full license text is available in 'licenses/MIT.txt'.
+//
+
+namespace Antmicro.Renode.Peripherals.Sensor
+{
+    public interface IPressureSensor : ISensor
+    {
+        // hPa
+        decimal Pressure { get; set; }
+    }
+}
