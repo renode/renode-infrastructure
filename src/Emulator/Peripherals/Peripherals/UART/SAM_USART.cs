6667549371dfdd3f2b3d09e067b71cf9d35a10d5
Wojciech Meller <wmeller@antmicro.com>
2026-06-23 14:18:05 +0200

    [#98006] SAM_USART: Add CharacterReceptionDelay

--- src/Emulator/Peripherals/Peripherals/UART/SAM_USART.cs
+++ src/Emulator/Peripherals/Peripherals/UART/SAM_USART.cs
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2010-2025 Antmicro
+// Copyright (c) 2010-2026 Antmicro
 //
 // This file is licensed under the MIT License.
 // Full license text is available in 'licenses/MIT.txt'.
@@ -122,6 +122,8 @@ namespace Antmicro.Renode.Peripherals.UART
         public uint CharacterLength => 5 + (uint)characterLength.Value;
 
         public TimeInterval CharacterTransmissionDelay { get; set; } = TimeInterval.Empty;
+
+        public TimeInterval CharacterReceptionDelay { get; set; } = TimeInterval.Empty;
 
         public bool AutoUpdateDelay
         {
@@ -679,8 +681,7 @@ namespace Antmicro.Renode.Peripherals.UART
                 return;
             }
 
-            CharacterTransmissionDelay = TimeInterval.FromNanoseconds(
-                (ulong)(this.GetActualTransmissionDuration(CharacterLength).TotalNanoseconds * DelayMultiplier));
+            CharacterTransmissionDelay = this.GetActualTransmissionDuration(CharacterLength) * DelayMultiplier;
         }
 
         private void UpdateBaudrate()
