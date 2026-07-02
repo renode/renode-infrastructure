6c78f964ab87d4ee30db15e80b0a24a2010cc731
Adrian Śliwa <asliwa@internships.antmicro.com>
2026-07-02 15:07:02 +0200

    [#99893] NEORV32_UART: Add CTRL enable/status/IRQ-enable bits support

--- src/Emulator/Peripherals/Peripherals/UART/NEORV32_UART.cs
+++ src/Emulator/Peripherals/Peripherals/UART/NEORV32_UART.cs
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2010-2024 Antmicro
+// Copyright (c) 2010-2026 Antmicro
 //
 // This file is licensed under the MIT License.
 // Full license text is available in 'licenses/MIT.txt'.
@@ -14,6 +14,7 @@ using Antmicro.Renode.Peripherals.Bus;
 
 namespace Antmicro.Renode.Peripherals.UART
 {
+    // This peripheral implements the UART from NEORV32 board v1.11.6.
     public class NEORV32_UART : UARTBase, IDoubleWordPeripheral, IProvidesRegisterCollection<DoubleWordRegisterCollection>, IKnownSize
     {
         public NEORV32_UART(IMachine machine) : base(machine)
@@ -21,30 +22,27 @@ namespace Antmicro.Renode.Peripherals.UART
             var registersMap = new Dictionary<long, DoubleWordRegister>();
 
             registersMap.Add((long)Registers.Control, new DoubleWordRegister(this)
-                .WithTaggedFlag("CTRL_EN", 0) // UART enable
+                .WithFlag(0, out enabled, name: "CTRL_EN") // UART enable
                 .WithTaggedFlag("CTRL_SIM_MODE", 1) // Enable simulation mode
                 .WithTaggedFlag(name: "HWFC_EN", 2) // Enable RTS/CTS hardware flow-control
                 .WithTag("CTRL_PRSC2:CTRL_PRSC0", 3, 3) // Baud rate clock prescaler select
                 .WithTag("CTRL_BAUD9:CTRL_BAUD0", 6, 10) // 12-bit Baud value configuration value
-                .WithFlag(16,
-                    FieldMode.Read,
-                    valueProviderCallback: _ => rxWasNotEmptyOnLastRead,
-                    name: "CTRL_RX_NEMPTY") // RX FIFO not empty
+                .WithFlag(16, FieldMode.Read, valueProviderCallback: _ => RxNotEmpty, name: "CTRL_RX_NEMPTY") // RX FIFO not empty
                 .WithTaggedFlag("CTRL_RX_HALF", 17) // RX FIFO at least half-full
                 .WithTaggedFlag("CTRL_RX_FULL", 18) // RX FIFO full
-                .WithTaggedFlag("CTRL_TX_EMPTY", 19) // TX FIFO empty
-                .WithTaggedFlag("CTRL_TX_NHALF", 20) // TX FIFO not at least half-full
-                .WithTaggedFlag("CTRL_TX_FULL", 21) // TX FIFO full
-                .WithTaggedFlag("CTRL_IRQ_RX_NEMPTY", 22) // fire IRQ if RX FIFO not empty
+                .WithFlag(19, FieldMode.Read, valueProviderCallback: _ => true, name: "CTRL_TX_EMPTY") // TX FIFO empty
+                .WithFlag(20, FieldMode.Read, valueProviderCallback: _ => true, name: "CTRL_TX_NHALF") // TX FIFO not at least half-full
+                .WithFlag(21, FieldMode.Read, valueProviderCallback: _ => false, name: "CTRL_TX_FULL") // TX FIFO full
+                .WithFlag(22, out irqRxNotEmptyEnabled, name: "CTRL_IRQ_RX_NEMPTY") // fire IRQ if RX FIFO not empty
                 .WithTaggedFlag("CTRL_IRQ_RX_HALF", 23) // fire IRQ if RX FIFO at least half-full
                 .WithTaggedFlag("CTRL_IRQ_RX_FULL", 24) // fire IRQ if RX FIFO full
-                .WithTaggedFlag("CTRL_IRQ_TX_EMPTY", 25) // fire IRQ if TX FIFO empty
+                .WithFlag(25, out irqTxEmptyEnabled, name: "CTRL_IRQ_TX_EMPTY") // fire IRQ if TX FIFO empty
                 .WithTaggedFlag("CTRL_IRQ_TX_NHALF", 26) // fire IRQ if TX not at least half full
                 .WithReservedBits(27, 1) // Reserved read as zero
                 .WithTaggedFlag("CTRL_RX_CLR", 28) // Clear RX FIFO, flag auto-clears
                 .WithTaggedFlag("CTRL_TX_CLR", 29) // Clear TX FIFO, flag auto-clears
                 .WithTaggedFlag("CTRL_RX_OVER", 30) // RX FIFO overflow; leared by disabling the module
-                .WithTaggedFlag("CTRL_TX_BUSY", 31) // TX busy or TX FIFO not empty
+                .WithFlag(31, FieldMode.Read, valueProviderCallback: _ => false, name: "CTRL_TX_BUSY") // TX busy or TX FIFO not empty
                 .WithWriteCallback((_, __) => UpdateInterrupts())
             );
 
@@ -58,7 +56,7 @@ namespace Antmicro.Renode.Peripherals.UART
                 // There's PR fixing this: https://github.com/zephyrproject-rtos/zephyr/pull/72385
                 .WithFlag(16,
                     FieldMode.Read,
-                    valueProviderCallback: _ => rxWasNotEmptyOnLastRead,
+                    valueProviderCallback: _ => RxNotEmpty,
                     name: "NEORV32_UART_CTRL_RX_NEMPTY")
                 .WithReservedBits(17, 15)
             );
@@ -123,13 +121,12 @@ namespace Antmicro.Renode.Peripherals.UART
             // intentionally left blank
         }
 
+        protected override bool IsReceiveEnabled => enabled.Value;
+
         private uint HandleReceiveData()
         {
-            rxWasNotEmptyOnLastRead = false;
-
             if(receiveQueue.TryDequeue(out var result))
             {
-                rxWasNotEmptyOnLastRead = true;
                 UpdateInterrupts();
             }
             return result;
@@ -137,6 +134,11 @@ namespace Antmicro.Renode.Peripherals.UART
 
         private void HandleTransmitData(ulong value)
         {
+            if(!enabled.Value)
+            {
+                return;
+            }
+
             TransmitCharacter((byte)value);
 
             UpdateInterrupts();
@@ -144,18 +146,25 @@ namespace Antmicro.Renode.Peripherals.UART
 
         private void UpdateInterrupts()
         {
-            bool rxInterrupt = receiveQueue.Count > 0;
+            if(!enabled.Value)
+            {
+                return;
+            }
+            bool rxInterrupt = irqRxNotEmptyEnabled.Value && RxNotEmpty;
             // In Renode, the TX happens instantly,
             // and the TX interrupt fires if the TX FIFO is empty.
-            bool txInterrupt = true;
+            bool txInterrupt = irqTxEmptyEnabled.Value;
             this.Log(LogLevel.Noisy, "Updating interrupts, tx: {0}, rx: {1}", txInterrupt, rxInterrupt);
 
             TxInterrupt.Set(txInterrupt);
             RxInterrupt.Set(rxInterrupt);
         }
 
-        private bool rxWasNotEmptyOnLastRead;
+        private bool RxNotEmpty => receiveQueue.Count > 0;
 
+        private readonly IFlagRegisterField enabled;
+        private readonly IFlagRegisterField irqTxEmptyEnabled;
+        private readonly IFlagRegisterField irqRxNotEmptyEnabled;
         private readonly Queue<byte> receiveQueue = new Queue<byte>();
 
         private const int FifoSize = 8;
