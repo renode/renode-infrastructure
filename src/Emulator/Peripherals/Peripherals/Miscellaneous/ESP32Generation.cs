76c63a076eb1c5f2a6a7dcfe1b9e56808bca8fe1
Patryk Koscik <pkoscik@antmicro.com>
2026-07-10 08:59:00 +0200

    [#100525] Add ESP32Generation

---
+++ src/Emulator/Peripherals/Peripherals/Miscellaneous/ESP32Generation.cs
@@ -0,0 +1,16 @@
+//
+// Copyright (c) 2010-2026 Antmicro
+//
+// This file is licensed under the MIT License.
+// Full license text is available in 'licenses/MIT.txt'.
+//
+
+namespace Antmicro.Renode.Peripherals.Miscellaneous
+{
+    public enum ESP32Generation
+    {
+        ESP32,   // LX6
+        ESP32S2, // LX7
+        ESP32S3, // LX7, dual core
+    }
+}
