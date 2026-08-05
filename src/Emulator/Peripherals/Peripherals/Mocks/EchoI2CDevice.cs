4a9bc6abbe0248f16ea387ddcb99fb423dd5123b
Dawid Olszewski <dolszewski@antmicro.com>
2026-08-05 10:50:14 +0200

    [#101295] EchoI2CDevice: Implement merging of consecutive I2C accesses

--- src/Emulator/Peripherals/Peripherals/Mocks/EchoI2CDevice.cs
+++ src/Emulator/Peripherals/Peripherals/Mocks/EchoI2CDevice.cs
@@ -1,9 +1,11 @@
 //
-// Copyright (c) 2010-2025 Antmicro
+// Copyright (c) 2010-2026 Antmicro
 //
 // This file is licensed under the MIT License.
 // Full license text is available in 'licenses/MIT.txt'.
 //
+using System.Collections.Generic;
+using System.Linq;
 
 using Antmicro.Renode.Logging;
 using Antmicro.Renode.Peripherals.I2C;
@@ -15,39 +17,54 @@ namespace Antmicro.Renode.Peripherals.Mocks
     {
         public EchoI2CDevice()
         {
-            Reset();
         }
 
         public void Write(byte[] data)
         {
-            this.Log(LogLevel.Debug, "Written {0} bytes of data: {1}", data.Length, Misc.PrettyPrintCollectionHex(data));
-            buffer = data;
+            this.DebugLog("Written {0} bytes of data: {1}", data.Length, Misc.PrettyPrintCollectionHex(data));
+            if(lastAccessType != AccessType.Write)
+            {
+                internalBuffer.Clear();
+            }
+            internalBuffer.EnqueueRange(data);
+            lastAccessType = AccessType.Write;
+        }
+
+        public void Reset()
+        {
+            lastAccessType = null;
+            readBuffer.Clear();
+            internalBuffer.Clear();
         }
 
         public byte[] Read(int count = 1)
         {
-            this.Log(LogLevel.Debug, "Reading {0} bytes", count);
-            var result = new byte[count];
-            for(var i = 0; i < result.Length; i++)
+            if(lastAccessType != AccessType.Read)
             {
-                result[i] = (i < buffer.Length)
-                    ? buffer[i]
-                    : (byte)0;
+                readBuffer.Clear();
+                readBuffer.EnqueueRange(internalBuffer);
             }
-
+            var result = readBuffer.DequeueRange(count).Concat(Misc.Iterate(() => (byte)0)).Take(count).ToArray();
+            this.DebugLog("Read {0} bytes: {1}", count, Misc.PrettyPrintCollectionHex(result));
+            lastAccessType = AccessType.Read;
             return result;
         }
 
         public void FinishTransmission()
         {
-            this.Log(LogLevel.Debug, "Finishing transmission");
+            this.DebugLog("Finishing transmission");
+            lastAccessType = null;
         }
 
-        public void Reset()
+        private AccessType? lastAccessType;
+
+        private readonly Queue<byte> internalBuffer = new();
+        private readonly Queue<byte> readBuffer = new();
+
+        public enum AccessType
         {
-            buffer = new byte[0];
+            Read,
+            Write
         }
-
-        private byte[] buffer;
     }
 }
