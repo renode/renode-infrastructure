4a388645715c113846faa634645d89dfc47a3fe3
Bartlomiej Stefanski <bstefanski@internships.antmicro.com>
2026-08-24 14:58:04 +0200

    [#100943] EmulationManager: Do not offset timestamp in LoadLatestSnapshot

--- src/Emulator/Main/Core/EmulationManager.cs
+++ src/Emulator/Main/Core/EmulationManager.cs
@@ -265,11 +265,7 @@ namespace Antmicro.Renode.Core
         public void LoadLatestSnapshot(bool autoStart = false)
         {
             var currentTimeStamp = CurrentEmulation.MasterTimeSource.ElapsedVirtualTime;
-            if(currentTimeStamp == TimeInterval.FromTicks(0))
-            {
-                throw new RecoverableException("There are no snapshots taken before this timestamp.");
-            }
-            LoadLatestSnapshot(currentTimeStamp - TimeInterval.FromTicks(1), autoStart);
+            LoadLatestSnapshot(currentTimeStamp, autoStart);
         }
 
         public void LoadLatestSnapshot(TimeInterval beforeOrAtTimestamp, bool autoStart = false)
