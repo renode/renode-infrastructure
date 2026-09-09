2c1e55ceb6120a83c0edb9fd70159a47310e3249
Bartlomiej Stefanski <bstefanski@internships.antmicro.com>
2026-09-09 11:50:30 +0200

    [DEV] EmulationManager: Fix copyright

--- src/Emulator/Main/Core/EmulationManager.cs
+++ src/Emulator/Main/Core/EmulationManager.cs
@@ -1,6 +1,5 @@
 //
-// Copyright (c) 2010-2025 Antmicro
-// Copyright (c) 2010-2025 Antmicro
+// Copyright (c) 2010-2026 Antmicro
 // Copyright (c) 2011-2015 Realtime Embedded
 //
 // This file is licensed under the MIT License.
